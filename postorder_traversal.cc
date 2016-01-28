1da0d6253dd4e2f063e69ac5f2a05a4bf41e4b95
Isaiah Xin <xinzhige8@gmail.com>
2016-01-28 12:32:31 -0800

    next commit

--- postorder_traversal.cc
+++ postorder_traversal.cc
@@ -1,21 +1,49 @@
+// simple but time consuming because of vector insertion
 class Solution {
 public:
   vector<int> postorderTraversal(TreeNode *root) {
     vector<int> result;
-    stack<TreeNode*> my_stack;
-    if (root != NULL) {
-            my_stack.push(root);
+    stack<TreeNode*> mystack;
+    if (root != nullptr) {
+      mystack.push(root);
     }
-    while (!my_stack.empty()) {
-      TreeNode* curr_node = my_stack.top();
-      my_stack.pop();
+    while (!mystack.empty()) {
+      TreeNode* curr_node = mystack.top();
+      mystack.pop();
       result.insert(result.begin(), curr_node->val);
-      if (curr_node->left != NULL) {
-	my_stack.push(curr_node->left);
+      if (curr_node->left != nullptr) {
+	mystack.push(curr_node->left);
       }
-      if (curr_node->right != NULL) {
-	my_stack.push(curr_node->right);
+      if (curr_node->right != nullptr) {
+	mystack.push(curr_node->right);
       }
+    }
+    return result;
+  }
+};
+
+// traditional way by tracking a last node visited
+class Solution {
+public:
+  vector<int> postorderTraversal(TreeNode *root) {
+    vector<int> result;
+    stack<TreeNode *> mystack;
+    TreeNode *last_visited = nullptr;
+    TreeNode *node = root;
+    while (!mystack.empty() || node != nullptr) {
+      if (node != nullptr) {
+        mystack.push(node);
+	node = node->left;
+      } else {
+        TreeNode *curr = mystack.top(); 
+	if (curr->right != nullptr && last_visited != curr->right) {
+          node = curr->right;		
+        } else {
+	  result.push_back(curr->val);
+	  mystack.pop();
+	  last_visited = curr;
+        } 
+      } 
     }
     return result;
   }
