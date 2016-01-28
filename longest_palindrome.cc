b3ce3d564686b22d0c8c06d77a17d58608f9a1d8
Isaiah Xin <xinzhige8@gmail.com>
2016-01-27 16:13:23 -0800

    next commit

--- longest_palindrome.cc
+++ longest_palindrome.cc
@@ -1,13 +1,13 @@
 class Solution {
 public:
-  string expandAroundCenter(string s, int c1, int c2) {
-    int l = c1, r = c2;
+  string expandAroundCenter(string s, int left, int right) {
+    int l = left, r = right;
     int n = s.length();
     while (l >= 0 && r <= n-1 && s[l] == s[r]) {
       l--;
       r++;
     }
-    return s.substr(l+1, r-l-1);
+    return s.substr(l + 1, r - l - 1);
   }
   string longestPalindrome(string s) {
     int n = s.length();
@@ -15,21 +15,17 @@ public:
       return "";
     }
     string longest = s.substr(0, 1);  // a single char itself is a palindrome
-    for (int i = 0; i < n-1; i++) {
+    for (int i = 0; i < n - 1; i++) {
       string p1 = expandAroundCenter(s, i, i);
       if (p1.length() > longest.length()) { // center is a char
 	longest = p1;
       }
       
-      string p2 = expandAroundCenter(s, i, i+1);
-      if (p2.length() > longest.length()) { // center is between two chars
+      string p2 = expandAroundCenter(s, i, i + 1);
+      if (p2.length() > longest.length()) { // center is in between two neighbor chars
 	longest = p2;
       }
     }
     return longest;
   }
 };
-
-int main() {
-  return 0;
-}
