6ea569728417ed8c85f194a21fd87c03a7c769f0
Isaiah Xin <xinzhige8@gmail.com>
2016-02-01 23:38:19 -0800

    next commit

--- n_queens2.cc
+++ n_queens2.cc
@@ -1,33 +1,31 @@
 class Solution {
 public:
-  // A[i] represents at which column for queen on row i
-  bool is_valid(int A[], int row) {
-    // we only need to consider rows before row because 
-    // rows after have not been set
+  bool isValid(vector<int>& A, int row) {
     for (int i = 0; i < row; ++i) {
-      if ((A[row] == A[i]) || (abs(A[row] - A[i]) == row - i)) {
-	      return false;
+      if ((A[row] == A[i]) || 
+	  (abs(A[row] - A[i]) == row - i)) {
+        return false;
       }
     }
     return true;
   }
-  void n_queens(int A[], int curr, int n, int& result) {
-    if (curr == n) {
-      ++result;
+  void nQueens(vector<int> A, int row, int n, int& result) {
+    if (row == n) {
+      result += 1;
       return;
     } else {
-      for (int i = 0; i < n; ++i) {
-	      A[curr] = i;
-	      if (is_valid(A, curr)){
-	        n_queens(A, curr + 1, n, result);
-	      }
+      for (int col = 0; col < n; col++) {
+        A[row] = col;
+	if (isValid(A, row)){
+	  nQueens(A, row + 1, n, result);
+	}
       }
     }
   }
   int totalNQueens(int n) {
     int result = 0;
-    int A[n];
-    n_queens(A, 0, n, result);
+    vector<int> A(n, 0);
+    nQueens(A, 0, n, result);
     return result;
   }
 };
