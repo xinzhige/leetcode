276759aa9172330a62b52e2d666320b2070c8a8f
Isaiah Xin <xinzhige8@gmail.com>
2016-02-02 00:09:19 -0800

    next commit

--- generate_parentheses.cc
+++ generate_parentheses.cc
@@ -1,16 +1,16 @@
 class Solution {
 public:
-  void addParenthesis(vector<string> &list, string str, int left_remain, 
-		      int right_remain) {
-    if (left_remain == 0 && right_remain == 0) {
+  void addParenthesis(vector<string> &list, string str, int left, 
+		      int right) {
+    if (left == 0 && right == 0) {
       list.push_back(str);
       return;
     } else {
-      if (left_remain > 0) {
-	addParenthesis(list, str + '(', left_remain - 1, right_remain);
+      if (left > 0) {
+	addParenthesis(list, str + '(', left - 1, right);
       }
-      if (right_remain > left_remain) {
-	addParenthesis(list, str + ')', left_remain, right_remain - 1);
+      if (right > left) {
+	addParenthesis(list, str + ')', left, right - 1);
       }
     }
   }
