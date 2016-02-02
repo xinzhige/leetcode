7277de48eb64437d7879d05f2de153ea7c511356
Isaiah Xin <xinzhige8@gmail.com>
2016-02-01 19:24:21 -0800

    next commit

---
+++ missing_number.cc
@@ -0,0 +1,13 @@
+class Solution {
+public:
+  int missingNumber(vector<int>& nums) {
+    int result = 0;
+    for(int i = 0; i < nums.size(); i++){
+      result ^=  nums[i];
+    }
+    for(int i = 1; i <= nums.size(); i++){
+      result ^= i;
+    }
+    return result;
+  }
+};
