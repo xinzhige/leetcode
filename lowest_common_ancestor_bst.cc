723a9877c9c29a42646f2411421cf547c2cd3bb9
Isaiah Xin <xinzhige8@gmail.com>
2016-02-04 16:48:41 -0800

    next commit

--- lowest_common_ancestor_bst.cc
+++ lowest_common_ancestor_bst.cc
@@ -1,14 +1,12 @@
 class Solution {
 public:
   TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-    while (root) {
+    while (root != nullptr) {
       if (root->val < p->val && root->val < q->val) {
         root = root->right;
-	continue;
       }	   
       if (root->val > p->val && root->val > q->val) {
         root = root->left;
-	continue;
       }
       return root;
     }
