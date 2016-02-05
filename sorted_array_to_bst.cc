9f5b10434192a19e04bd4091a6da5da4b1613045
Isaiah Xin <xinzhige8@gmail.com>
2016-02-04 23:36:24 -0800

    next commit

--- sorted_array_to_bst.cc
+++ sorted_array_to_bst.cc
@@ -1,21 +1,16 @@
 class Solution {
 public:
-  TreeNode *SortedArrayToBSTHelper(vector<int> &num, int begin, int end) {
+  TreeNode *SortedArrayToBSTHelper(vector<int> &nums, int begin, int end) {
     if (begin > end) {
-      return NULL;
+      return nullptr;
     }
-    int mid = begin + (end-begin)/2;
-    TreeNode *root = new TreeNode(num[mid]);
-    root->left = SortedArrayToBSTHelper(num, begin, mid-1);
-    root->right = SortedArrayToBSTHelper(num, mid+1, end);
+    int mid = begin + (end - begin) / 2;
+    TreeNode *root = new TreeNode(nums[mid]);
+    root->left = SortedArrayToBSTHelper(nums, begin, mid - 1);
+    root->right = SortedArrayToBSTHelper(nums, mid + 1, end);
     return root;
   }
-  TreeNode *sortedArrayToBST(vector<int> &num) {
-    // IMPORTANT: Please reset any member data you declared, as
-    // the same Solution instance will be reused for each test case.
-    if (num.size() == 0) {
-      return NULL;
-    }
-    return SortedArrayToBSTHelper(num, 0, num.size()-1);
+  TreeNode *sortedArrayToBST(vector<int> &nums) {
+    return SortedArrayToBSTHelper(nums, 0, nums.size() - 1);
   }
 };
