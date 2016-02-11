e61b62700ed41f9caeafa9b58f3883a233ea3ed0
Isaiah Xin <xinzhige8@gmail.com>
2016-02-11 11:32:51 -0800

    next commit

--- reverse_bits.cc
+++ reverse_bits.cc
@@ -3,13 +3,8 @@ public:
   uint32_t reverseBits(uint32_t n) {
     uint32_t result = 0;
     for (int i = 0; i < 32; i++) {
-      uint32_t curr_digit = n & 1;
-      if (result <= UINT32_MAX / 2 && 2 * result <= UINT32_MAX - curr_digit) {
-	result = result * 2 + curr_digit;
+	result = result * 2 + (n & 1);
         n >>= 1;
-      } else {
-        return UINT32_MAX;	      
-      }
     }
     return result;
   }
