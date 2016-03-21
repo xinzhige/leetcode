cae32cdd6625ef82a952755d257b5beda93ece53
Isaiah Xin <xinzhige8@gmail.com>
2016-03-21 11:33:29 -0700

    next commit

--- odd_even_list.cc
+++ odd_even_list.cc
@@ -1,11 +1,11 @@
 class Solution {
 public:
   ListNode* oddEvenList(ListNode* head) {
-    if(head != nullptr) {
+    if (head != nullptr) {
       ListNode* odd = head;
       ListNode* even = head->next;
       ListNode* even_head = even;
-      while(odd->next != nullptr && even->next != nullptr) {
+      while (odd->next != nullptr && even->next != nullptr) {
         odd->next = even->next;
 	odd = odd->next;
 	even->next = odd->next;
