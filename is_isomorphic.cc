a882744c69c2254d394747f843c100913051d7d8
Isaiah Xin <xinzhige8@gmail.com>
2016-11-11 17:41:24 -0800

    Add Isomorphic Strings

--- is_isomorphic.cc
+++ is_isomorphic.cc
@@ -1,19 +1,17 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        unordered_map<char, char> mymap;
-        unordered_map<char, char> mymap_reversed;
+        unordered_map<char, int> mymap;
+        unordered_map<char, int> mymap_reversed;
         if (s.size() != t.size()) {
             return false; 
         }
         for (int i = 0; i < s.size(); i++) {
-            if (mymap.find(s[i]) == mymap.end() && 
-                mymap_reversed.find(t[i]) == mymap_revered.end()) { 
-                mymap[s[i]] = t[i]; 
-                mymap_reversed[t[i]] = s[i];
-            } else if (mymap[s[i]] != t[i]) {
-                    return false;  
+            if (mymap[s[i]] != mymap_reversed[t[i]]) {
+                return false; 
             } 
+            mymap[s[i]] = i + 1;
+            mymap_reversed[t[i]] = i + 1;
         }
         return true;
     }
