715eb9711f38531767c90a41e23ad7fd9e66aed9
Zhige Xin <xinzhige8@gmail.com>
2017-01-09 15:47:34 -0800

    Add Combination Sum III

---
+++ combination_sum3.cc
@@ -0,0 +1,21 @@
+iclass Solution {
+public:
+    vector<vector<int> > combinationSum3(int k, int n) {
+        vector<vector<int>> result;
+        vector<int> combination;
+        combinationSum3(result, combination, 1, k, n);
+        return result;
+    }
+private:
+    void combinationSum3(vector<vector<int> > &res, vector<int> &combination, int start, int k, int n) {
+        if (k == 0 && n == 0) {
+            res.push_back(combination);
+            return;
+        }
+        for (int i = start; i < 10; ++i) {
+            combination.push_back(i);
+            combinationSum3(res, combination, i + 1, k - 1, n - i);
+            combination.pop_back();
+        }
+    }
+};
