51c124b5bfc9f5adeef1b2463bb5b1210a2278b6
Zhige Xin <xinzhige8@gmail.com>
2017-01-17 18:12:43 -0800

    Add Two Sum II - Input array is sorted

---
+++ two_sum2.cc
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int head = 0;
+        int tail = numbers.size() - 1;
+        vector<int> result;
+        if (numbers.size() < 2) {
+            return result; 
+        } 
+        int curr_sum = 0;
+        while (head < tail) {
+            curr_sum = numbers[head] + numbers[tail]; 
+            if (curr_sum > target) {
+                tail -= 1; 
+            } else if (curr_sum < target) {
+                head += 1; 
+            } else {
+                result.push_back(head + 1); 
+                result.push_back(tail + 1); 
+                break;
+            } 
+        }
+        return result;
+    }
+};
