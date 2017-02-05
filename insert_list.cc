f61353f5b3b02ff55a8f82f828b2f5e1d8e662a3
Zhige Xin <xinzhige8@gmail.com>
2017-02-05 14:03:09 -0800

    Change the logic of code

--- insert_list.cc
+++ insert_list.cc
@@ -1,32 +1,25 @@
-// needs 4 pointers, one is current, one is previous and the other two are their copies. Note: Change the nodes need to be swapped by starting from the rear (easy to remember and not to be wrong)
-class Solution {  
-public:  
-  ListNode *insertionSortList(ListNode *head) {  
-    if( head == NULL || head->next == NULL) {
-      return head;  
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* insertionSortList(ListNode* head) {
+        ListNode dummy(0);                   
+        while (head != nullptr) {
+            ListNode *node = &dummy;
+            while (node->next != nullptr && node->next->val < head->val) {
+                node = node->next; 
+            }
+            ListNode *tmp = head->next;
+            head->next = node->next; 
+            node->next = head; 
+            head = tmp; 
+        }
+        return dummy.next;
     }
-    ListNode dummy(0);  
-    dummy.next = head;  
-    ListNode *curr = dummy.next->next;  
-    while(curr != NULL) {
-      ListNode *prev = &dummy;  
-      while(prev->next->val <= curr->val && prev->next != curr) { // skip all the elements less than current
-	prev = prev->next;  
-      }
-      if(prev->next == curr) { // previous list is sorted
-	curr = curr->next;  
-      } else {  
-	ListNode *temp_prev = prev->next;  
-	ListNode *temp_curr = curr->next;  
-	curr->next = temp_prev;  
-	while(temp_prev->next != curr) { // if there are some elements between curr and temp_prev, skip them
-	  temp_prev = temp_prev->next;  
-	}
-	temp_prev->next = temp_curr;  
-	prev->next = curr;  
-	curr = temp_curr;  
-      }  
-    }  
-    return dummy.next;  
-  }  
 };
