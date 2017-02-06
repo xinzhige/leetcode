bdee0de5870f2339e9878bdefdc43ad0240f42ac
Zhige Xin <xinzhige8@gmail.com>
2017-02-06 13:19:51 -0800

    Make the code more readable

--- swap_pairs.cc
+++ swap_pairs.cc
@@ -1,20 +1,27 @@
-#include "linked_list.h"
-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 class Solution {
 public:
-  ListNode *swapPairs(ListNode *head) {
-    if (head == nullptr || head->next == nullptr) {
-      return head;
+    ListNode *swapPairs(ListNode *head) {
+        if (head == nullptr || head->next == nullptr) {
+            return head;
+        }
+        ListNode dummy(0);
+        dummy.next = head;
+        while (curr != nullptr && curr->next != nullptr) {
+           prev->next = post;
+           curr->next = post->next;
+           post->next = curr;
+           prev = curr;
+           curr = curr->next;
+           post = (curr == nullptr) ? nullptr : curr->next;
+        }   
+        return dummy.next;
     }
-    ListNode dummy(0);
-    dummy.next = head;
-    for(ListNode *prev = &dummy, *curr = prev->next, *post = curr->next;
-	post;
-	prev = curr, curr = curr->next, post = curr ? curr->next : nullptr) {
-      prev->next = post;
-      curr->next = post->next;
-      post->next = curr;
-    }
-    return dummy.next;
-  }
 };
