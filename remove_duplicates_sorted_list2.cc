47d40ff2e8b483897e3035e6f237a8c709d52799
Zhige Xin <xinzhige8@gmail.com>
2017-02-07 15:45:02 -0800

    Change recursive into non-recursive

--- remove_duplicates_sorted_list2.cc
+++ remove_duplicates_sorted_list2.cc
@@ -1,20 +1,20 @@
 class Solution {
 public:
-  ListNode *deleteDuplicates(ListNode *head) {
-    if(head == NULL ||head->next == NULL) {
-      return head;
+    ListNode *deleteDuplicates(ListNode *head) {
+        ListNode dummy(0);
+        auto prev = &dummy;	
+        auto curr = head;
+        while (curr) {
+	        if (curr->next && curr->next->val == curr->val) {
+                auto val = curr->val; 
+                for (; curr && curr->val == val; curr = curr->next); 
+                prev->next = curr;
+            } else {
+                prev->next = curr;
+                prev = curr;
+                curr = curr->next;
+            } 
+        }
+        return dummy.next; 
     }
-    ListNode *p = head->next;
-    if(head->val == p->val) {
-	while(head->val == p->val) {
-	    p = p->next;
-	    if(!p) {
-	      break;
-	    }
-	  }
-	return deleteDuplicates(p);
-      }
-    head->next = deleteDuplicates(head->next);
-    return head;
-  }
 };
