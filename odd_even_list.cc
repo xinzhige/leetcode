cb528d286d3bba91f5f1fb5aea2702ff66e39234
Zhige Xin <xinzhige8@gmail.com>
2017-02-07 23:03:03 -0800

    Make code more readable

--- odd_even_list.cc
+++ odd_even_list.cc
@@ -1,18 +1,18 @@
 class Solution {
 public:
-  ListNode* oddEvenList(ListNode* head) {
-    if (head != nullptr) {
-      ListNode* odd = head;
-      ListNode* even = head->next;
-      ListNode* even_head = even;
-      while (odd->next != nullptr && even->next != nullptr) {
-        odd->next = even->next;
-	odd = odd->next;
-	even->next = odd->next;
-	even = even->next;
-      }
-      odd->next = even_head;
+    ListNode* oddEvenList(ListNode* head) {
+        if (head != nullptr) {
+            ListNode* odd = head;
+            ListNode* even = head->next;
+            ListNode* even_head = even;
+            while (odd->next != nullptr && even->next != nullptr) {
+                odd->next = even->next;
+                odd = odd->next;
+                even->next = odd->next;
+                even = even->next;
+            }
+            odd->next = even_head;
+        }
+        return head;
     }
-    return head;
-  }
 };
