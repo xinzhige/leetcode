24f9dd02f91bdaca8d2a0fa6930f2d229af7b687
Zhige Xin <xinzhige8@gmail.com>
2017-02-07 23:16:07 -0800

    Make code more readable

--- rotate_list.cc
+++ rotate_list.cc
@@ -1,24 +1,24 @@
 class Solution {
 public:
-  ListNode *rotateRight(ListNode *head, int k) {
-    if (head == nullptr) {
-      return nullptr;
+    ListNode *rotateRight(ListNode *head, int k) {
+        if (head == nullptr) {
+            return head;
+        }
+        ListNode *prev = head;
+        ListNode *curr = head;
+        for (int i = 0; i < k; i++) {
+            curr = curr->next;
+            if (curr == nullptr) {
+	            curr = head;
+            }
+        }
+        while (curr->next != nullptr) {
+            prev = prev->next;
+            curr = curr->next;
+        }
+        curr->next = head;
+        head = prev->next;
+        prev->next = nullptr;
+        return head;
     }
-    ListNode *prev = head;
-    ListNode *curr = head;
-    for (int i = 0; i < k; i++) {
-      curr = curr->next;
-      if (curr == nullptr) {
-	curr = head;
-      }
-    }
-    while (curr->next != nullptr) {
-      prev = prev->next;
-      curr = curr->next;
-    }
-    curr->next = head;
-    head = prev->next;
-    prev->next = nullptr;
-    return head;
-  }
 };
