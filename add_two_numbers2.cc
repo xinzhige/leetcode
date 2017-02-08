f19799889f2659139df5992ed56be0e12d60c2fb
Zhige Xin <xinzhige8@gmail.com>
2017-02-07 23:09:47 -0800

    Make code more readable

--- add_two_numbers2.cc
+++ add_two_numbers2.cc
@@ -11,26 +11,26 @@ public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         ListNode* p1 = reverseList(l1);
         ListNode* p2 = reverseList(l2);
-        int offset = 0;
+        int carry = 0;
         int sum = 0;
-        ListNode* dummy = new ListNode(0);
-        while (p1 || p2) {
+        ListNode dummy(0);
+        while (p1 != nullptr || p2 != nullptr || carry != 0) {
             int curr1 = (p1 == nullptr) ? 0 : p1->val;
             int curr2 = (p2 == nullptr) ? 0 : p2->val;
-            int curr = curr1 + curr2 + offset; 
+            int curr = curr1 + curr2 + carry; 
             sum = curr % 10; 
+            carry = curr / 10; 
             ListNode* node = new ListNode(sum);
-            node->next = dummy->next;
-            dummy->next = node;
-            offset = curr / 10; 
-            if (p1) {
+            node->next = dummy.next;
+            dummy.next = node;
+            if (p1 != nullptr) {
                 p1 = p1->next; 
             }
-            if (p2) {
+            if (p2 != nullptr) {
                 p2 = p2->next; 
             }
         }
-        return dummy->next; 
+        return dummy.next; 
     }
 private:
     ListNode* reverseList(ListNode* head) {
