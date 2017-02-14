f1f4fe8dc546a42a745c9b8f1b12873aa15e4dba
Zhige Xin <xinzhige8@gmail.com>
2017-02-14 14:36:58 -0800

    Replace vector with list

--- postorder_traversal.cc
+++ postorder_traversal.cc
@@ -1,53 +1,57 @@
-// simple but time consuming because of vector insertion
+// Use a stack and a list
 class Solution {
 public:
-  vector<int> postorderTraversal(TreeNode *root) {
-    vector<int> result;
-    stack<TreeNode*> mystack;
-    if (root != nullptr) {
-      mystack.push(root);
+    vector<int> postorderTraversal(TreeNode *root) {
+        vector<int> result;
+        stack<TreeNode*> mystack;
+        list<int> mylist;
+        if (root != nullptr) {
+            mystack.push(root);
+        }
+        while (!mystack.empty()) {
+            TreeNode* node = mystack.top();
+            mystack.pop();
+            mylist.emplace_front(node->val);
+            if (node->left != nullptr) {
+                mystack.push(node->left);
+            }
+            if (node->right != nullptr) {
+                mystack.push(node->right);
+            }
+        }
+        for (auto value : mylist) {
+            result.emplace_back(value); 
+        }
+        return result;
     }
-    while (!mystack.empty()) {
-      TreeNode* curr_node = mystack.top();
-      mystack.pop();
-      result.insert(result.begin(), curr_node->val);
-      if (curr_node->left != nullptr) {
-	mystack.push(curr_node->left);
-      }
-      if (curr_node->right != nullptr) {
-	mystack.push(curr_node->right);
-      }
-    }
-    return result;
-  }
 };
 
 // traditional way by tracking a last node visited
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode *root) {
-      vector<int> result;
-      if (root == nullptr) {
-        return result;	  
-      }
-      stack<TreeNode *> mystack;
-      TreeNode* last_visited = nullptr;
-      while (!mystack.empty() || root != nullptr) {
-        if (root != nullptr) {
-          mystack.push(root);
-          root = root->left;
-        } else {
-          TreeNode* node = mystack.top();
-          if (node->right != nullptr &&
-    	  last_visited != node->right) {
-            root = node->right; 
-          } else {
-            mystack.pop();
-    	    result.push_back(node->val);
-    	    last_visited = node;
-          }
+        vector<int> result;
+        if (root == nullptr) {
+            return result;	  
         }
-      }
-      return result;
+        stack<TreeNode *> mystack;
+        TreeNode* last_visited = nullptr;
+        while (!mystack.empty() || root != nullptr) {
+            if (root != nullptr) {
+                mystack.push(root);
+                root = root->left;
+            } else {
+                TreeNode* node = mystack.top();
+                if (node->right != nullptr &&
+                    last_visited != node->right) {
+                    root = node->right; 
+                } else {
+                    mystack.pop();
+                    result.push_back(node->val);
+                    last_visited = node;
+                }
+            }
+        }
+        return result;
     }
 };
