497b00e810f5b4cc5f7f82f28d617c21b2c3118d
Zhige Xin <xinzhige8@gmail.com>
2017-02-14 00:24:59 -0800

    Make the code more readable

--- simplify_path.cc
+++ simplify_path.cc
@@ -1,21 +1,21 @@
 class Solution {
 public:
-  string simplifyPath(string path) {
-    string result, current;
-    vector<string> dirs;
-    stringstream out(path);
-    while (getline(out, current, '/')) {
-      if (current == "..") {
-	if (!dirs.empty()) {
-	  dirs.pop_back();
-	}
-      } else if (!(current.size() == 0) && !(current == ".")) {
-	  dirs.push_back(current);
-      }
+    string simplifyPath(string path) {
+        string result, current;
+        vector<string> dirs;
+        stringstream out(path);
+        while (getline(out, current, '/')) {
+            if (current == "..") {
+	            if (!dirs.empty()) {
+	                dirs.pop_back();
+	            }
+            } else if (!current.empty() && current != ".") {
+	            dirs.push_back(current);
+            }
+        }
+        for (auto s : dirs) {
+            result += "/" + s;
+        }
+        return result.size() == 0 ? "/" : result;
     }
-    for (auto s : dirs) {
-      result += "/" + s;
-    }
-    return result.size() == 0 ? "/" : result;
-  }
 };
