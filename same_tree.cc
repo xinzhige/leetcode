d08b77dcec1a92a0e4070d745183890adb5cc5f8
Zhige Xin <xinzhige8@gmail.com>
2017-02-16 14:49:58 -0800

    Minor changes

--- same_tree.cc
+++ same_tree.cc
@@ -1,45 +1,44 @@
 // recursive
 class Solution {
 public:
-  bool isSameTree(TreeNode *p, TreeNode *q) {
-    if (p == nullptr && q == nullptr) {
-      return true;
+    bool isSameTree(TreeNode *p, TreeNode *q) {
+        if (p == nullptr && q == nullptr) {
+            return true;
+        }
+        if (p == nullptr || q == nullptr || p->val != q->val) {
+            return false;
+        }
+        return isSameTree(p->left, q->left) 
+            && isSameTree(p->right, q->right);
     }
-    if (p == nullptr || q == nullptr) {
-      return false;
-    }
-    return p->val == q->val 
-      && isSameTree(p->left, q->left) 
-      && isSameTree(p->right, q->right);
-  }
 };
 
 // Iterative
 class Solution {
 public:
-  bool isSameTree(TreeNode *p, TreeNode *q) {
-    queue<TreeNode *> queue1, queue2;
-    queue1.push(p);
-    queue2.push(q);
-    while (!queue1.empty() && !queue2.empty()) {
-      TreeNode* node1 = queue1.front();
-      TreeNode* node2 = queue2.front();
-      queue1.pop();
-      queue2.pop();
-      if (node1 == nullptr && node2 == nullptr) {
-        continue; 
-      }
-      if (node1 == nullptr || node2 == nullptr) {
-        return false;	      
-      }
-      if (node1->val != node2->val) {
-        return false;	      
-      }
-      queue1.push(node1->left);
-      queue2.push(node2->left);
-      queue1.push(node1->right);
-      queue2.push(node2->right);
+    bool isSameTree(TreeNode *p, TreeNode *q) {
+        queue<TreeNode *> queue1, queue2;
+        queue1.emplace(p);
+        queue2.push(q);
+        while (!queue1.empty() && !queue2.empty()) {
+            TreeNode* node1 = queue1.front();
+            TreeNode* node2 = queue2.front();
+            queue1.pop();
+            queue2.pop();
+            if (node1 == nullptr && node2 == nullptr) {
+                continue; 
+            }
+            if (node1 == nullptr || node2 == nullptr) {
+                return false;	      
+            }
+            if (node1->val != node2->val) {
+                return false;	      
+            }
+            queue1.push(node1->left);
+            queue2.push(node2->left);
+            queue1.push(node1->right);
+            queue2.push(node2->right);
+        }
+        return true;
     }
-    return true;
-  }
 };
