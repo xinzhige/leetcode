22e4e7b766746b4aaa577b6123d419fdfaaa0809
Zhige Xin <xinzhige8@gmail.com>
2017-02-15 23:47:03 -0800

    Delete unnecessary lines

--- binary_tree_paths.cc
+++ binary_tree_paths.cc
@@ -1,25 +1,25 @@
 class Solution {
 public:
-  void binaryTreePathsHelper(TreeNode* root, vector<int> solution, vector<string>& result) {
-    if (root == nullptr) return;
-    solution.push_back(root->val);
-    if (root->left == nullptr && root->right == nullptr){
-      if(solution.size() > 0){
-        stringstream ss;
-        for(int i = 0; i < solution.size(); i++){
-          ss << solution[i] << ((i < solution.size() - 1) ? "->" : "");
+    void binaryTreePathsHelper(TreeNode* root, vector<int> solution, vector<string>& result) {
+        if (root == nullptr) {
+            return;
         }
-        result.push_back(ss.str());
-      }
-      return;
+        solution.emplace_back(root->val);
+        if (root->left == nullptr && root->right == nullptr) {
+            stringstream ss;
+            for (int i = 0; i < solution.size(); i++) {
+                ss << solution[i] << ((i < solution.size() - 1) ? "->" : "");
+            }
+            result.emplace_back(ss.str());
+            return;
+        }
+        binaryTreePathsHelper(root->left, solution, result);
+        binaryTreePathsHelper(root->right, solution, result);
     }
-    binaryTreePathsHelper(root->left, solution, result);
-    binaryTreePathsHelper(root->right, solution, result);
-  }
-  vector<string> binaryTreePaths(TreeNode* root) {
-    vector<string> result;
-    vector<int> solution;
-    binaryTreePathsHelper(root, solution, result);
-    return result;
-  }
+    vector<string> binaryTreePaths(TreeNode* root) {
+        vector<string> result;
+        vector<int> solution;
+        binaryTreePathsHelper(root, solution, result);
+        return result;
+    }
 };
