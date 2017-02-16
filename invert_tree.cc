b157c67269d514ba0fdfb77ba9148a2548788ed8
Zhige Xin <xinzhige8@gmail.com>
2017-02-16 11:21:18 -0800

    Use swap function

--- invert_tree.cc
+++ invert_tree.cc
@@ -1,37 +1,37 @@
 // Recursive: invert left tree and store it then invert right tree and assign it to left child lastly assign the temp to right child
 class Solution {
 public:
-  TreeNode* invertTree(TreeNode* root) {
-    if (root == nullptr) {
-      return nullptr;	    
-    } 
-    TreeNode *node = invertTree(root->left);
-    root->left = invertTree(root->right);
-    root->right = node;
-    return node;
-  }
+    TreeNode* invertTree(TreeNode* root) {
+        if (root == nullptr) {
+            return root;	    
+        } 
+        swap(root->left, root->right); 
+        invertTree(root->left);
+        invertTree(root->right);
+        return root; 
+    }
 };
 
 // Non-recursive
 class Solution {
 public:
-  TreeNode* invertTree(TreeNode* root) {
-    if (root == nullptr) {
-      return root;
+    TreeNode* invertTree(TreeNode* root) {
+        if (root == nullptr) {
+            return root;
+        }
+        stack<TreeNode *> mystack;
+        mystack.emplace(root);
+        while (!mystack.empty()) {
+            TreeNode* curr = mystack.top();
+            mystack.pop();
+            swap(curr->left, curr->right);
+            if (curr->left != nullptr) {
+                mystack.emplace(curr->left);
+            }
+            if (curr->right != nullptr) {
+                mystack.emplace(curr->right);
+            }
+        }
+        return root;
     }
-    stack<TreeNode *> mystack;
-    mystack.push(root);
-    while (!mystack.empty()) {
-      TreeNode* curr = mystack.top();
-      mystack.pop();
-      swap(curr->left, curr->right);
-      if (curr->left != nullptr) {
-        mystack.push(curr->left);
-      }
-      if (curr->right != nullptr) {
-        mystack.push(curr->right);
-      }
-    }
-    return root;
-  }
 };
