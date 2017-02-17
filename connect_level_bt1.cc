e2cf41d387c96467e51276debfe7dc011f6d246d
Isaiah Xin <xinzhige8@gmail.com>
2017-02-17 12:27:29 -0800

    Indentation

--- connect_level_bt1.cc
+++ connect_level_bt1.cc
@@ -1,16 +1,16 @@
 class Solution {
 public:
-  void connect(TreeLinkNode *root) {
-    if (root == nullptr) {
-      return;
+    void connect(TreeLinkNode *root) {
+        if (root == nullptr) {
+            return;
+        }
+        if (root->left != nullptr && root->right != nullptr) {
+            root->left->next = root->right;
+        }
+        if (root->right != nullptr && root->next != nullptr) {
+            root->right->next = root->next->left;
+        }
+        connect(root->left);
+        connect(root->right);
     }
-    if (root->left != nullptr && root->right != nullptr) {
-      root->left->next = root->right;
-    }
-    if (root->right != nullptr && root->next != nullptr) {
-      root->right->next = root->next->left;
-    }
-    connect(root->left);
-    connect(root->right);
-  }
 };
