dae7a3f86e0cde1a76772e2cfe9d066f96d50ee0
Zhige Xin <xinzhige8@gmail.com>
2017-02-16 20:31:59 -0800

    Minor changes

--- lowest_common_ancestor_bt.cc
+++ lowest_common_ancestor_bt.cc
@@ -1,14 +1,14 @@
 class Solution {
 public:
-  TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-    if (root == nullptr || root == p || root == q) {
-      return root;	    
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (root == nullptr || root == p || root == q) {
+            return root;	    
+        }
+        TreeNode *left_side = lowestCommonAncestor(root->left, p, q);
+        TreeNode *right_side = lowestCommonAncestor(root->right, p, q);
+        if (left_side != nullptr && right_side != nullptr) {
+            return root;	    
+        }
+        return left_side != nullptr ? left_side : right_side;
     }
-    TreeNode *left_side = lowestCommonAncestor(root->left, p, q);
-    TreeNode *right_side = lowestCommonAncestor(root->right, p, q);
-    if (left_side != nullptr && right_side != nullptr) {
-      return root;	    
-    }
-    return left_side != nullptr ? left_side : right_side;
-  }
 };
