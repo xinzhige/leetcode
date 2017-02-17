b93b8921a6a365b94bdd4f522f8a8650e0837c4e
Zhige Xin <xinzhige8@gmail.com>
2017-02-16 21:15:11 -0800

    Delete the erroneous solution

--- is_valid_BST.cc
+++ is_valid_BST.cc
@@ -1,47 +1,23 @@
-// min-max solution
-class Solution {
-public:
-  bool isValidBST(TreeNode *root, int min, int max) {
-    if (root == nullptr) {
-      return true;
-    }
-    if (root->val <= min || root->val >= max) {
-      return false;
-    }
-    if (!isValidBST(root->left, min, root->val) ||
-	!isValidBST(root->right, root->val, max)) {
-      return false;
-    }
-    return true;
-  }
-  bool isValidBST(TreeNode *root) {
-    return isValidBST(root, INT_MIN, INT_MAX);
-  }
-};
-
 // inorder solution, recursive version
 class Solution {
 public:
-  // Add wrapper function to be tested on leetcode
-  int last_val;
-  bool isValidBST(TreeNode *root) {
-    last_val = INT_MIN;
-    return  isValidBST_inner(root);
-  } 
-  bool isValidBST_inner(TreeNode *root) {
-    if (root == nullptr) {
-      return true;
+    bool isValidBST(TreeNode *root) {
+        TreeNode* prev = nullptr;
+        return  validate(root, prev);
+    } 
+
+private:
+    bool validate(TreeNode* root, TreeNode* &prev) {
+        if (root == nullptr) {
+            return true;
+        }
+        if (!validate(root->left, prev)) {
+            return false;
+        }
+        if (prev != nullptr && root->val <= prev->val) {
+            return false;
+        }
+        prev = root;
+        return validate(root->right, prev);
     }
-    if (!isValidBST_inner(root->left)) {
-      return false;
-    }
-    if (root->value <= last_val) {
-      return false;
-    }
-    last_val = root->value;
-    if (!isValidBST_inner(root->right)) {
-      return false;
-    }
-    return true;
-  }
 };
