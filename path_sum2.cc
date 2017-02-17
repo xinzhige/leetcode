ffe32e9ca2f30e09e4c22d4f1d59793574ad6ad1
Zhige Xin <xinzhige8@gmail.com>
2017-02-16 20:48:53 -0800

    Update the code

--- path_sum2.cc
+++ path_sum2.cc
@@ -1,30 +1,31 @@
 class Solution {
 public:
-  void findSol(TreeNode* root, int sum, vector<vector<int> > &all_sol, vector<int> &sol){
-    if ( (root->left == NULL) && (root->right == NULL) && (sum-root->val == 0) ) { 
-      sol.push_back(root->val); 
-      all_sol.push_back(sol);
-      return;    
+    vector<vector<int> > pathSum(TreeNode *root, int sum) {
+        vector<vector<int>> result;
+        vector<int> tmp;
+        dfs(root, sum, tmp, result);
+        return result;
     }
-    sol.push_back(root->val);
-    if (root->left != NULL){
-      findSol(root->left, sum-root->val, all_sol, sol);
-      sol.pop_back();
+
+private:
+    void dfs(TreeNode* root, int sum, vector<int>& tmp, vector<vector<int>>& result) {
+        if (root == nullptr) {
+            return; 
+        }
+        if (root->left == nullptr && root->right == nullptr && 
+            root->val == sum) { 
+            tmp.emplace_back(root->val); 
+            result.emplace_back(tmp);
+            return;    
+        }
+        tmp.emplace_back(root->val);
+        if (root->left != nullptr) {
+            dfs(root->left, sum - root->val, tmp, result);
+            tmp.pop_back();
+        }
+        if (root->right != nullptr) {
+            dfs(root->right, sum - root->val, tmp, result);    
+            tmp.pop_back();
+        }
     }
-    if (root->right != NULL){
-      findSol(root->right, sum-root->val, all_sol, sol);    
-      sol.pop_back();
-    }
-  }
-  vector<vector<int> > pathSum(TreeNode *root, int sum) {
-    // Start typing your C/C++ solution below
-    // DO NOT write int main() function
-    vector<vector<int> > all_sol;
-    vector<int> sol;
-    if (root == NULL) {
-      return all_sol;
-    }
-    findSol(root, sum, all_sol, sol);
-    return all_sol;
-  }
 };
