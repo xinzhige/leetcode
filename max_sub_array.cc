abec66f576103c74413b76d92d1dc9be74bea9e0
Zhige Xin <xinzhige8@gmail.com>
2017-02-18 22:58:28 -0800

    Indentation

--- max_sub_array.cc
+++ max_sub_array.cc
@@ -1,24 +1,24 @@
 class Solution {
 public:
-  int maxSubArray(int A[], int n) {
-    int max_so_far = A[0];
-    int max_ending_here = A[0];
-    // int begin = 0;
-    // int begin_temp = 0;
-    // int end = 0;
-    for (int i = 1; i < n; ++i) {
-      if (max_ending_here < 0) {
-	    max_ending_here = A[i];
-	    // begin_temp = i;
-      } else {
-	    max_ending_here += A[i];
-      }
-      if (max_ending_here > max_so_far) {
-	    max_so_far = max_ending_here;
-	    // begin = begin_temp;
-	    // end = i;
-      }
+    int maxSubArray(vector<int>& nums) {
+        int max_so_far = nums[0];
+        int max_ending_here = nums[0];
+        // int begin = 0;
+        // int begin_temp = 0;
+        // int end = 0;
+        for (int i = 1; i < nums.size(); ++i) {
+            if (max_ending_here < 0) {
+                max_ending_here = nums[i];
+                // begin_temp = i;
+            } else {
+                max_ending_here += nums[i];
+            }
+            if (max_ending_here > max_so_far) {
+                max_so_far = max_ending_here;
+                // begin = begin_temp;
+                // end = i;
+            }
+        }
+        return max_so_far;
     }
-    return max_so_far;
-  }
 };
