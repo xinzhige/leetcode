e2c9c766659bdb295a7c2f109b5fd164e5411d86
Zhige Xin <xinzhige8@gmail.com>
2017-02-23 15:07:33 -0800

    Delete the recursive way

--- merge_two_lists.cc
+++ merge_two_lists.cc
@@ -1,30 +1,3 @@
-// recursive way
-/*
-class Solution {
-public:
-  ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
-    // Start typing your C/C++ solution below
-    // DO NOT write int main() function
-    if (l1==nullptr) {
-      return l2;
-    }
-    if (l2==nullptr) {
-      return l1;
-    }
-    ListNode *ret;
-    if (l1->val < l2->val) {
-      ret = l1;
-      ret->next = mergeTwoLists(l1->next,l2);
-    } else {
-      ret = l2;
-      ret->next = mergeTwoLists(l1,l2->next);
-    }
-    return ret;
-  }
-};
-*/
-
-// non-recursive 
 class Solution {
 public:
   ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
