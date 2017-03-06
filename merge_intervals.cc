883c8eb6fbe57042208ece77b5634d6039a19a53
Zhige Xin <xinzhige8@gmail.com>
2017-03-05 22:19:43 -0800

    Change the logic by using sort

--- merge_intervals.cc
+++ merge_intervals.cc
@@ -9,30 +9,23 @@
  */
 class Solution {
 public:
-  vector<Interval> merge(vector<Interval> &intervals) {
-    vector<Interval> result;
-    for (auto curr : intervals) {
-      insert(result, curr);
+    vector<Interval> merge(vector<Interval> &intervals) {
+        if (intervals.size() == 0) {
+            return intervals; 
+        }
+        sort(intervals.begin(), intervals.end(), myfunc);
+        vector<Interval> result{intervals[0]};
+        for (int i = 1; i < intervals.size(); ++i) {
+            if (intervals[i].start <= result.back().end) {
+                result.back().end = max(intervals[i].end, result.back().end); 
+            } else {
+                result.emplace_back(intervals[i]); 
+            } 
+        }
+        return result;
     }
-    return result;
-  }
 private:
-  vector<Interval> insert(vector<Interval> &intervals, Interval newInterval) {
-    vector<Interval>::iterator it = intervals.begin();
-    while (it != intervals.end()) {
-      if (newInterval.end < it->start) {
-	intervals.insert(it, newInterval);
-	return intervals;
-      } else if (newInterval.start > it->end) {
-	it++;
-	continue;
-      } else {
-	newInterval.start = min(newInterval.start, it->start);
-	newInterval.end = max(newInterval.end, it->end);
-	it = intervals.erase(it);
-      } 
-    }
-    intervals.push_back(newInterval);
-    return intervals;
-  }
+    static bool myfunc(const Interval& a, const Interval& b) {
+        return a.start < b.start; 
+    } 
 };
