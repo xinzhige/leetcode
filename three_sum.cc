f0031c7f627d8d39703577873b3b23970acb7fd0
Zhige Xin <xinzhige8@gmail.com>
2017-03-07 21:53:39 -0800

    Change some coding style

--- three_sum.cc
+++ three_sum.cc
@@ -1,34 +1,30 @@
 class Solution {  
 public:  
-  vector<vector<int> > threeSum(vector<int> &num) {
-    sort(num.begin(), num.end());
-    vector<vector<int>> result;
-    size_t num_len = num.size();
-    for (int i = 0; i < num_len; i++) {
-      if ((i > 0) && (num[i] == num[i - 1])) {
-        continue;
-      }
-      int l = i + 1;
-      int r = num_len - 1;
-      while (l < r) {
-        int s = num[i] + num[l] + num[r];
-        if (s > 0) {
-          r -= 1;
-        } else if (s < 0) {
-          l += 1;
-        } else {
-          result.push_back(vector<int> {num[i], num[l], num[r]});
-          while (num[l] == num[l + 1]) {
-            l += 1;
-          }
-          while (num[r] == num[r - 1]) {
-            r -= 1;
-          }
-          l += 1; 
-          r -= 1;
+    vector<vector<int> > threeSum(vector<int> &nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> result;
+        size_t nums_len = nums.size();
+        for (int i = 0; i < nums_len; i++) {
+            if ((i > 0) && (nums[i] == nums[i - 1])) {
+                continue;
+            }
+            int l = i + 1;
+            int r = nums_len - 1;
+            while (l < r) {
+                int tmp = nums[i] + nums[l] + nums[r];
+                if (tmp > 0) {
+                    r -= 1;
+                } else if (tmp < 0) {
+                    l += 1;
+                } else {
+                    result.emplace_back(vector<int> {nums[i], nums[l], nums[r]});
+                    for (; nums[l] == nums[l + 1]; ++l); 
+                    for (; nums[r] == nums[r - 1]; --r); 
+                    l += 1; 
+                    r -= 1;
+                }
+            }
         }
-      }
+        return result;
     }
-    return result;
-  }
 };
