85a876e657bc858d02927dd0d2485ad95bec456b
Zhige Xin <xinzhige8@gmail.com>
2017-03-19 22:51:56 -0700

    Remove kth_largest_array.cc

--- kth_largest_array.cc
+++
@@ -1,8 +0,0 @@
-class Solution {
-public:
-  static bool greater(int i,int j) { return i > j; }
-  int findKthLargest(vector<int>& nums, int k) {
-    sort(nums.begin(), nums.end(), greater);
-    return nums[k-1];
-  }
-};
