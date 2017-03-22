9ee2e50b44080ca0eff1ecce2e12f5a6cfde7e57
Zhige Xin <xinzhige8@gmail.com>
2017-03-21 21:37:42 -0700

    Change the algorithm

--- tinyurl.cc
+++ tinyurl.cc
@@ -1,58 +1,32 @@
 class Solution {
 public:
 
-    // Encodes a URL to a shortened URL.
-    string encode(string longUrl) {
-        indexEncodes.emplace_back(longUrl);
-        return base + encode62(indexEncodes.size() - 1);
+  string getRand() {
+    string s;
+    for (int i = 0; i < 6; i++) {
+      s += alphabet[rand() % 62];
+    }
+    return s;
+  }
+
+  // Encodes a URL to a shortened URL.
+  string encode(string longUrl) {
+    while (mymap.count(key)) {
+      key = getRand();
+    }
+    mymap[key] = longUrl;
+    return "http://tinyurl.com/" + key;
   }
 
   // Decodes a shortened URL to its original URL.
   string decode(string shortUrl) {
-    return indexEncodes[decode62(shortUrl.substr(19))];
+    return mymap.get(shortUrl.replace("http://tinyurl.com/", ""));
   }
 
 private:
-    vector<string> indexEncodes; 
-    const string base = "http://tinyurl.com/";
-
-    char map62(int x) {
-        if (x < 10) {
-            return (char)('0' + x);
-        } else if (x < 36) {
-            return (char)(x + 'a' - 10);
-        } else {
-            return (char)(x + 'A' - 36);
-        }
-    }
-    
-    int rev62(char c) {
-        if (c >= '0' && c <= '9') {
-            return c - '0';
-        } else if (c >= 'a' && c <= 'z') {
-            return c - 'a' + 10;
-        } else if (c >= 'A' && c <= 'Z') {
-            return c - 'A' + 36;
-        }
-    }
-    
-    string encode62(int x){
-        string res;
-        for(; x > 0; x /= 62) {
-            int rem = x % 62;
-            res += map62(rem);
-        }
-        return res;
-    }
-    
-    int decode62(string s) {
-        int x = 0;
-        for(int i = s.size() - 1; i >= 0; i--) {
-            x *= 62;
-            x += rev62(s[i]);
-        }
-        return x;
-    }
+  string alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  unordered_map<string, string> mymap;
+  string key;
 };
 
 // Your Solution object will be instantiated and called as such:
