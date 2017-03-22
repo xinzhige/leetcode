f05b7994743811882b3107ffaf24294784913b89
Zhige Xin <xinzhige8@gmail.com>
2017-03-21 18:52:01 -0700

    Replace find with count

--- contiguous_array.cc
+++ contiguous_array.cc
@@ -6,7 +6,7 @@ public:
     unordered_map<int, int> mymap{{0, -1}};
     for (int i = 0; i < nums.size(); ++i) {
       sum += (nums[i] << 1) - 1; 
-      if (mymap.find(sum) != mymap.end()) {
+      if (mymap.count(sum)) {
         result = max(result, i - mymap[sum]); 
       } else {
         mymap[sum] = i; 
