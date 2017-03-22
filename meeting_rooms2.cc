1e461a50bef1d73c932006a041ee3a2d7aced771
Zhige Xin <xinzhige8@gmail.com>
2017-03-21 23:34:37 -0700

    Change the first algorithm

--- meeting_rooms2.cc
+++ meeting_rooms2.cc
@@ -9,45 +9,42 @@
  */
 class Solution {
 public:
-    int minMeetingRooms(vector<Interval>& intervals) {
-        vector<int> mystart, myend;
-        for (auto i : intervals) {
-            mystart.emplace_back(i.start); 
-            myend.emplace_back(i.end);
-        }
-        sort(mystart.begin(), mystart.end());
-        sort(myend.begin(), myend.end());
-        int result = 0;
-        int end_pos = 0;
-        for (int i = 0; i < mystart.size(); ++i) {
-            if (mystart[i] < myend[end_pos]) {
-                result += 1; 
-            } else {
-                end_pos += 1; 
-            } 
-        }
-        return result;
+  int minMeetingRooms(vector<Interval>& intervals) {
+    map<int, int> mymap;
+    for (const auto &a : intervals) {
+      mymap[a.start] += 1;
+      mymap[a.end] -= 1;
     }
+    int rooms = 0;
+    int result = 0;
+    for (const auto &it : mymap) {
+      rooms += it.second; 
+      result = max(result, rooms);
+    }
+    return result;
+  }
 };
 
 
 // Heap
 class Solution {
 public:
-    int minMeetingRooms(vector<Interval>& intervals) {
-        sort(intervals.begin(), intervals.end(), compare);
-        priority_queue<int, vector<int>, greater<int>> q;
-        for (auto i : intervals) {
-            if (!q.empty() && q.top() <= i.start) {
-                q.pop(); 
-            } 
-            q.emplace(i.end);
-        }
-        return q.size();
+  int minMeetingRooms(vector<Interval>& intervals) {
+    sort(intervals.begin(), intervals.end(), mycompare);
+    priority_queue<int, vector<int>, greater<int>> q;
+    for (auto i : intervals) {
+      if (!q.empty() && q.top() <= i.start) {
+	q.pop(); 
+      } 
+      q.emplace(i.end);
     }
+    return q.size();
+  }
 
 private:
-    static bool compare(const Interval& a, const Interval& b) {
+  struct Compare {
+    bool operator() (const Interval& a, const Interval& b) {
         return a.start < b.start; 
     }
+  }mycompare;
 };
