3b7d20be3bebf3b94d7c978f534793dceaba8071
Zhige Xin <xinzhige8@gmail.com>
2017-03-22 22:12:38 -0700

    Minor change

--- level_order_bt.cc
+++ level_order_bt.cc
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    vector<vector<int>> levelOrder(TreeNode* root) {
-      vector<vector<int> > result;
-      queue<TreeNode *> myqueue;
-      if (root != nullptr) {
-        myqueue.push(root);
-      }
-      while (!myqueue.empty()) {
-        vector<int> level;
-        for (int i = myqueue.size(); i > 0; i--) {
-          TreeNode* node = myqueue.front();
-          myqueue.pop();
-          level.push_back(node->val);
-          if (node->left != nullptr) {
-            myqueue.push(node->left);	      
-          }
-          if (node->right != nullptr) {
-            myqueue.push(node->right);	      
-          }
-        }
-        result.push_back(level);
-      }
-      return result;
+  vector<vector<int>> levelOrder(TreeNode* root) {
+    vector<vector<int> > result;
+    queue<TreeNode *> myqueue;
+    if (root) {
+      myqueue.push(root);
     }
+    while (!myqueue.empty()) {
+      vector<int> level;
+      for (int i = myqueue.size(); i > 0; i--) {
+	TreeNode* node = myqueue.front();
+	myqueue.pop();
+	level.push_back(node->val);
+	if (node->left) {
+	  myqueue.push(node->left);	      
+	}
+	if (node->right) {
+	  myqueue.push(node->right);	      
+	}
+      }
+      result.push_back(level);
+    }
+    return result;
+  }
 };
