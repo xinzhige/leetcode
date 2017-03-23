c8f9efa58f87b53d0aa83a0dfed2ff8f213adf9b
Zhige Xin <xinzhige8@gmail.com>
2017-03-22 20:54:52 -0700

    Minor change

--- combination_sum3.cc
+++ combination_sum3.cc
@@ -1,21 +1,22 @@
-iclass Solution {
+class Solution {
 public:
-    vector<vector<int> > combinationSum3(int k, int n) {
-        vector<vector<int>> result;
-        vector<int> combination;
-        combinationSum3(result, combination, 1, k, n);
-        return result;
-    }
+  vector<vector<int> > combinationSum3(int k, int n) {
+    vector<vector<int>> result;
+    vector<int> combination;
+    combinationSum3(result, combination, 1, k, n);
+    return result;
+  }
 private:
-    void combinationSum3(vector<vector<int> > &res, vector<int> &combination, int start, int k, int n) {
-        if (k == 0 && n == 0) {
-            res.push_back(combination);
-            return;
-        }
-        for (int i = start; i < 10; ++i) {
-            combination.push_back(i);
-            combinationSum3(res, combination, i + 1, k - 1, n - i);
-            combination.pop_back();
-        }
+  void combinationSum3(vector<vector<int>> &res, vector<int> &combination, int start, int k, int n) {
+    if (k == 0 && n == 0) {
+      res.push_back(combination);
+      return;
     }
+    for (int i = start; i <= num; ++i) {
+      combination.emplace_back(i);
+      combinationSum3(res, combination, i + 1, k - 1, n - i);
+      combination.pop_back();
+    }
+  }
+  const int num = 9;
 };
