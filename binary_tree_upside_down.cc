d562be61c930be1fc77ff17acc302ee0679f9497
Isaiah Xin <xinzhige8@gmail.com>
2017-06-25 18:20:59 -0700

    Add Binary Tree Upside Down

---
+++ binary_tree_upside_down.cc
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+  public:
+  TreeNode* upsideDownBinaryTree(TreeNode* root) {
+    if (root == nullptr || (root->left == nullptr && root->right == nullptr)) {
+      return root; 
+    }                           
+    TreeNode *new_root = upsideDownBinaryTree(root->left);
+    root->left->left = root->right;
+    root->left->right = root;
+    root->left = nullptr;
+    root->right = nullptr;
+    return new_root;
+  }
+};
