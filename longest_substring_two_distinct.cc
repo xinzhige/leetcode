8bb336f6f3560985f15998a91b0f92254aec2b6f
Zhige Xin <xinzhige8@gmail.com>
2017-06-29 22:45:10 -0700

    Add a hash table method

--- longest_substring_two_distinct.cc
+++ longest_substring_two_distinct.cc
@@ -17,3 +17,26 @@ public:
     return max(n - l, len);  
   }
 };
+
+
+// Hash table
+class Solution {
+public:
+  int lengthOfLongestSubstringTwoDistinct(string s) {
+    int result = 0;
+    int left = 0;
+    unordered_map<char, int> mymap;
+    for (int i = 0; i < s.size(); ++i) {
+      mymap[s[i]] += 1;
+      while (mymap.size() > 2) {
+        mymap[s[left]] -= 1; 
+        if (mymap[s[left]] == 0) {
+          mymap.erase(s[left]); 
+        }
+        left += 1;
+      }
+      result = max(result, i - left + 1);
+    }
+    return result;
+  }
+};
