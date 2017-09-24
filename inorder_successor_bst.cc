2bf2e585b10955b7857c84e3c0bee7c2e713ee3b
Zhige Xin <xinzhige8@gmail.com>
2017-09-23 17:05:09 -0700

    Add iterative algorithm

--- inorder_successor_bst.cc
+++ inorder_successor_bst.cc
@@ -9,15 +9,32 @@
  */
 class Solution {
 public:
-    TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p) {
-        if (root == nullptr) {
-            return nullptr; 
-        }
-        if (root->val <= p->val) {
-            return inorderSuccessor(root->right, p);
-        } else {
-            auto curr_left = inorderSuccessor(root->left, p);
-            return curr_left == nullptr ? root : curr_left;
-        }
+  TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p) {
+    if (root == nullptr) {
+      return nullptr; 
     }
+    if (root->val <= p->val) {
+      return inorderSuccessor(root->right, p);
+    } else {
+      auto curr = inorderSuccessor(root->left, p);
+      return curr == nullptr ? root : curr;
+    }
+  }
+};
+
+// iterative
+class Solution {
+public:
+  TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p) {
+    TreeNode *result = nullptr;
+    while (root) {
+      if (root->val <= p->val) {
+        root = root->right;
+      } else {
+        result = root;
+        root = root->left;
+      }
+    }
+    return result;
+  }
 };
