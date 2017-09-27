b7b0a0eec1ac208348248df5a36b2d9439c16434
Zhige Xin <xinzhige8@gmail.com>
2017-09-26 19:42:58 -0700

    Add another algorithm

--- is_valid_bst.cc
+++ is_valid_bst.cc
@@ -4,7 +4,7 @@ public:
     bool isValidBST(TreeNode *root) {
         TreeNode* prev = nullptr;
         return  validate(root, prev);
-    } 
+    }
 
 private:
     bool validate(TreeNode* root, TreeNode* &prev) {
@@ -21,3 +21,20 @@ private:
         return validate(root->right, prev);
     }
 };
+
+// In C
+int validate(node* root, node* minNode, node* maxNode) {
+  if(root == NULL) {
+    return 1;
+  }
+  if((minNode && root->val <= minNode->val) ||
+     (maxNode && root->val >= maxNode->val)) {
+    return 0;
+  }
+  return validate(root->left, minNode, root) &&
+    validate(root->right, root, maxNode);
+}
+
+int isValidBST(node* root) {
+  return validate(root, NULL, NULL);
+}
