6bdd95f9d6c75c905a8c7b9d91555ec3a29ebc0f
Zhige Xin <xinzhige8@gmail.com>
2017-09-27 18:55:13 -0700

    Determine if the two retangles overlap before computing

--- rectangle_area.cc
+++ rectangle_area.cc
@@ -1,17 +1,11 @@
 class Solution {
 public:
-    int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
-        int overlap = 0;
-        if(C < E || G < A || D < F || H < B){
-            overlap = 0;
-        } else {
-            vector<int> x = {A, C, E, G};
-            vector<int> y = {B, D, F, H};
-            sort(x.begin(), x.end());
-            sort(y.begin(), y.end());
-            overlap = (x[2] - x[1]) * (y[2] - y[1]);
-        }
-        int combined = (C - A) * (D - B) + (G - E) * (H - F);
-        return combined - overlap;
+  int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
+    int sum = (C - A) * (D - B) + (H - F) * (G - E);
+    if (E >= C || F >= D || B >= H || A >= G) {
+      return sum;
     }
+    int overlapping = (min(G, C) - max(A, E)) * (min(D, H) - max(B, F));
+    return sum - overlapping;
+  }
 };
