a52420d1bf6aee667bc7b3e2083cce03192e4a5c
Zhige Xin <xinzhige8@gmail.com>
2017-10-03 11:44:58 -0700

    Modularize the code

--- sort_list.cc
+++ sort_list.cc
@@ -8,36 +8,45 @@
  */
 class Solution {
 public:
-    ListNode* sortList(ListNode* head) {
-        if (head == nullptr || head->next == nullptr) {
-            return head; 
-        }
-        auto slow = head;
-        auto fast = head;
-        while (fast->next && fast->next->next) {
-            slow = slow->next;
-            fast = fast->next->next;
-        }
-        fast = slow;
-        slow = fast->next;
-        fast->next = nullptr;
-        return mergeLists(sortList(head), sortList(slow));
+  ListNode* sortList(ListNode* head) {
+    if (head == nullptr || head->next == nullptr) {
+      return head;
     }
+    ListNode *mid = midNode(head);
+    return mergeTwoLists(sortList(head), sortList(mid));
+  }
+
 private:
-    ListNode* mergeLists(ListNode *l1, ListNode *l2) {
-        ListNode dummy(0);
-        auto curr = &dummy;
-        while (l1 != nullptr && l2 != nullptr) {
-            if (l1->val < l2->val) {
-                curr->next = l1;
-                l1 = l1->next;
-            } else {
-                curr->next = l2;
-                l2 = l2->next;
-            } 
-            curr = curr->next;
-        }
-        curr->next = (l1 != nullptr) ? l1 : l2;
-        return dummy.next;
+  ListNode* midNode(ListNode* head) {
+    if (head == nullptr) {
+      return nullptr;
     }
+    auto slow = head;
+    auto fast = head;
+    while (fast->next && fast->next->next) {
+      slow = slow->next;
+      fast = fast->next->next;
+    }
+    fast = slow;
+    ListNode* mid = slow->next;
+    fast->next = nullptr;
+    return mid;
+  }
+
+  ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+    ListNode dummy{0};
+    auto curr = &dummy;
+    while (l1 && l2) {
+      if (l1->val < l2->val) {
+        curr->next = l1;
+        l1 = l1->next;
+      } else {
+        curr->next = l2;
+        l2 = l2->next;
+      }
+      curr = curr->next;
+    }
+    curr->next = l1 ? l1 : l2;
+    return dummy.next;
+  }
 };
