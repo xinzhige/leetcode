14f80b527371503d9e215d9091a796e5f4a348a0
Zhige Xin <xinzhige8@gmail.com>
2017-10-04 13:35:36 -0700

    Add one more algorithm

--- permutations2.cc
+++ permutations2.cc
@@ -8,21 +8,45 @@ public:
     perm_helper(num, used, path, result);
     return result;
   }
+private:
   void perm_helper(vector<int> &num, vector<bool> &used, vector<int> &path, vector<vector<int> > &result) {
     if (num.size() == path.size()) {
       result.push_back(path);
       return;
     }
     for (int i = 0; i < num.size(); ++i) {
-      if (used[i] || 
-	  (i != 0 && num[i] == num[i-1] && used[i-1])) {  // avoid the same ones after certain number used
-	continue;
+      if (!used[i]) {
+        if (i != 0 && num[i] == num[i-1] && used[i-1]) {
+          continue;
+        }
+        used[i] = true;
+        path.push_back(num[i]);
+        perm_helper(num, used, path, result);
+        used[i] = false;
+        path.pop_back();
       }
-      used[i] = true;
-      path.push_back(num[i]);
-      perm_helper(num, used, path, result);
-      used[i] = false;
-      path.pop_back();
+    }
+  }
+};
+
+
+// With set
+class Solution {
+public:
+  vector<vector<int>> permuteUnique(vector<int>& nums) {
+    set<vector<int>> res;
+    permute(nums, 0, res);
+    return vector<vector<int>> (res.begin(), res.end());
+  }
+  void permute(vector<int> &nums, int start, set<vector<int>> &res) {
+    if (start >= nums.size()) res.insert(nums);
+    for (int i = start; i < nums.size(); ++i) {
+      if (i != start && nums[i] == nums[start]) {
+        continue;
+      }
+      swap(nums[i], nums[start]);
+      permute(nums, start + 1, res);
+      swap(nums[i], nums[start]);
     }
   }
 };
