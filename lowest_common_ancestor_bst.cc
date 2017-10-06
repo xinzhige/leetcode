bbdd2285e50d0c8c78e19128533cf1c925fc782e
Zhige Xin <xinzhige8@gmail.com>
2017-10-05 21:36:55 -0700

    Minor change

--- lowest_common_ancestor_bst.cc
+++ lowest_common_ancestor_bst.cc
@@ -9,22 +9,19 @@
  */
 class Solution {
 public:
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if (p == nullptr || q == nullptr) {
-            return nullptr 
-        }
-        if (p->val > q->val) {
-            swap(p, q); 
-        }
-        while (root != nullptr) {
-            if (root->val < p->val) {
-                root = root->right;
-            } else if (root->val > q->val) {
-                root = root->left;
-            } else {
-                break;
-            }
-        }
-        return root;
+  TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    if (p->val > q->val) {
+      swap(p, q); 
     }
+    while (root) {
+      if (root->val < p->val) {
+        root = root->right;
+      } else if (root->val > q->val) {
+        root = root->left;
+      } else {
+        break;
+      }
+    }
+    return root;
+  }
 };
