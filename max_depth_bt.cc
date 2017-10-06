8804081cd5eacde8b3fa9f0b3b26ec8670d8b920
Zhige Xin <xinzhige8@gmail.com>
2017-10-05 20:49:19 -0700

    Add a iterative algorithm

--- max_depth_bt.cc
+++ max_depth_bt.cc
@@ -1,3 +1,4 @@
+// Recursive
 class Solution {
 public:
   int maxDepth(TreeNode *root) {
@@ -5,5 +6,34 @@ public:
       return 0;
     }
     return 1 + max(maxDepth(root->left), maxDepth(root->right));
+  }
+};
+
+// Iterative
+class Solution {
+public:
+  int maxDepth(TreeNode *root) {
+    if (root == nullptr) {
+      return 0;
+    } else {
+      root->val = 1;
+    }
+    queue<TreeNode *> q;
+    q.push(root);
+    while (!q.empty()) {
+      TreeNode *curr = q.front();
+      q.pop();
+      if (q.empty() && curr->left == nullptr && curr->right == nullptr) {
+        return curr->val;
+      }
+      if (curr->left != nullptr) {
+        curr->left->val = curr->val + 1;
+        q.push(curr->left);
+      }
+      if (curr->right != nullptr) {
+        curr->right->val = curr->val + 1;
+        q.push(curr->right);
+      }
+    }
   }
 };
