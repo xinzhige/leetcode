4654d82c4ebd97544b5eea5a0c576e14774add11
Zhige Xin <xinzhige8@gmail.com>
2017-10-05 21:22:25 -0700

    Change stack into queue

--- invert_tree.cc
+++ invert_tree.cc
@@ -1,37 +1,37 @@
 // Recursive: invert left tree and store it then invert right tree and assign it to left child lastly assign the temp to right child
 class Solution {
 public:
-    TreeNode* invertTree(TreeNode* root) {
-        if (root == nullptr) {
-            return root;	    
-        } 
-        swap(root->left, root->right); 
-        invertTree(root->left);
-        invertTree(root->right);
-        return root; 
+  TreeNode* invertTree(TreeNode* root) {
+    if (root == nullptr) {
+      return root;
     }
+    swap(root->left, root->right);
+    invertTree(root->left);
+    invertTree(root->right);
+    return root;
+  }
 };
 
-// Non-recursive
+// Iterative
 class Solution {
 public:
-    TreeNode* invertTree(TreeNode* root) {
-        if (root == nullptr) {
-            return root;
-        }
-        stack<TreeNode *> mystack;
-        mystack.emplace(root);
-        while (!mystack.empty()) {
-            TreeNode* curr = mystack.top();
-            mystack.pop();
-            swap(curr->left, curr->right);
-            if (curr->left != nullptr) {
-                mystack.emplace(curr->left);
-            }
-            if (curr->right != nullptr) {
-                mystack.emplace(curr->right);
-            }
-        }
-        return root;
+  TreeNode* invertTree(TreeNode* root) {
+    if (root == nullptr) {
+      return root;
     }
+    queue<TreeNode *> q;
+    q.push(root);
+    while (!q.empty()) {
+      TreeNode* curr = q.front();
+      q.pop();
+      swap(curr->left, curr->right);
+      if (curr->left != nullptr) {
+        q.push(curr->left);
+      }
+      if (curr->right != nullptr) {
+        q.push(curr->right);
+      }
+    }
+    return root;
+  }
 };
