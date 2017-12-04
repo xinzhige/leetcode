6fa6239dc1d1adaca552c7f2fda0e57bd6baf4cf
Zhige Xin <xinzhige8@gmail.com>
2017-12-03 16:05:30 -0800

    Refactor the code

--- matrix_multiplication.cc
+++ matrix_multiplication.cc
@@ -1,21 +1,19 @@
 class Solution {
 public:
-    vector<vector<int>> multiply(vector<vector<int>>& A, vector<vector<int>>& B) {
-        int m = A.size();            
-        int l = A[0].size();
-        int n = B[0].size(); 
-        vector<vector<int>> C(m, vector<int>(n, 0));
-        for (int i = 0; i < m; ++i) {
-            for (int k = 0; k < l; ++k) {
-                if (A[i][k] != 0) {
-                    for (int j = 0; j < n; ++j) {
-                        if (B[k][j] != 0) {
-                            C[i][j] += A[i][k] * B[k][j]; 
-                        } 
-                    } 
-                } 
-            }
+  vector<vector<int>> multiply(vector<vector<int>>& A, vector<vector<int>>& B) {
+    int m = A.size();
+    int l = A[0].size();
+    int n = B[0].size();
+    vector<vector<int>> C(m, vector<int>(n, 0));
+    for (int i = 0; i < m; ++i) {
+      for (int j = 0; j < n; ++j) {
+        for (int k = 0; k < l; ++k) {
+          if (A[i][k] != 0 && B[k][j] != 0) {
+            C[i][j] += A[i][k] * B[k][j];
+          }
         }
-        return C;
+      }
     }
+    return C;
+  }
 };
