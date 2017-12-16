c6d3ccd7a5e617656f7a423ce085a29968c86c13
Zhige Xin <xinzhige8@gmail.com>
2017-12-15 17:32:01 -0800

    Add 482.License Key Formatting

---
+++ cpp/license_key_formatting.cc
@@ -0,0 +1,16 @@
+class Solution {
+public:
+  string licenseKeyFormatting(string S, int K) {
+    string result;
+    for (auto it = S.rbegin(); it != S.rend(); ++it) {
+      if ((*it) != '-') {
+        if ((result.size() % (K + 1))  == K) {
+          result += '-';
+        }
+        result += toupper((*it));
+      }
+    }
+    reverse(result.begin(), result.end());
+    return result;
+  }
+};
