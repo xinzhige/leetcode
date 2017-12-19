5938bf72ea2cbf3c905026c13983df95e8755ea9
Zhige Xin <xinzhige8@gmail.com>
2017-12-19 14:59:49 -0800

    Change variables names

--- cpp/search_rotated_array2.cc
+++ cpp/search_rotated_array2.cc
@@ -1,30 +1,30 @@
 class Solution {
 public:
-    bool search(vector<int>& nums, int target) {
-        int left = 0;
-        int right = n - 1;
-        int mid = 0;
-        while (left <= right) {
-            mid = left + ((right - left) / 2);
-            if (A[mid] == target) {
-                return true;    
-            }
-            if (A[left] < A[mid]) {   //left half is sorted
-                if (A[left] <= target && target < A[mid]) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
-            } else if (A[left] > A[mid]) { //right half is sorted
-                if (A[mid] < target && target <= A[right]) {
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
-                }
-            } else {
-                left += 1; // we dont know what the situation is on the left part, for example, one is {3, 3, 3, 3, 0, 1, 2} or {3, 4, 5, 6, 3, 3, 3, 3, 3}
-            }
+  bool search(vector<int>& nums, int target) {
+    int begin = 0;
+    int end = nums.size() - 1;
+    int mid = 0;
+    while (begin <= end) {
+      mid = begin + (end - begin) / 2;
+      if (nums[mid] == target) {
+        return true;
+      } else if (nums[begin] < nums[mid]) {   // The left half is sorted
+        if (nums[begin] <= target && target < nums[mid]) {
+          end = mid - 1;
+        } else {
+          begin = mid + 1;
         }
-        return false;
+      } else if (nums[begin] > nums[mid]) { // The right half is sorted
+        if (nums[mid] < target && target <= nums[end]) {
+          begin = mid + 1;
+        } else {
+          end = mid - 1;
+        }
+      } else {
+        // We dont know what the situation is on the left part, for example, {3, 3, 3, 3, 0, 1, 2} or {3, 4, 5, 6, 3, 3, 3, 3, 3}
+        begin += 1;
+      }
     }
+    return false;
+  }
 };
