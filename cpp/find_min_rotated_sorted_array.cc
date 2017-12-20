adefa2a456b12a49bb692168f3d1ed23f1d0fdfe
Zhige Xin <xinzhige8@gmail.com>
2017-12-20 15:54:48 -0800

    Rename some variables

--- cpp/find_min_rotated_sorted_array.cc
+++ cpp/find_min_rotated_sorted_array.cc
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    int findMin(vector<int> &num) {
-        int left = 0;
-        int right = num.size() - 1;
-        int mid = 0;
-        // Find min left s.t. nums[left] < nums[left + 1]. 
-        while (left < right && nums[left] > nums[right]) {
-            mid = left + ((right - left) / 2);
-            if (nums[mid] < nums[left]) {
-                right = mid;  
-            } else {
-                left = mid + 1; 
-            }
-        }
-        return nums[left];
+  int findMin(vector<int> &num) {
+    int begin = 0;
+    int end = nums.size() - 1;
+    int mid = 0;
+    // Find min begin s.t. nums[begin] < nums[begin + 1].
+    while (begin < end && nums[begin] > nums[end]) {
+      mid = begin + ((end - begin) / 2);
+      if (nums[mid] < nums[begin]) {
+        end = mid;
+      } else {
+        begin = mid + 1;
+      }
     }
+    return nums[begin];
+  }
 };
--- cpp/find_min_rotated_sorted_array2.cc
+++ cpp/find_min_rotated_sorted_array2.cc
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    int findMin(vector<int> &nums) {
-        int left = 0;
-        int right = nums.size() - 1;
-        int mid = 0;
-        while (left < right && nums[left] >= nums[right]) {
-            mid = left + ((right - left) / 2);
-            if (nums[mid] > nums[right]) {
-                left = mid + 1;
-            } else if (nums[mid] < nums[left]) {
-                right = mid;
-            } else {
-                left += 1;
-            }
-        }
-        return nums[left];
+  int findMin(vector<int> &nums) {
+    int begin = 0;
+    int end = nums.size() - 1;
+    int mid = 0;
+    while (begin < end && nums[begin] >= nums[end]) {
+      mid = begin + (end - begin) / 2;
+      if (nums[mid] > nums[end]) {
+        begin = mid + 1;
+      } else if (nums[mid] < nums[begin]) {
+        end = mid;
+      } else {
+        begin += 1;
+      }
     }
+    return nums[begin];
+  }
 };
