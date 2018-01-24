6852c2ce9eb1549a423e5f2f94c7422ac39038eb
Zhige Xin <xinzhige8@gmail.com>
2018-01-23 22:10:14 -0800

    remove one variable

--- cpp/can_place_flowers.cc
+++ cpp/can_place_flowers.cc
@@ -2,15 +2,14 @@ class Solution {
 public:
   bool canPlaceFlowers(vector<int>& flowerbed, int n) {
     int m= flowerbed.size();
-    int num = 0;
     for (int i = 0; i < m; ++i) {
       if (flowerbed[i] == 0 && (i == 0 || flowerbed[i-1] == 0) &&
           (i == m - 1 || flowerbed[i+1] == 0)) {
         flowerbed[i] = 0;
         ++i;
-        ++num;
+        -n;
       }
-      if (num >= n) {
+      if (n <= 0) {
         return true;
       }
     }
