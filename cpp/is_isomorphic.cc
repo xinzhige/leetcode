4ed9aa1ec7c5a9a87945423725ccc943d2fcbcd3
Zhige Xin <xinzhige8@gmail.com>
2018-01-25 22:35:47 -0800

    add comments

--- cpp/is_isomorphic.cc
+++ cpp/is_isomorphic.cc
@@ -1,18 +1,20 @@
 class Solution {
 public:
-    bool isIsomorphic(string s, string t) {
-        unordered_map<char, int> mymap;
-        unordered_map<char, int> mymap_reversed;
-        if (s.size() != t.size()) {
-            return false; 
-        }
-        for (int i = 0; i < s.size(); i++) {
-            if (mymap[s[i]] != mymap_reversed[t[i]]) {
-                return false; 
-            } 
-            mymap[s[i]] = i + 1;
-            mymap_reversed[t[i]] = i + 1;
-        }
-        return true;
+  bool isIsomorphic(string s, string t) {
+    unordered_map<char, int> maps;
+    unordered_map<char, int> mapt;
+    if (s.size() != t.size()) {
+      return false;
     }
+    for (int i = 0; i < s.size(); i++) {
+      if (maps[s[i]] != mapt[t[i]]) {
+        return false;
+      }
+      // If the map does not contain current character, the operator [] would
+      // return 0 so use i + 1 instead of i.
+      maps[s[i]] = i + 1;
+      mapt[t[i]] = i + 1;
+    }
+    return true;
+  }
 };
