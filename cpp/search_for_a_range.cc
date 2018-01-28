7292a1480be6ff088990fe56ff7f7a9edf9b9f72
Zhige Xin <xinzhige8@gmail.com>
2018-01-28 15:41:58 -0800

    change the algorithm

---
+++ cpp/search_for_a_range.cc
@@ -0,0 +1,27 @@
+class Solution {
+public:
+  vector<int> searchRange(vector<int>& nums, int target) {
+    vector<int> result{-1, -1};
+    int leftIndex = binarySearch(nums, target, true);
+    if (leftIndex == nums.size() || nums[leftIndex] != target) {
+      return result;
+    }
+    result[0] = leftIndex;
+    result[1] = binarySearch(nums, target, false) - 1;
+    return result;
+  }
+
+  int binarySearch(vector<int>& nums, int target, bool left) {
+    int low = 0;
+    int high = nums.size();
+    while (low < high) {
+      int mid = low + (high - low) / 2;
+      if (nums[mid] > target || (left && target == nums[mid])) {
+        high = mid;
+      } else {
+        low = mid + 1;
+      }
+    }
+    return low;
+  }
+};
