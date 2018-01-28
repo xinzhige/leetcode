da630fd4d2ebee2518b764af8e1be2949bf9bce0
Zhige Xin <xinzhige8@gmail.com>
2018-01-27 17:52:39 -0800

    change some variable names

--- cpp/minimum_window_substring.cc
+++ cpp/minimum_window_substring.cc
@@ -6,40 +6,39 @@ public:
     if (s_len < t_len) {
       return "";
     }
-    const int ASCII_MAX = 256;
-    vector<int> appeared(ASCII_MAX, 0);
-    vector<int> expected(ASCII_MAX, 0);
-    int min_width = INT_MAX;
+    vector<int> appeared(256, 0);
+    vector<int> expected(256, 0);
+    int min_len = INT_MAX;
     int win_begin = 0;
     int min_begin = 0;
     int count = 0;
     for (int i = 0; i < t_len; ++i) {
-      ++expected[T[i]];
+      expected[T[i]] += 1;
     }
     for (int win_end = 0; win_end < s_len; ++win_end) {
       if (expected[S[win_end]] > 0) {
-        ++appeared[S[win_end]];
+        appeared[S[win_end]] += 1;
         if (appeared[S[win_end]] <= expected[S[win_end]]) {
-          ++count;
+          count += 1;
         }
       }
       if (count == t_len) {
         while (appeared[S[win_begin]] > expected[S[win_begin]] ||
                expected[S[win_begin]] == 0) {
-          --appeared[S[win_begin]];
-          ++win_begin;
+          appeared[S[win_begin]] -= 1;
+          win_begin += 1;
         }
-        int window_length = win_end - win_begin + 1;
-        if (min_width > window_length) {
-          min_width = window_length;
+        int win_len = win_end - win_begin + 1;
+        if (win_len < min_len) {
+          min_len = win_len;
           min_begin = win_begin;
         }
       }
     }
-    if (min_width == INT_MAX) {
+    if (min_len == INT_MAX) {
       return "";
     } else {
-      return S.substr(min_begin, min_width);
+      return S.substr(min_begin, min_len);
     }
   }
 };
