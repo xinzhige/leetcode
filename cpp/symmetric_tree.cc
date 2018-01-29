96d6b6e70e546d632474232043af52db04ad3e89
Zhige Xin <xinzhige8@gmail.com>
2018-01-29 15:38:31 -0800

    rename the file

---
+++ cpp/symmetric_tree.cc
@@ -0,0 +1,51 @@
+// Recursive way
+class Solution {
+public:
+  bool isSymmetricHelper(TreeNode* l, TreeNode* r) {
+    if (l == nullptr && r == nullptr) {
+      return true;
+    } else if (l == nullptr || r == nullptr) {
+      return false;
+    } else if (l->val != r->val) {
+      return false;
+    }
+    return isSymmetricHelper(l->left, r->right) &&
+      isSymmetricHelper(l->right, r->left);
+  }
+  bool isSymmetric(TreeNode *root) {
+    if (root == nullptr) {
+      return true;
+    }
+    return isSymmetricHelper(root->left, root->right);
+  }
+};
+
+// Iterative way by queue
+class Solution {
+public:
+  bool isSymmetric(TreeNode *root) {
+    if (root == nullptr) {
+      return true;
+    }
+    queue<TreeNode *> q;
+    q.push(root->left);
+    q.push(root->right);
+    while (!q.empty()) {
+      auto l = q.front();
+      q.pop();
+      auto r = q.front();
+      q.pop();
+      if (l == nullptr && r == nullptr) {
+        continue;
+      } else if (l == nullptr || r == nullptr ||
+          (l->val != r->val)) {
+        return false;
+      }
+      q.push(l->left);
+      q.push(r->right);
+      q.push(l->right);
+      q.push(r->left);
+    }
+    return true;
+  }
+};
