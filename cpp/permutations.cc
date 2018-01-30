839ddf0796a0d122ba5c52df84876cc2ce470aa4
Zhige Xin <xinzhige8@gmail.com>
2018-01-30 01:33:40 -0800

    delete the first method

--- cpp/permutations.cc
+++ cpp/permutations.cc
@@ -1,38 +1,3 @@
-// Space: O(n)
-class Solution {
-public:
-  vector<vector<int> > permute(vector<int> &num) {
-    vector<vector<int> > result;
-    vector<int> tmp;
-    dfs(num, tmp, result);
-    return result;
-  }
-
-private:
-  bool contains(vector<int> &a, int x) {
-    for (auto &i : a) {
-      if (i == x) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  void dfs(vector<int>& nums, vector<int> tmp, vector<vector<int>> &result) {
-    if (tmp.size() == nums.size()) {
-      result.emplace_back(tmp);
-    } else {
-      for (int i = 0; i < nums.size(); ++i) {
-        if (!contains(tmp, nums[i])) {
-          tmp.emplace_back(nums[i]);
-          dfs(nums, tmp, result);
-          tmp.pop_back();
-        }
-      }
-    }
-  }
-};
-
 // Space: O(1)
 class Solution {
 public:
