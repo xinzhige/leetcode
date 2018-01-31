683f48267e59f7502dd24abdcd45e2275e615d74
Zhige Xin <xinzhige8@gmail.com>
2018-01-31 02:36:18 -0800

    update the algorithm

--- cpp/maximum_product_subarray.cc
+++ cpp/maximum_product_subarray.cc
@@ -1,18 +1,16 @@
 class Solution {
 public:
   int maxProduct(vector<int>& nums) {
-    int len = nums.size();
-    if (len == 0) {
-      return INT_MIN;
-    }
     int result = nums[0];
-    int curr_max = nums[0];
-    int curr_min = nums[0];
-    for (int i = 1; i < len; ++i) {
-      int tmp_max = curr_max;
-      curr_max = max(max(nums[i] * tmp_max, nums[i]), nums[i] * curr_min);
-      curr_min = min(min(nums[i] * tmp_max, nums[i]), nums[i] * curr_min);
-      result = max(curr_max, result);
+    int currMax = nums[0];
+    int currMin = nums[0];
+    for (int i = 1; i < nums.size(); ++i) {
+      if (nums[i] < 0) {
+        swap(currMax, currMin);
+      }
+      currMax = max(nums[i], nums[i] * currMax);
+      currMin = min(nums[i], nums[i] * currMin);
+      result = max(currMax, result);
     }
     return result;
   }
