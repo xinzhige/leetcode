c4429cfc51bb038f8852b97633cefc922f892e05
Zhige Xin <xinzhige8@gmail.com>
2018-02-05 14:07:56 -0800

    add Manacher's Algorithm

--- cpp/palindromic_substrings.cc
+++ cpp/palindromic_substrings.cc
@@ -1,9 +1,7 @@
+// Time: O(n^2), space: O(1)
 class Solution {
 public:
   int countSubstrings(string s) {
-    if (s.size() == 0) {
-      return 0;
-    }
     int count = 0;
     for (int i = 0; i < s.size(); i++) {
       extendPalindrome(s, i, i, count);
@@ -13,27 +11,44 @@ public:
   }
   void extendPalindrome(string s, int left, int right, int& count) {
     while (left >= 0 && right < s.size() && s[left] == s[right]) {
-      ++count;
-      --left;
-      ++right;
+      count += 1;
+      left -= 1;
+      right += 1;
     }
   }
 };
 
-// DP
+// Manacher's Algorithm
 class Solution {
 public:
   int countSubstrings(string s) {
-    int n = s.size();
-    int result = 0;
-    vector<vector<bool>> dp(n, vector<bool>(n, false));
-    for (int i = n - 1; i >= 0; --i) {
-      for (int j = i; j < n; ++j) {
-        dp[i][j] = (s[i] == s[j]) && (j - i <= 2 || dp[i + 1][j - 1]);
-        if (dp[i][j]) {
-          ++result;
-        }
+    string str(2 * s.size() + 3, ' ');
+    str[0] = '@';
+    str[1] = '#';
+    str[str.size() - 1] = '$';
+    int t = 2;
+    for (auto &c : s) {
+      str[t++] = c;
+      str[t++] = '#';
+    }
+    int center = 0;
+    int right = 0;
+    vector<int> dist(str.size(), 0);
+    for (int i = 1; i < dist.size() - 1; ++i) {
+      if (i < right) {
+        dist[i] = min(dist[2 * center - i], right - i);
       }
+      while (str[i + dist[i] + 1] == str[i - dist[i] - 1]) {
+        dist[i] += 1;
+      }
+      if (i + dist[i] > right) {
+        right = i + dist[i];
+        center = i;
+      }
+    }
+    int result = 0;
+    for (const auto &d : dist) {
+      result += (d + 1) / 2;
     }
     return result;
   }
