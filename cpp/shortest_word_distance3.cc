4174d139b53ed30acf307fa3064445292b238d08
Zhige Xin <xinzhige8@gmail.com>
2018-02-13 18:13:15 -0800

    add comments

--- cpp/shortest_word_distance3.cc
+++ cpp/shortest_word_distance3.cc
@@ -1,8 +1,8 @@
 class Solution {
 public:
   int shortestWordDistance(vector<string>& words, string word1, string word2) {
-    // Initialize index1 and index2 to be the values such that the value of
-    // dist computed first to be large enough (words length).
+    // Initialize index1 and index2 to be the values such that the first
+    // distance computed to be large enough (words length).
     int index1 = words.size();
     int index2 = -words.size();
     int dist = INT_MAX;
