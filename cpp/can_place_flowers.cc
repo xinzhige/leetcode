40f8ad6c461fa28a1aa5241bc7248bcfd831fac7
Zhige Xin <xinzhige8@gmail.com>
2018-02-13 18:49:40 -0800

    delete a space

--- cpp/can_place_flowers.cc
+++ cpp/can_place_flowers.cc
@@ -1,14 +1,14 @@
 class Solution {
 public:
   bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-    int m= flowerbed.size();
+    int m = flowerbed.size();
     for (int i = 0; i < m; ++i) {
       if (flowerbed[i] == 0 && (i == 0 || flowerbed[i-1] == 0) &&
           (i == m - 1 || flowerbed[i+1] == 0)) {
-        flowerbed[i] = 0;
         ++i;
         -n;
       }
+      // Use <= instead of == in case n==0.
       if (n <= 0) {
         return true;
       }
