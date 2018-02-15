f80cedbc68245c811077705bc27910a500ef97dd
Zhige Xin <xinzhige8@gmail.com>
2018-02-14 21:04:50 -0800

    use iterator

--- cpp/house_robber.cc
+++ cpp/house_robber.cc
@@ -1,10 +1,10 @@
 class Solution {
 public:
   int rob(vector<int>& nums) {
-    int prev_max = 0; 
+    int prev_max = 0;
     int curr_max = 0;
-    for (int i = 0; i < nums.size(); i++) {
-      int curr = max(curr_max, prev_max + nums[i]);
+    for (auto &n : nums) {
+      int curr = max(curr_max, prev_max + n);
       prev_max = curr_max;
       curr_max = curr;
     }
