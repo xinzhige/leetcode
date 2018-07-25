ff83bbcaba1f37897148de0058998d582e539a6b
Isaiah Xin <xinzhige8@gmail.com>
2018-07-24 19:01:35 -0700

    Add comments

--- cpp/best_time_buy_sell_stock_cooldown.cc
+++ cpp/best_time_buy_sell_stock_cooldown.cc
@@ -4,6 +4,8 @@ public:
   int maxProfit(vector<int>& prices) {
     int n = prices.size();
     vector<vector<int>> dp(n + 1, vector<int>(3, 0));
+    // Note: the buy must be initilized to be minimum such that
+    // the first buy can be made
     dp[0][0] = INT_MIN;
     for (int i = 1; i <= n; ++i) {
       dp[i][0] = max(dp[i-1][2] - prices[i-1], dp[i-1][0]);
