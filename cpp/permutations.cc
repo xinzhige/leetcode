5b9bb63a1fc8a1567874bafb04b50750b270e543
Zhige Xin <xinzhige8@gmail.com>
2018-08-07 14:59:32 -0700

    Rename variables

--- cpp/permutations.cc
+++ cpp/permutations.cc
@@ -1,19 +1,19 @@
 // Time: O(n!), space: O(1)
 class Solution {
 public:
-  vector<vector<int> > permute(vector<int> &num) {
-    vector<vector<int> > result;
-    dfs(num, 0, result);
+  vector<vector<int>> permute(vector<int> &nums) {
+    vector<vector<int>> result;
+    dfs(nums, 0, result);
     return result;
   }
-  void dfs(vector<int> &num, int pos, vector<vector<int>> &result) {
-    if(pos == num.size()) {
-      result.push_back(num);
+  void dfs(vector<int> &nums, int pos, vector<vector<int>> &result) {
+    if(pos == nums.size()) {
+      result.push_back(nums);
     } else {
-      for(int i = pos; i < num.size(); ++i) {
-        swap(num[i], num[pos]);
-        dfs(num, pos+1, result);
-        swap(num[i], num[pos]);
+      for(int i = pos; i < nums.size(); ++i) {
+        swap(nums[i], nums[pos]);
+        dfs(nums, pos + 1, result);
+        swap(nums[i], nums[pos]);
       }
     }
   }
