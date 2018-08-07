3f04b4dcb5b0cb34560e4b8452461e630809cb04
Zhige Xin <isaiah@MacBookPro.local>
2018-08-06 21:51:15 -0700

    Change the names of parameters

--- cpp/subsets.cc
+++ cpp/subsets.cc
@@ -1,37 +1,37 @@
+// Recursive
 class Solution {
 public:
-  vector<vector<int>> subsets(vector<int> &S) {
-    vector<vector<int>> result(1);
-    sort(S.begin(), S.end());
-    for (int i = 0; i < S.size(); ++i) {
-      int size = result.size();
-      for (int j = 0; j < size; ++j) {
-	result.emplace_back(result[j]);
-	result.back().emplace_back(S[i]);
-      }
-    }
-    return result;
-  }
-};
-
-// DFS
-class Solution {
-public:
-  vector<vector<int>> subsets(vector<int> &S) {
+  vector<vector<int>> subsets(vector<int> &nums) {
     vector<vector<int>> result;
-    vector<int> out;
-    sort(S.begin(), S.end());
-    getSubsets(S, 0, out, result);
+    vector<int> sub;
+    dfs(nums, 0, sub, result);
     return result;
   }
 
 private:
-  void getSubsets(vector<int> &S, int pos, vector<int> &out, vector<vector<int> > &result) {
-    result.emplace_back(out);
-    for (int i = pos; i < S.size(); ++i) {
-      out.emplace_back(S[i]);
-      getSubsets(S, i + 1, out, result);
-      out.pop_back();
+  void dfs(vector<int> &nums, int start, vector<int> &sub, vector<vector<int>> &result) {
+    result.push_back(sub);
+    for (int i = start; i < nums.size(); ++i) {
+      sub.push_back(nums[i]);
+      dfs(nums, i + 1, sub, result);
+      sub.pop_back();
     }
+  }
+};
+
+
+// Iterative
+class Solution {
+public:
+  vector<vector<int>> subsets(vector<int> &nums) {
+    vector<vector<int>> result(1);
+    for (const auto &num : nums) {
+      int n = result.size();
+      for (int i = 0; i < n; ++i) {
+        result.push_back(result[i]);
+        result.back().push_back(num);
+      }
+    }
+    return result;
   }
 };
