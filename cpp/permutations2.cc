427bef4e8bde084690707d391982287089170fc3
Zhige Xin <xinzhige8@gmail.com>
2018-08-07 18:49:25 -0700

    Delete unnecessary code

--- cpp/permutations2.cc
+++ cpp/permutations2.cc
@@ -20,27 +20,3 @@ private:
     }
   }
 };
-
-
-// With set
-class Solution {
-public:
-  vector<vector<int>> permuteUnique(vector<int>& nums) {
-    set<vector<int>> result;
-    permute(nums, 0, result);
-    return vector<vector<int>> (result.begin(), result.end());
-  }
-  void permute(vector<int> &nums, int start, set<vector<int>> &result) {
-    if (start >= nums.size()) {
-      result.insert(nums);
-    }
-    for (int i = start; i < nums.size(); ++i) {
-      if (i != start && nums[i] == nums[start]) {
-        continue;
-      }
-      swap(nums[i], nums[start]);
-      permute(nums, start + 1, result);
-      swap(nums[i], nums[start]);
-    }
-  }
-};
