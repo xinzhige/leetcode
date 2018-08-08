e279800c1fd0f35aa183f8b388c323ac6ed2e1c2
Zhige Xin <xinzhige8@gmail.com>
2018-08-07 18:45:56 -0700

    Delete unnecessary code

--- cpp/permutations2.cc
+++ cpp/permutations2.cc
@@ -1,29 +1,21 @@
 class Solution {
 public:
-  vector<vector<int> > permuteUnique(vector<int> &num) {
-    vector<bool> used(num.size(), false);
-    vector<int> path;
-    vector<vector<int> > result;
-    sort(num.begin(), num.end());
-    permuteHelper(num, used, path, result);
+  vector<vector<int> > permuteUnique(vector<int> &nums) {
+    vector<vector<int>> result;
+    sort(nums.begin(), nums.end());
+    dfs(nums, 0, result);
     return result;
   }
 private:
-  void permuteHelper(vector<int> &num, vector<bool> &used, vector<int> &path, vector<vector<int> > &result) {
-    if (num.size() == path.size()) {
-      result.push_back(path);
-      return;
-    }
-    for (int i = 0; i < num.size(); ++i) {
-      if (!used[i]) {
-        if (i != 0 && num[i] == num[i-1] && used[i-1]) {
-          continue;
+  void dfs(vector<int> nums, int start, vector<vector<int> > &result) {
+    if (start == nums.size() - 1) {
+      result.push_back(nums);
+    } else {
+      for (int i = start; i < nums.size(); ++i) {
+        if (i == start || nums[i] != nums[start]) {
+          swap(nums[i], nums[start]);
+          dfs(nums, start + 1, result);
         }
-        used[i] = true;
-        path.push_back(num[i]);
-        permuteHelper(num, used, path, result);
-        used[i] = false;
-        path.pop_back();
       }
     }
   }
