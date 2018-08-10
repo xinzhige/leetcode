37feef1e270aeab24dfcea8aac93941599449177
Zhige Xin <xinzhige8@gmail.com>
2018-08-09 22:58:38 -0700

    Add an iterative algorithm

--- cpp/combination.cc
+++ cpp/combination.cc
@@ -1,21 +1,47 @@
+// Backtracking
 class Solution {
 public:
-  vector<vector<int> > combine(int n, int k) {
-    vector<int> temp; 
-    vector<vector<int> > result; 
-    combine_res(n, k, 1, temp, result); 
-    return result; 
+  vector<vector<int>> combine(int n, int k) {
+    vector<int> tmp;
+    vector<vector<int>> result;
+    backtrack(n, k, 1, tmp, result);
+    return result;
   }
 
-  void combine_res(int n, int k, int j, vector<int> temp, vector<vector<int> > &result) { 
-    if(temp.size() == k) { 
-      result.push_back(temp); 
-      return; 
+private:
+  void backtrack(int n, int k, int j, vector<int> tmp,
+                 vector<vector<int>> &result) {
+    if(tmp.size() == k) {
+      result.push_back(tmp);
+    } else {
+      for(int i = j; i <= n; ++i, ++j) {
+        tmp.push_back(i);
+        backtrack(n, k, j + 1, tmp, result);
+        tmp.pop_back();
+      }
     }
-    for(int i = j; i <= n; ++i) {
-      temp.push_back(i);
-      combine_res(n, k, ++j, temp, result);
-      temp.pop_back();
-    }
+  }
+};
+
+
+// Iterative
+class Solution {
+public:
+  vector<vector<int>> combine(int n, int k) {
+    vector<vector<int>> result;
+		int i = 0;
+		vector<int> comb(k, 0);
+		while (i >= 0) {
+			comb[i] += 1;
+			if (comb[i] > n) {
+        i -= 1;
+      } else if (i == k - 1) {
+        result.push_back(comb);
+      } else {
+        i += 1;
+        comb[i] = comb[i - 1];
+			}
+		}
+		return result;
   }
 };
