2ff0247ed7b0c3ef79c9f2651e54e4efe92c8ab4
Zhige Xin <xinzhige8@gmail.com>
2018-08-09 17:04:33 -0700

    Rename the variables

--- cpp/combination_sum3.cc
+++ cpp/combination_sum3.cc
@@ -1,22 +1,26 @@
 class Solution {
 public:
-  vector<vector<int> > combinationSum3(int k, int n) {
+  vector<vector<int>> combinationSum3(int k, int n) {
     vector<vector<int>> result;
-    vector<int> combination;
-    combinationSum3(result, combination, 1, k, n);
+    vector<int> comb;
+    combinationSum3(result, comb, 1, k, n);
     return result;
   }
+
 private:
-  void combinationSum3(vector<vector<int>> &res, vector<int> &combination, int start, int k, int n) {
+  void combinationSum3(vector<vector<int>> &result, vector<int> &comb,
+                       int start, int k, int n) {
     if (k == 0 && n == 0) {
-      res.push_back(combination);
-      return;
-    }
-    for (int i = start; i <= num; ++i) {
-      combination.emplace_back(i);
-      combinationSum3(res, combination, i + 1, k - 1, n - i);
-      combination.pop_back();
+      result.push_back(comb);
+    } else {
+      for (int i = start; i <= NUM; ++i) {
+        comb.push_back(i);
+        combinationSum3(result, comb, i + 1, k - 1, n - i);
+        comb.pop_back();
+      }
     }
   }
-  const int num = 9;
+
+private:
+  const int NUM = 9;
 };
