30134ceb803377be3c4c0fee0d4529c86b0fb3d9
Zhige Xin <xinzhige8@gmail.com>
2018-08-23 15:42:56 -0700

    Delete the unnecessary variable

--- cpp/coin_change.cc
+++ cpp/coin_change.cc
@@ -1,8 +1,7 @@
 class Solution {
 public:
   int coinChange(vector<int>& coins, int amount) {
-    int currMax = amount + 1;
-    vector<int> dp(amount + 1, currMax);
+    vector<int> dp(amount + 1, amount + 1);
     dp[0] = 0;
     for (int i = 1; i <= amount; ++i) {
       for (int j = 0; j < coins.size(); ++j) {
@@ -11,6 +10,6 @@ public:
         }
       }
     }
-    return dp[amount] > amount ? -1 : dp[amount];
+    return dp[amount] == amount + 1 ? -1 : dp[amount];
   }
 };
