dcb0d550a2b0d78c674d38a5cd984aea6198c58c
Zhige Xin <xinzhige8@gmail.com>
2018-09-03 17:33:00 -0700

    Add 348.Design Tic-Tac-Toe

---
+++ cpp/design_tic_tac_toe.cc
@@ -0,0 +1,28 @@
+class TicTacToe {
+public:
+  /** Initialize your data structure here. */
+  TicTacToe(int n): rows(n), cols(n), N(n), diag(0), anti_diag(0) {}
+
+  /** Player {player} makes a move at ({row}, {col}).
+      @param row The row of the board.
+      @param col The column of the board.
+      @param player The player, can be either 1 or 2.
+      @return The current winning condition, can be either:
+      0: No one wins.
+      1: Player 1 wins.
+      2: Player 2 wins. */
+
+  int move(int row, int col, int player) {
+    int add = (player == 1 ? 1 : -1);
+    rows[row] += add;
+    cols[col] += add;
+    diag += (row == col ? add : 0);
+    anti_diag += (row == N - col - 1 ? add : 0);
+    return (abs(rows[row]) == N || abs(cols[col]) == N || abs(diag) == N ||
+            abs(anti_diag) == N) ? player : 0;
+  }
+
+private:
+  vector<int> rows, cols;
+  int diag, anti_diag, N;
+};
