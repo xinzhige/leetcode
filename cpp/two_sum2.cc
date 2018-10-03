7903f0df40fd4a5c625fd0b85f5fb1af162670dd
Zhige Xin <xinzhige8@gmail.com>
2018-10-03 09:02:02 -0700

    Change the format

--- cpp/two_sum2.cc
+++ cpp/two_sum2.cc
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& numbers, int target) {
-        int head = 0;
-        int tail = numbers.size() - 1;
-        vector<int> result;
-        int curr_sum = 0;
-        while (head < tail) {
-            curr_sum = numbers[head] + numbers[tail]; 
-            if (curr_sum > target) {
-                tail -= 1; 
-            } else if (curr_sum < target) {
-                head += 1; 
-            } else {
-                result.push_back(head + 1); 
-                result.push_back(tail + 1); 
-                break;
-            } 
-        }
-        return result;
+  vector<int> twoSum(vector<int>& numbers, int target) {
+    int head = 0;
+    int tail = numbers.size() - 1;
+    vector<int> result;
+    while (head < tail) {
+      int curr_sum = numbers[head] + numbers[tail];
+      if (curr_sum > target) {
+        tail -= 1;
+      } else if (curr_sum < target) {
+        head += 1;
+      } else {
+        result.push_back(head + 1);
+        result.push_back(tail + 1);
+        break;
+      }
     }
+    return result;
+  }
 };
