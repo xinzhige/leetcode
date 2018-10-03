b202fa25c7bcca23e57c98d29a9c0ad5e463c420
Zhige Xin <xinzhige8@gmail.com>
2018-10-03 10:32:16 -0700

    Add 653. Two Sum IV - Input is a BST

---
+++ cpp/two_sum4.cc
@@ -0,0 +1,44 @@
+// Recursive
+class Solution {
+public:
+  bool findTarget(TreeNode* root, int k) {
+    unordered_set<int> nums;
+    return find(root, k, nums);
+  }
+  bool find(TreeNode* root, int k, unordered_set<int> &nums) {
+    if (root == nullptr) {
+      return false;
+    }
+    if (nums.count(k - root->val)) {
+      return true;
+    }
+    nums.insert(root->val);
+    return find(root->left, k, nums) || find(root->right, k, nums);
+  }
+};
+
+
+// Iterative
+class Solution {
+public:
+  bool findTarget(TreeNode* root, int k) {
+    unordered_set<int> nums;
+    queue<TreeNode*> q;
+    q.push(root);
+    while (!q.empty()) {
+      auto curr = q.front();
+      q.pop();
+      if (nums.count(k - curr->val) > 0) {
+        return true;
+      }
+      nums.insert(curr->val);
+      if (curr->left != nullptr) {
+        q.push(curr->left);
+      }
+      if (curr->right != nullptr) {
+        q.push(curr->right);
+      }
+    }
+    return false;
+  }
+};
