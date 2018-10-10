aefebf5d04e9c5fe1cbc3fd23a724239847a00d9
Zhige Xin <xinzhige8@gmail.com>
2018-10-10 10:42:53 -0700

    Clean the code

--- cpp/binary_watch.cc
+++ cpp/binary_watch.cc
@@ -3,11 +3,14 @@ class Solution {
 public:
   vector<string> readBinaryWatch(int num) {
     vector<string> result;
-    for (int h = 0; h < 12; h++) {
-      for (int m = 0; m < 60; m++) {
-        if (bitset<10>(h << 6 | m).count() == num) {
-          result.emplace_back(to_string(h) + (m < 10 ? ":0" : ":") +
-                              to_string(m));
+    if (num >= 0 && num <= 10) {
+      for (int h = 0; h < 12; h++) {
+        for (int m = 0; m < 60; m++) {
+          bitset<10> curr_bits(h << 6 | m);
+          if (curr_bits.count() == num) {
+            result.push_back(to_string(h) + (m < 10 ? ":0" : ":") +
+                                to_string(m));
+          }
         }
       }
     }
@@ -21,17 +24,19 @@ class Solution {
 public:
   vector<string> readBinaryWatch(int num) {
     vector<string> result;
-    vector<int> hour{8, 4, 2, 1};
-    vector<int> minute{32, 16, 8, 4, 2, 1};
-    for (int i = 0; i <= num; ++i) {
-      vector<int> hours = generate(hour, i);
-      vector<int> minutes = generate(minute, num - i);
-      for (const auto &h : hours) {
-        if (h < 12) {
-          for (const auto &m : minutes) {
-            if (m < 60) {
-              result.push_back(to_string(h) + (m < 10 ? ":0" : ":") +
-                               to_string(m));
+    if (num >= 0 && num <= 10) {
+      vector<int> hour{8, 4, 2, 1};
+      vector<int> minute{32, 16, 8, 4, 2, 1};
+      for (int i = 0; i <= num; ++i) {
+        vector<int> hours = generate(hour, i);
+        vector<int> minutes = generate(minute, num - i);
+        for (const auto &h : hours) {
+          if (h < 12) {
+            for (const auto &m : minutes) {
+              if (m < 60) {
+                result.push_back(to_string(h) + (m < 10 ? ":0" : ":") +
+                                 to_string(m));
+              }
             }
           }
         }
