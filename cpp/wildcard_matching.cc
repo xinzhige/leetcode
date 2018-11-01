ca0106eaf8c377ffba8fcafb83aaab664ed09069
Zhige Xin <xinzhige8@gmail.com>
2018-11-01 16:50:54 -0700

    Change the parameters

---
+++ cpp/wildcard_matching.cc
@@ -0,0 +1,31 @@
+class Solution {
+public:
+  bool isMatch(string s, string p) {
+    int m = s.size();
+    int n = p.size();
+    int i = 0;
+    int j = 0;
+    int asterisk = -1;
+    int match = 0;
+    while (i < m) {
+      if (j < n && (s[i] == p[j] || p[j] == '?')) {
+        ++i;
+        ++j;
+      } else if (j < n && p[j] == '*') {
+        match = i;
+        asterisk = j;
+        ++j;
+      } else if (asterisk >= 0) {
+        ++match;
+        i = match;
+        j = asterisk + 1;
+      } else {
+        return false;
+      }
+    }
+    while (j < n && p[j] == '*') {
+      ++j;
+    }
+    return j == n;
+  }
+};
