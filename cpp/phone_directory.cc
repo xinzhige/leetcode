1d312983be74b28d312bd08a4d9060bc85501378
Zhige Xin <xinzhige8@gmail.com>
2018-11-06 12:07:40 -0800

    Rewrite the constructor

--- cpp/phone_directory.cc
+++ cpp/phone_directory.cc
@@ -1,46 +1,48 @@
 class PhoneDirectory {
 public:
-    /** Initialize your data structure here
-            @param maxNumbers - The maximum numbers that can be stored in the phone directory. */
-    PhoneDirectory(int maxNumbers) :
-        curr(0), numbers(maxNumbers), used(maxNumbers) {  // Time: O(n), Space: O(n)
-        iota(numbers.begin(), numbers.end(), 0);
-    }
-                                                                
-    /** Provide a number which is not assigned to anyone.
-        @return - Return an available number. Return -1 if none is available. */
-    int get() {  // Time: O(1), Space: O(1)
-        if (curr == numbers.size()) {
-            return -1;
-        }
-        const auto number = numbers[curr];
-        curr++;
-        used[number] = true;
-        return number;
-    }
-    
-    /** Check if a number is available or not. */
-    bool check(int number) {  // Time: O(1), Space: O(1)
-        if (number < 0 || number >= numbers.size()) {
-            return false;
-        }
-        return !used[number];
-    }
+  /** Initialize your data structure here
+      @param maxNumbers - The maximum numbers that can be stored in the phone directory. */
+  PhoneDirectory(int maxNumbers) {
+    idx = 0;
+    numbers.resize(maxNumbers);
+    used.resize(maxNumbers);
+    iota(numbers.begin(), numbers.end(), 0);
+  }
 
-    /** Recycle or release a number. */
-    void release(int number) {  // Time: O(1), Space: O(1)
-        if (number < 0 || number >= numbers.size() || !used[number]) {
-            return;
-        }
-        used[number] = false;
-        curr--;
-        numbers[curr] = number;
+  /** Provide a number which is not assigned to anyone.
+      @return - Return an available number. Return -1 if none is available. */
+  int get() {
+    if (idx == numbers.size()) {
+      return -1;
     }
+    int number = numbers[idx];
+    idx += 1;
+    used[number] = true;
+    return number;
+  }
+
+  /** Check if a number is available or not. */
+  bool check(int number) {
+    if (number < 0 || number >= numbers.size()) {
+      return false;
+    }
+    return !used[number];
+  }
+
+  /** Recycle or release a number. */
+  void release(int number) {
+    if (number < 0 || number >= numbers.size() || !used[number]) {
+      return;
+    }
+    used[number] = false;
+    idx -= 1;
+    numbers[idx] = number;
+  }
 
 private:
-    int curr;
-    vector<int> numbers;
-    vector<bool> used;
+  int idx;
+  vector<int> numbers;
+  vector<bool> used;
 };
 /**
  * Your PhoneDirectory object will be instantiated and called as such:
