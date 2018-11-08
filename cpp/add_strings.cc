870bd1c32b7a612c64a302767e428d5b8ecf190d
Zhige Xin <xinzhige8@gmail.com>
2018-11-07 22:35:49 -0800

    Add 415. Add Strings

---
+++ cpp/add_strings.cc
@@ -0,0 +1,18 @@
+class Solution {
+public:
+  string addStrings(string num1, string num2) {
+    int carry = 0;
+    int i = num1.size() - 1;
+    int j = num2.size() - 1;
+    string result("");
+    for (; i >= 0 || j >= 0 || carry > 0; --i, --j) {
+      int x = i >= 0 ? num1[i] - '0' : 0;
+      int y = j >= 0 ? num2[j] - '0' : 0;
+      int sum = x + y + carry;
+      carry = sum / 10;
+      result = result + to_string(sum % 10);
+    }
+    reverse(result.begin(), result.end());
+    return result;
+  }
+};
