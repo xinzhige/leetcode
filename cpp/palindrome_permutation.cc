1afb3d408e7dda2fb75b44943ca7678e836347ea
Zhige Xin <xinzhige8@gmail.com>
2018-11-19 01:02:59 -0800

    One pass algorithm

--- cpp/palindrome_permutation.cc
+++ cpp/palindrome_permutation.cc
@@ -2,13 +2,13 @@ class Solution {
 public:
   bool canPermutePalindrome(string s) {
     unordered_map<char, int> mymap;
-    for (auto &c : s) {
-      mymap[c] += 1;
-    }
     int result = 0;
-    for (auto it = mymap.begin(); it != mymap.end(); it++) {
-      if (iter.second % 2 != 0) {
+    for (int i = 0; i < s.size(); ++i) {
+      mymap[s[i]] += 1;
+      if ((mymap[s[i]] % 2) != 0) {
         result += 1;
+      } else {
+        result -= 1;
       }
     }
     return result <= 1;
