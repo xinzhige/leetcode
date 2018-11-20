827649a583c087d360ff0e1caf196ef3de5f8426
Zhige Xin <xinzhige8@gmail.com>
2018-11-20 02:44:16 -0800

    Clean code

--- cpp/reverse_list2.cc
+++ cpp/reverse_list2.cc
@@ -8,22 +8,20 @@
  */
 class Solution {
 public:
-    ListNode *reverseBetween(ListNode *head, int m, int n) {
-        ListNode dummy(0);
-        dummy.next = head;
-        ListNode *prev = &dummy;
-        for (int i = 0; i < m - 1; ++i) {
-            prev = prev->next;
-        }
-        ListNode *head2 = prev;
-        prev = head2->next;
-        ListNode *curr = prev->next;
-        for (int i = m; i < n; ++i) {
-            prev->next = curr->next; 
-            curr->next = head2->next; 
-            head2->next = curr;
-            curr = prev->next;
-        }
-        return dummy.next;
+  ListNode *reverseBetween(ListNode *head, int m, int n) {
+    ListNode dummy(0);
+    dummy.next = head;
+    ListNode *prev = &dummy;
+    for (int i = 0; i < m - 1; ++i) {
+      prev = prev->next;
     }
+    ListNode *curr = prev->next;
+    for (int i = 0; i < n - m; ++i) {
+      ListNode *tmp = curr->next;
+      curr->next = tmp->next;
+      tmp->next = prev->next;
+      prev->next = tmp;
+    }
+    return dummy.next;
+  }
 };
