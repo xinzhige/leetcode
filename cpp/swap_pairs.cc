54c3803b5a3755f053db2cda1e94d245c461bf18
Zhige Xin <xinzhige8@gmail.com>
2018-11-20 02:11:33 -0800

    Optimize the algorithm

--- cpp/swap_pairs.cc
+++ cpp/swap_pairs.cc
@@ -3,25 +3,18 @@
  * struct ListNode {
  *     int val;
  *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
  * };
  */
 class Solution {
 public:
-    ListNode *swapPairs(ListNode *head) {
-        if (head == nullptr || head->next == nullptr) {
-            return head;
-        }
-        ListNode dummy(0);
-        dummy.next = head;
-        while (curr != nullptr && curr->next != nullptr) {
-           prev->next = post;
-           curr->next = post->next;
-           post->next = curr;
-           prev = curr;
-           curr = curr->next;
-           post = (curr == nullptr) ? nullptr : curr->next;
-        }   
-        return dummy.next;
+  ListNode *swapPairs(ListNode *head) {
+    if (head == nullptr || head->next == nullptr) {
+      return head;
     }
+    ListNode* post= head->next;
+    head->next = swapPairs(post->next);
+    post->next = head;
+    return post;
+  }
 };
