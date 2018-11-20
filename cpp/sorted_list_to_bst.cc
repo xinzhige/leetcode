ede807900e45f202d57c3ccc74458c1e4102d152
Zhige Xin <xinzhige8@gmail.com>
2018-11-20 01:25:09 -0800

    Clean code

--- cpp/sorted_list_to_bst.cc
+++ cpp/sorted_list_to_bst.cc
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    TreeNode *sortedListToBST(ListNode *head) {
-        return build(head, nullptr); 
-    }
+  TreeNode *sortedListToBST(ListNode *head) {
+    return build(head, nullptr);
+  }
 
-    TreeNode *build(ListNode *begin, ListNode *end) {
-        if (begin == end) {
-            return nullptr; 
-        }
-        ListNode *slow = begin;
-        ListNode *fast = begin;
-        while (fast != nullptr && fast->next != nullptr) {
-            slow = slow->next;
-            fast = fast->next->next;
-        }
-        TreeNode *curr = new TreeNode(slow->val);
-        curr->left = build(begin, slow);
-        curr->right = build(slow->next, end);
-        return curr;
+  TreeNode *build(ListNode *begin, ListNode *end) {
+    if (begin == end) {
+      return nullptr;
     }
+    ListNode *slow = begin;
+    ListNode *fast = begin;
+    while (fast != nullptr && fast->next != nullptr) {
+      slow = slow->next;
+      fast = fast->next->next;
+    }
+    TreeNode *curr = new TreeNode(slow->val);
+    curr->left = build(begin, slow);
+    curr->right = build(slow->next, end);
+    return curr;
+  }
 };
