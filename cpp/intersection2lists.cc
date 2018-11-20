fdddadb3a85fd779dd56afec3cc10a04ab16801d
Zhige Xin <xinzhige8@gmail.com>
2018-11-20 02:52:01 -0800

    Clean code

--- cpp/intersection2lists.cc
+++ cpp/intersection2lists.cc
@@ -1,23 +1,15 @@
 class Solution {
 public:
   ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-    if (headA == nullptr || headB == nullptr) return nullptr; 
-	ListNode *pA = headA;
-	ListNode *pB = headB;
-	ListNode *tailA = nullptr;
-	ListNode *tailB = nullptr;
-	while (1) {
-      if (pA == nullptr) pA = headB;
-	  if (pB == nullptr) pB = headA;
-	  if (pA->next == nullptr) tailA = pA;
-	  if (pB->next == nullptr) tailB = pB;
-	  if (tailA != nullptr && tailB != nullptr && 
-		  tailA != tailB) {
-	    return nullptr;
-	  } 	
-	  if (pA == pB) return pA;	
-	  pA = pA->next;
-	  pB = pB->next;	
-	}					        
+    if (headA == nullptr || headB == nullptr) {
+      return nullptr;
+    }
+    ListNode *a = headA;
+    ListNode *b = headB;
+    while (a != b) {
+      a = (a == nullptr ? headB : a->next);
+      b = (b == nullptr ? headA : b->next);
+    }
+    return a;
   }
 };
