549f09ae39002ecf1e23f4165f02cbafc4491c87
Zhige Xin <xinzhige8@gmail.com>
2018-11-20 17:03:02 -0800

    Clean code

--- cpp/spiral_matrix.cc
+++ cpp/spiral_matrix.cc
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    vector<int> spiralOrder(vector<vector<int> > &matrix) {
-        vector<int> result;
-        if (matrix.empty()) {
-            return result;
-        }
-        for (int left = 0, right = matrix[0].size() - 1,
-                 top = 0, bottom = matrix.size() - 1;
-             left <= right && top <= bottom;
-             ++left, --right, ++top, --bottom) {
-
-            for (int j = left; j <= right; ++j) {
-                result.emplace_back(matrix[top][j]);
-            }
-            for (int i = top + 1; i < bottom; ++i) {
-                result.emplace_back(matrix[i][right]);
-            }
-            for (int j = right; top < bottom && j >= left; --j) {
-                result.emplace_back(matrix[bottom][j]);
-            }
-            for (int i = bottom - 1; left < right && i > top; --i) {
-                result.emplace_back(matrix[i][left]);
-            }
-        }
-        return result;
+  vector<int> spiralOrder(vector<vector<int> > &matrix) {
+    vector<int> result;
+    if (matrix.empty()) {
+      return result;
     }
+    int left = 0;
+    int right = matrix[0].size() - 1;
+    int top = 0;
+    int bottom = matrix.size() - 1;
+    for (; left <= right && top <= bottom; ++left, --right, ++top, --bottom) {
+      for (int j = left; j <= right; ++j) {
+        result.emplace_back(matrix[top][j]);
+      }
+      for (int i = top + 1; i < bottom; ++i) {
+        result.emplace_back(matrix[i][right]);
+      }
+      for (int j = right; top < bottom && j >= left; --j) {
+        result.emplace_back(matrix[bottom][j]);
+      }
+      for (int i = bottom - 1; left < right && i > top; --i) {
+        result.emplace_back(matrix[i][left]);
+      }
+    }
+    return result;
+  }
 };
