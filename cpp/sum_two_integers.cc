f6d03cbbc94285c4f622ce7e0c06bf9c24f2a6fd
Zhige Xin <xinzhige8@gmail.com>
2018-11-26 12:27:56 -0800

    Clean code

--- cpp/sum_two_integers.cc
+++ cpp/sum_two_integers.cc
@@ -7,9 +7,9 @@ class Solution {
 public:
   int getSum(int a, int b) {
     while (b != 0) {
-      int carry = (a & b) << 1;
+      int carry = a & b;
       a ^= b;
-      b = carry;
+      b = carry << 1;
     }
     return a;
   }
