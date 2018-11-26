8b3946648a01d5a13545a994a66d96782cf2ae8e
Zhige Xin <xinzhige8@gmail.com>
2018-11-26 12:20:01 -0800

    Add comments

--- cpp/sum_two_integers.cc
+++ cpp/sum_two_integers.cc
@@ -1,3 +1,8 @@
+// First, we can use "and"("&") operation between a and b to find a carry.
+// carry = a & b, then carry = 0001;
+// Second, we can use "xor" ("^") operation between a and b to find
+// the different bit, and assign it to a;
+// Then, we shift carry one position left and assign it to b, b = 0010.
 class Solution {
 public:
   int getSum(int a, int b) {
