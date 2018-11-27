f84583480f8c23ea7fe3b2aa2262eb80282cd3d1
Zhige Xin <xinzhige8@gmail.com>
2018-11-27 15:23:52 -0800

    Add 110. Balanced Binary Tree

---
+++ cpp/balanced_binary_tree.cc
@@ -0,0 +1,24 @@
+// Time: O(n), only traverse each node once.
+class Solution {
+public:
+  bool isBalanced(TreeNode *root) {
+    if (root == nullptr) {
+      return true;
+    }
+    return depth(root) != -1;
+  }
+
+private:
+  int depth(TreeNode *root) {
+    if (root == nullptr) {
+      return 0;
+    }
+    int left_depth = depth(root->left);
+    int right_depth = depth(root->right);
+    if (left_depth == -1 || right_depth == -1 ||
+        abs(left_depth - right_depth) > 1) {
+      return -1;
+    }
+    return 1 + max(left_depth, right_depth);
+  }
+};
