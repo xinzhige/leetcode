f4e9dd569810b973bfaf5a8c5c0dc35690c53a22
Zhige Xin <xinzhige8@gmail.com>
2018-11-27 18:27:16 -0800

    Add Morris traversal

--- cpp/inorder_traversal.cc
+++ cpp/inorder_traversal.cc
@@ -1,20 +1,52 @@
-// iterative
+// Iterative, space: O(n)
 class Solution {
 public:
-    vector<int> inorderTraversal(TreeNode *root) {
-        vector<int> result;
-        stack<TreeNode*> my_stack;
-        while (root != nullptr || !my_stack.empty()) {
-            if (root != nullptr) {
-                my_stack.emplace(root);
-                root = root>left;
-            } else {
-                root = my_stack.top();
-                my_stack.pop();
-                result.emplace_back(root->val);
-                root = root->right;
-            }
-        }
-        return result;
+  vector<int> inorderTraversal(TreeNode *root) {
+    vector<int> result;
+    stack<TreeNode*> my_stack;
+    while (root != nullptr || !my_stack.empty()) {
+      if (root != nullptr) {
+        my_stack.emplace(root);
+        root = root>left;
+      } else {
+        root = my_stack.top();
+        my_stack.pop();
+        result.emplace_back(root->val);
+        root = root->right;
+      }
     }
+    return result;
+  }
+};
+
+
+
+// Morris traversal, space: O(1)
+class Solution {
+public:
+  vector<int> inorderTraversal(TreeNode *root) {
+    vector<int> result;
+    TreeNode *curr = root;
+    TreeNode *prev = nullptr;
+    while (curr != nullptr) {
+      if (curr->left == nullptr) {
+        result.push_back(curr->val);
+        curr = curr->right;
+      } else {
+        prev = curr->left;
+        while (prev->right != nullptr && prev->right != curr) {
+          prev = prev->right;
+        }
+        if (prev->right == nullptr) {
+          prev->right = curr;
+          curr = curr->left;
+        } else {
+          prev->right = nullptr;
+          result.push_back(curr->val);
+          curr = curr->right;
+        }
+      }
+    }
+    return result;
+  }
 };
