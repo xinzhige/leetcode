26a9a49600954d15207c490a74b570d1a6795be7
Zhige Xin <xinzhige8@gmail.com>
2018-11-28 23:24:59 -0800

    Clean code

--- cpp/kth_smallest_bst.cc
+++ cpp/kth_smallest_bst.cc
@@ -2,21 +2,20 @@
 class Solution {
 public:
   int kthSmallestHelper(TreeNode* root, int& k) {
-    if (root == nullptr) {
-      return INT_MIN;    
-    }
-    int result = kthSmallestHelper(root->left, k);
-    if (k == 0) {
-      return result;	    
-    }
-    k -= 1;
-    if (k == 0) {
-      return root->val;	    
-    }
-    return kthSmallestHelper(root->right, k);
+    int result = 0;
+    inorder(root, k, result);
+    return result;
   }
-  int kthSmallest(TreeNode* root, int k) {
-    return kthSmallestHelper(root, k);
+  void inorder(TreeNode* root, int &k, int &result) {
+    if (root != nullptr) {
+      inorder(root->left, k, result);
+      --k;
+      if (k == 0) {
+        result = root->val;
+        return;
+      }
+      inorder(root->right, k, result);
+    }
   }
 };
 
@@ -29,15 +28,16 @@ public:
     while (!mystack.empty() || root != nullptr) {
       while (root != nullptr) {
         mystack.push(root);
-	root = root->left;
-      }	   
-      TreeNode* curr = mystack.top();      
-      k -= 1;
-      if (k == 0) {
-        return curr->val;    
+        root = root->left;
+      } else {
+        TreeNode* curr = mystack.top();
+        mystack.pop();
+        k -= 1;
+        if (k == 0) {
+          return curr->val;
+        }
+        root = curr->right;
       }
-      root = curr->right;
-      mystack.pop();
     }
     return INT_MIN;
   }
