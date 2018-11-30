0258d2b7492d21cab75663def4f760e42ede93bf
Zhige Xin <xinzhige8@gmail.com>
2018-11-29 23:38:20 -0800

    Clean code

--- cpp/is_subsequence.cc
+++ cpp/is_subsequence.cc
@@ -1,3 +1,4 @@
+// Idea: two pointers
 class Solution {
 public:
   bool isSubsequence(string s, string t) {
@@ -8,12 +9,9 @@ public:
     for (int j = 0; j < t.size(); ++j) {
       if (t[j] == s[i]) {
         ++i;
-        if (i == s.size()) {
-          return true;
-        }
       }
     }
-    return false;
+    return i == s.size();
   }
 };
 
@@ -24,42 +22,20 @@ public:
   bool isSubsequence(string s, string t) {
     unordered_map<char, vector<int>> index;
     for (int i = 0; i < t.size(); ++i) {
-      if (index.count(t[i]) == 0) {
-        vector<int> curr;
-        index[t[i]] = curr;
-      }
       index[t[i]].push_back(i);
     }
-    int prev = 0;
-    for (int i = 0; i < s.size(); ++i) {
-      if (index.count(s[i]) == 0) {
+    int curr = -1;
+    for (const char &c : s) {
+      if (index.count(c) == 0) {
         return false;
       }
-      int j = binarySearch(index[s[i]], prev);
-      if (j < 0) {
-        j = -j - 1;
-      }
-      if (j == index[s[i]].size()) {
+      vector<int> arr = index[c];
+      auto it = upper_bound(arr.begin(), arr.end(), curr);
+      if (it == arr.end()) {
         return false;
       }
-      prev = index[s[i]][j] + 1;
+      curr = *it;
     }
     return true;
-  }
-  int binarySearch(vector<int> &nums, int x) {
-    int begin = 0;
-    int end = nums.size() - 1;
-    int mid = 0;
-    while (begin <= end) {
-      mid = begin + (end - begin) / 2;
-      if (nums[mid] == x) {
-        return i;
-      } else if (nums[mid] < x) {
-        begin = mid + 1;
-      } else {
-        end = mid - 1;
-      }
-    }
-    return -1;
   }
 };
