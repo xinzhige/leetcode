fdd5cbf70596a272fb17f0a444519cdc097ab77b
Zhige Xin <xinzhige8@gmail.com>
2018-11-29 20:26:06 -0800

    Add 209. Minimum Size Subarray Sum

---
+++ cpp/minimum_size_subarray_sum.cc
@@ -0,0 +1,20 @@
+// Idea: two pointers point to the begin and the end of current subarray
+// every time increment the end and decrease the begin until the sum is
+// less than s.
+class Solution {
+public:
+  int minSubArrayLen(int s, vector<int>& nums) {
+    int result = INT_MAX;
+    int left = 0;
+    int sum = 0;
+    for (int i = 0; i < nums.size(); ++i) {
+      sum += nums[i];
+      while (sum >= s) {
+        result = min(result, i - left + 1);
+        sum -= nums[left];
+        left += 1;
+      }
+    }
+    return result == INT_MAX ? 0 : result;
+  }
+};
