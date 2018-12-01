72b4bf40406887fe138e2cf5829283e76ba3a218
xinzhige <xinzhige8@gmail.com>
2018-12-01 12:35:30 -0800

    Add comments

--- cpp/contains_duplicate3.cc
+++ cpp/contains_duplicate3.cc
@@ -1,19 +1,23 @@
+// Idea: two pointers to control the range and map to find lower bound.
+// Note: long long to handle INT_MIN.
 class Solution {
 public:
   bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
-    map<long long, int> m;
-    for (int j = 0, i = 0; i < nums.size(); ++i) {
+    map<long long, int> index;
+    int j = 0;
+    for (int i = 0; i < nums.size(); ++i) {
+      // make sure the range is not greater than k
       if (i - j > k) {
-        m.erase(nums[j]);
+        index.erase(nums[j]);
         j += 1;
       }
       // x >= nums[i] - t
-      auto p = m.lower_bound((long long)nums[i] - t);
+      auto p = index.lower_bound((long long)nums[i] - t);
       // x <= nums[i] + t
-      if (p != m.end() && (p->first - nums[i]) <= t) {
+      if (p != index.end() && (p->first - nums[i]) <= t) {
         return true;
       }
-      m[nums[i]] = i;
+      index[nums[i]] = i;
     }
     return false;
   }
