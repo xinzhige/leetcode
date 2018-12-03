a69f627f84d80a5bbe86ae1848cedae9c4fccb4c
Zhige Xin <xinzhige8@gmail.com>
2018-12-03 14:49:02 -0800

    Add 222. Count Complete Tree Nodes

--- cpp/count_complete_tree_nodes.cc
+++ cpp/count_complete_tree_nodes.cc
@@ -1,3 +1,5 @@
+// Idea: go down to the left most leaf and the right leaf and compare them,
+// if equal, then it is perfect binary tree. Otherwise, recursively count nodes.
 class Solution {
 public:
   int countNodes(TreeNode* root) {
@@ -13,5 +15,42 @@ public:
       return pow(2, hleft) - 1;
     }
     return countNodes(root->left) + countNodes(root->right) + 1;
+  }
+};
+
+
+// Idea: iterative, calculate height of right tree, if the same as height,
+// go to right tree (plus 1 to result), otherwise go to left
+// tree (plus 0 to result).
+class Solution {
+public:
+  int countNodes(TreeNode* root) {
+    if (root == nullptr) {
+      return 0;
+    }
+    int height = 0;
+    TreeNode* p = root;
+    for (; p != nullptr; p = p->left) {
+      height += 1;
+    }
+    int result = 1;
+    int rheight = 0;
+    TreeNode* visited = root;
+    height -= 1;
+    for (; height > 0; height -= 1) {
+      result *= 2;
+      rheight = 0;
+      p = visited->right;
+      for (; p != nullptr; p = p->left) {
+        rheight += 1;
+      }
+      if (rheight < height) {
+        visited = visited->left;
+      } else {
+        result += 1;
+        visited = visited->right;
+      }
+    }
+    return result;
   }
 };
