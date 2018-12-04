43e39fca17bbfbb3fa6fd6413478c36de16be188
Zhige Xin <xinzhige8@gmail.com>
2018-12-04 10:04:13 -0800

    Clean code

--- cpp/merge_intervals.cc
+++ cpp/merge_intervals.cc
@@ -8,13 +8,14 @@
  * };
  */
 class Solution {
-:
+public:
   vector<Interval> merge(vector<Interval> &intervals) {
-    if (intervals.size() == 0) {
+    if (intervals.empty()) {
       return intervals;
     }
-    sort(intervals.begin(), intervals.end(), myfunc);
-    vector<Interval> result{intervals[0]};
+    sort(intervals.begin(), intervals.end(), mycomparison);
+    vector<Interval> result;
+    result.push_back(intervals[0]);
     for (int i = 1; i < intervals.size(); ++i) {
       if (result.back().end < intervals[i].start) {
 	      result.emplace_back(intervals[i]);
@@ -25,10 +26,9 @@ class Solution {
     return result;
   }
 
-  // Note myfunc should be static because it is not object dependent.
-  // And it should compare start not end because it would fail if
-  // the intervals is [[1, 4], [0, 4]].
-  static bool myfunc(const Interval& a, const Interval& b) {
-    return a.start < b.start;
-  }
+  struct Comparison {
+    bool operator() (const Interval& a, const Interval& b) {
+      return a.start < b.start;
+    }
+  }mycomparison;
 };
