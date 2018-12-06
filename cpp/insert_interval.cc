f9eefe68b54bdb99b83f088894aa124d996613e9
Zhige Xin <xinzhige8@gmail.com>
2018-12-05 21:18:18 -0800

    Add comments

--- cpp/insert_interval.cc
+++ cpp/insert_interval.cc
@@ -1,29 +1,23 @@
-/**
- * Definition for an interval.
- * struct Interval {
- *     int start;
- *     int end;
- *     Interval() : start(0), end(0) {}
- *     Interval(int s, int e) : start(s), end(e) {}
- * };
- */
+// Idea: compare each element with new interval, if overlapping, then make a
+// new interval with minimum start and maximum end of them. Otherwise, put
+// either of them into the result.
 class Solution {
 public:
   vector<Interval> insert(vector<Interval> &intervals, Interval newInterval) {
     Interval tmp = newInterval;
     vector<Interval> result;
-    for (auto & curr : intervals) {
-      if (tmp.end < intervals[i].start) {
+    for (const auto& curr : intervals) {
+      if (tmp.end < curr.start) {
         result.push_back(tmp);
-        tmp = intervals[i];
-      } else if (intervals[i].end < temp.start) {
-        result.push_back(intervals[i]);
+        tmp = curr;
+      } else if (curr.end < tmp.start) {
+        result.push_back(curr);
       } else {
-        tmp.start = min(tmp.start, intervals[i].start);
-        tmp.end = max(tmp.end, intervals[i].end);
+        tmp.start = min(tmp.start, curr.start);
+        tmp.end = max(tmp.end, curr.end);
       }
     }
-    result.push_back(temp);
+    result.push_back(tmp);
     return result;
   }
 };
