67c1d84dfe7e0a7b94f8c989c3c08cdfee61cecb
Zhige Xin <xinzhige8@gmail.com>
2018-12-08 15:19:26 -0800

    Add comments

--- cpp/wiggle_sort.cc
+++ cpp/wiggle_sort.cc
@@ -1,11 +1,13 @@
+// Idea: compare current number nums[i] with the previous one nums[i-1],
+// 1. if odd position and nums[i] < nums[i-1] or
+// 2. even position and nums[i] >= nums[i-1], swap them.
 class Solution {
 public:
-    void wiggleSort(vector<int>& nums) {
-        for (int i = 1; i < nums.size(); ++i) {
-            if (((i & 1) && nums[i] < nums[i - 1])
-                || (!(i & 1) && nums[i] > nums[i - 1])) {
-                swap(nums[i], nums[i - 1]);
-            }
-        }
+  void wiggleSort(vector<int>& nums) {
+    for (int i = 1; i < nums.size(); ++i) {
+      if ((i % 2 != 0) == (nums[i - 1] > nums[i])) {
+        swap(nums[i - 1], nums[i]);
+      }
     }
+  }
 };
