4dfd16533841c542f01e22f63767e720d9e5a83b
Zhige Xin <xinzhige8@gmail.com>
2018-12-09 10:57:46 -0800

    Add comments

--- cpp/wiggle_sort2.cc
+++ cpp/wiggle_sort2.cc
@@ -1,38 +1,5 @@
-// Time: O(n), space: O(n)
-class Solution {
-public:
-  void wiggleSort(vector<int>& nums) {
-    int n = nums.size();
-    int m = (n + 1) / 2;
-    vector<int> copy(nums);
-    nth_element(nums.begin(), nums.begin() + m, nums.end());
-    int median = nums[m];
-    int i = 0;
-    int j = 0;
-    int k = n - 1;
-    while (j <= k) {
-      if (copy[j] < median) {
-        swap(copy[i], copy[j]);
-        ++i;
-        ++j;
-      } else if (copy[j] > median) {
-        swap(copy[j], copy[k]);
-        --k;
-      } else {
-        ++j;
-      }
-    }
-    for (int i = 0, j = m - 1; j >= 0; i += 2, --j) {
-      nums[i] = copy[j];
-    }
-    for (int i = 1, j = n - 1; j >= m; i += 2, --j) {
-      nums[i] = copy[j];
-    }
-  }
-};
-
-
-// Time: O(n), space: O(1)
+// Idea: find median and reset the index to the right place.
+// Note: if there are even numbers, n | 1.
 class Solution {
 public:
   void wiggleSort(vector<int>& nums) {
@@ -40,18 +7,80 @@ public:
     auto midptr = nums.begin() + n / 2;
     nth_element(nums.begin(), midptr, nums.end());
     int mid = *midptr;
-    #define A(i) nums[(1+2*(i)) % (n|1)]
     int i = 0;
     int j = 0;
     int k = n - 1;
     while (j <= k) {
-      if (A(j) > mid) {
-        swap(A(i++), A(j++));
-      } else if (A(j) < mid) {
-        swap(A(j), A(k--));
+      if (nums[A(j, n)] > mid) {
+        swap(nums[A(i++, n)], nums[A(j++, n)]);
+      } else if (nums[A(j, n)] < mid) {
+        swap(nums[A(j, n)], nums[A(k--, n)]);
       } else {
         j++;
       }
     }
+  }
+  int A(int i, int n) {
+    return (1 + 2 * (i)) % (n | 1);
+  }
+};
+
+
+class Solution {
+public:
+  void wiggleSort(vector<int>& nums) {
+    int n = nums.size();
+    int median = findKthLargest(nums, (n + 1) / 2);
+    int left = 0;
+    int i = 0;
+    int right = n - 1;
+    while (i <= right) {
+      if (nums[newIndex(i, n)] > median) {
+        swap(nums[newIndex(left++, n)], nums[newIndex(i++, n)]);
+      } else if (nums[newIndex(i, n)] < median) {
+        swap(nums[newIndex(right--, n)], nums[newIndex(i, n)]);
+      } else {
+        ++i;
+      }
+    }
+  }
+  int findKthLargest(vector<int>& nums, int k) {
+    int left = 0;
+    int right = nums.size() - 1;
+    int pos = 0;
+    while (true) {
+      pos = partition(nums, left, right);
+      if (pos == k - 1) {
+        break;
+      } else if (pos < k - 1) {
+        left = pos + 1;
+      } else {
+        right = pos - 1;
+      }
+    }
+    return nums[pos];
+  }
+  int partition(vector<int> &nums, int left, int right) {
+    int pivot = nums[left];
+    int l = left + 1;
+    int r = right;
+    while (l <= r) {
+      if (nums[l] < pivot && pivot < nums[r]) {
+        swap(nums[l], nums[r]);
+        ++l;
+        --r;
+      }
+      if (nums[l] >= pivot) {
+        ++l;
+      }
+      if (nums[r] <= pivot) {
+        --r;
+      }
+    }
+    swap(nums[left], nums[r]);
+    return r;
+  }
+  int newIndex(int index, int n) {
+    return (1 + 2 * index) % (n | 1);
   }
 };
