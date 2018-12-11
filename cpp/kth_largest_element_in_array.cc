e57f7908f792b3fdd91e8283ac11a42eed54c91a
Zhige Xin <xinzhige8@gmail.com>
2018-12-10 17:23:08 -0800

    Clean code

--- cpp/kth_largest_element_in_array.cc
+++ cpp/kth_largest_element_in_array.cc
@@ -2,17 +2,13 @@
 class Solution {
 public:
   int findKthLargest(vector<int>& nums, int k) {
-    priority_queue<int, vector<int>, greater<int>> q;
-    for (const auto &num : nums) {
-      q.emplace(num);
-      if (q.size() > k) {
-        q.pop();
-      }
+    priority_queue<int> pq(nums.begin(), nums.end());
+    for (int i = 0; i < k - 1; i++) {
+      pq.pop();
     }
-    return q.top();
+    return pq.top();
   }
 };
-
 
 // Quick sort like, time: O(n)
 class Solution {
