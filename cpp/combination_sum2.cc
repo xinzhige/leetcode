b9d68e8da4b61fcfd4a73dcd4d7f5a5f6587aa0b
Zhige Xin <xinzhige8@gmail.com>
2018-12-10 23:32:15 -0800

    Clean code

--- cpp/combination_sum2.cc
+++ cpp/combination_sum2.cc
@@ -3,25 +3,25 @@ public:
   vector<vector<int>> combinationSum2(vector<int> &candidates, int target) {
     sort(candidates.begin(), candidates.end());
     vector<vector<int>> result;
-    vector<int> tmp;
-    backtrack(candidates, 0, target, tmp, result);
+    vector<int> path;
+    backtrack(candidates, 0, target, path, result);
     return result;
   }
 
 private:
   void backtrack(vector<int> &nums, int pos, int target,
-                 vector<int> &tmp, vector<vector<int>> &result) {
+                 vector<int> &path, vector<vector<int>> &result) {
     if (target == 0) {
-      result.push_back(tmp);
+      result.push_back(path);
     } else {
       for (int i = pos; i < nums.size(); ++i) {
         if (target < nums[i]) {
           return;
         }
         if (i == pos || nums[i] != nums[i-1]) {
-          tmp.push_back(nums[i]);
-          backtrack(nums, i + 1, target-nums[i], tmp, result);
-          tmp.pop_back();
+          path.push_back(nums[i]);
+          backtrack(nums, i + 1, target - nums[i], path, result);
+          path.pop_back();
         }
       }
     }
