ecaca1bd7e57faed4db9026b8d876a39fb04dffa
Zhige Xin <xinzhige8@gmail.com>
2018-12-10 23:14:41 -0800

    Clean code

--- cpp/continuous_subarray_sum.cc
+++ cpp/continuous_subarray_sum.cc
@@ -2,16 +2,17 @@ class Solution {
 public:
   bool checkSubarraySum(vector<int>& nums, int k) {
     int sum = 0;
-    unordered_map<int, int> mymap{{0,-1}};
+    map<int, int> index;
+    index[0] = -1;
     for (int i = 0; i < nums.size(); ++i) {
       sum += nums[i];
       int tmp = (k == 0) ? sum : (sum % k);
-      if (mymap.find(tmp) != mymap.end()) {
-	if (i - mymap[tmp] > 1) {
-	  return true;
-	}
+      if (index.count(tmp) > 0) {
+        if (i - index[tmp] > 1) {
+          return true;
+        }
       } else {
-	mymap[tmp] = i;
+        index[tmp] = i;
       }
     }
     return false;
