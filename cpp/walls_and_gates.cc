435b3559ac944c500c070be97f4bbef124b80750
Zhige Xin <xinzhige8@gmail.com>
2018-12-10 17:40:36 -0800

    Clean code

--- cpp/walls_and_gates.cc
+++ cpp/walls_and_gates.cc
@@ -3,23 +3,21 @@ public:
   void wallsAndGates(vector<vector<int>>& rooms) {
     for (int i = 0; i < rooms.size(); ++i) {
       for (int j = 0; j < rooms[i].size(); ++j) {
-	if (rooms[i][j] == 0) {
-	  dfs(rooms, i, j, 0);
-	}
+        if (rooms[i][j] == 0) {
+          dfs(rooms, i, j, 0);
+        }
       }
     }
   }
-
-private:
   void dfs(vector<vector<int>> &rooms, int i, int j, int val) {
-    if (i < 0 || i >= rooms.size() || j < 0 ||
-	j >= rooms[i].size() || rooms[i][j] < val) {
-      return;
+    if (i >= 0 && i < rooms.size() && j >= 0 &&
+        j < rooms[i].size() && rooms[i][j] >= val) {
+      rooms[i][j] = val;
+      for (const auto &d : dirs) {
+        dfs(rooms, i + d[0], j + d[1], val + 1);
+      }
     }
-    rooms[i][j] = val;
-    dfs(rooms, i + 1, j, val + 1);
-    dfs(rooms, i - 1, j, val + 1);
-    dfs(rooms, i, j + 1, val + 1);
-    dfs(rooms, i, j - 1, val + 1);
   }
+private:
+  vector<vector<int>> dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 };
