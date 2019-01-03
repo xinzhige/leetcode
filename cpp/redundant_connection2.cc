56d942c0f751a5d31c2f243bda6fa4645c7a4e26
Zhige Xin <xinzhige8@gmail.com>
2019-01-03 12:35:18 -0800

    Add 685. Redundant Connection II

---
+++ cpp/redundant_connection2.cc
@@ -0,0 +1,44 @@
+class Solution {
+public:
+  vector<int> findRedundantDirectedConnection(vector<vector<int>>& edges) {
+    int n = edges.size();
+    vector<int> parent(n + 1, 0);
+    vector<int> first;
+    vector<int> second;
+    for (auto &e : edges) {
+      if (parent[e[1]] == 0) {
+        parent[e[1]] = e[0];
+      } else {
+        first = {parent[e[1]], e[1]};
+        second = e;
+        e[1] = 0;
+      }
+    }
+    for (int i = 1; i <= n; ++i) {
+      parent[i] = i;
+    }
+    for (const auto &e : edges) {
+      if (e[1] != 0) {
+        int p = find_root(parent, e[0]);
+        if (p == e[1]) {
+          if (first.empty()) {
+            // has cycle without two parents
+            return e;
+          } else {
+            // has cycle with two parents
+            return first;
+          }
+        }
+        parent[e[1]] = p;
+      }
+    }
+    // no cycle but with two parents
+    return second;
+  }
+  int find_root(vector<int>& parent, int x) {
+    for (; x != parent[x]; x = parent[x]) {
+      parent[x] = parent[parent[x]];
+    }
+    return x;
+  }
+};
