64e00849c20f25487ca011709cc984e433515eba
Zhige Xin <xinzhige8@gmail.com>
2019-01-10 15:49:53 -0800

    Use bitmask

--- cpp/n_queens.cc
+++ cpp/n_queens.cc
@@ -1,42 +1,36 @@
-// Backtracking
+// Bitmask
 class Solution {
 public:
   vector<vector<string>> solveNQueens(int n) {
     vector<vector<string>> result;
     vector<string> nQueens(n, string(n, '.'));
-    solveNQueens(n, 0, nQueens, result);
+    vector<int> flag_col(n, 1);
+    vector<int> flag_45(2 * n - 1, 1);
+    vector<int> flag_135(2 * n - 1, 1);
+    solveNQueens(n, 0, nQueens, flag_col, flag_45, flag_135, result);
     return result;
   }
 private:
   void solveNQueens(const int &n, int row, vector<string> &nQueens,
-                    vector<vector<string>> &result) {
+                    vector<int> &flag_col, vector<int> &flag_45,
+                    vector<int> &flag_135, vector<vector<string>> &result) {
     if (row == n) {
       result.push_back(nQueens);
       return;
     }
-    for (int col = 0; col < n; ++col)
-      if (isValid(nQueens, row, col, n)) {
+    for (int col = 0; col < n; ++col) {
+      if (flag_col[col] == 1 && flag_45[row + col] == 1 &&
+          flag_135[n - 1 + col - row] == 1) {
+        flag_col[col] = 0;
+        flag_45[row + col] = 0;
+        flag_135[n - 1 + col - row] = 0;
         nQueens[row][col] = 'Q';
-        solveNQueens(n, row + 1, nQueens, result);
+        solveNQueens(n, row + 1, nQueens, flag_col, flag_45, flag_135, result);
         nQueens[row][col] = '.';
-      }
-  }
-  bool isValid(vector<string> &nQueens, int row, int col, const int &n) {
-    for (int i = 0; i < row; ++i) {
-      if (nQueens[i][col] == 'Q') {
-        return false;
+        flag_col[col] = 1;
+        flag_45[row + col] = 1;
+        flag_135[n - 1 + col - row] = 1;
       }
     }
-    for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; --i, --j) {
-      if (nQueens[i][j] == 'Q') {
-        return false;
-      }
-    }
-    for (int i = row - 1, j = col + 1; i >= 0 && j < n; --i, ++j) {
-      if (nQueens[i][j] == 'Q') {
-        return false;
-      }
-    }
-    return true;
   }
 };
