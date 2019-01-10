6966f38ba1aecf397595a7721b262d3119f6073b
Zhige Xin <xinzhige8@gmail.com>
2019-01-09 21:55:18 -0800

    Add UnionFind class

--- cpp/number_of_islands2.cc
+++ cpp/number_of_islands2.cc
@@ -1,37 +1,77 @@
+class UnionFind {
+public:
+  UnionFind(int N) {
+    count = 0;
+    for (int i = 0; i < N; ++i) {
+      parent.push_back(-1);
+      rank.push_back(0);
+    }
+  }
+
+  int findRoot(int i) {
+    while (parent[i] != i) {
+      parent[i] = parent[parent[i]];
+      i = parent[i];
+    }
+    return i;
+  }
+
+  void runUnion(int x, int y) {
+    int rootx = findRoot(x);
+    int rooty = findRoot(y);
+    if (rootx != rooty) {
+      if (rank[rootx] > rank[rooty]) {
+        parent[rooty] = rootx;
+      } else if (rank[rootx] < rank[rooty]) {
+        parent[rootx] = rooty;
+      } else {
+        parent[rooty] = rootx;
+        rank[rootx] += 1;
+      }
+      count -= 1;
+    }
+  }
+
+  int getCount() {
+    return count;
+  }
+
+  bool isValid(int i) {
+    return parent[i] >= 0;
+  }
+
+  void setParent(int i) {
+    parent[i] = i;
+    count += 1;
+  }
+
+private:
+  vector<int> parent;
+  vector<int> rank;
+  int count;
+};
+
 class Solution {
 public:
-    vector<int> numIslands2(int m, int n, vector<pair<int, int>>& positions) {
-      vector<int> result;
-      int count = 0;
-      vector<int> roots(m * n, -1);
-      vector<vector<int>> dirs{{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
-      for (const auto &pos : positions) {
-        int id = n * pos.first + pos.second;
-        if (roots[id] == -1) {
-          roots[id] = id;
-          count += 1;
+  vector<int> numIslands2(int m, int n, vector<pair<int, int>>& positions) {
+    vector<int> result;
+    UnionFind uf (m*n);
+    vector<vector<int>> dirs = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
+    for (const auto& pos : positions) {
+      int i = pos.first;
+      int j = pos.second;
+      int prev_id = i*n + j;
+      uf.setParent(prev_id);
+      for (const auto &dir : dirs) {
+        int x = i + dir[0];
+        int y = j + dir[1];
+        int curr_id = x*n + y;
+        if (x >= 0 && x < m && y >= 0 && y < n && uf.isValid(curr_id)) {
+          uf.runUnion(prev_id, curr_id);
         }
-        for (const auto &dir : dirs) {
-          int x = pos.first + dir[0];
-          int y = pos.second + dir[1];
-          int curr_id = n * x + y;
-          if (x >= 0 && x < m && y >= 0 && y < n && roots[curr_id] != -1) {
-            int p = findRoot(roots, curr_id);
-            int q = findRoot(roots, id);
-            if (p != q) {
-              roots[p] = q;
-              count -= 1;
-            }
-          }
-        }
-        result.push_back(count);
       }
-      return result;
+      result.push_back(uf.getCount());
     }
-  int findRoot(vector<int>& roots, int id) {
-    for (; id != roots[id]; id = roots[id]) {
-      roots[id] = roots[roots[id]];
-    }
-    return id;
+    return result;
   }
 };
