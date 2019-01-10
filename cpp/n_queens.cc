c8135e6cc90e91f115e64c818d77b7ff9c897ab3
Zhige Xin <xinzhige8@gmail.com>
2019-01-10 15:16:12 -0800

    Rewrite the algorithm

--- cpp/n_queens.cc
+++ cpp/n_queens.cc
@@ -1,79 +1,42 @@
+// Backtracking
 class Solution {
 public:
-  void addResult(vector<int> &A, int n, vector<vector<string> > &result) {
-    vector<string> solution; 
-    for (int row = 0; row < n; row++) {
-      string str(n, '.');
-      str[A[row]] = 'Q';
-      solution.push_back(str);
-    }	 
-    result.push_back(solution);
-  } 
-  bool isValid(vector<int> &A, int row) {
-    for (int i = 0; i < row; i++) {
-      if (A[i] == A[row] ||
-	  (abs(A[i] - A[row]) == row - i)) {
+  vector<vector<string>> solveNQueens(int n) {
+    vector<vector<string>> result;
+    vector<string> nQueens(n, string(n, '.'));
+    solveNQueens(n, 0, nQueens, result);
+    return result;
+  }
+private:
+  void solveNQueens(const int &n, int row, vector<string> &nQueens,
+                    vector<vector<string>> &result) {
+    if (row == n) {
+      result.push_back(nQueens);
+      return;
+    }
+    for (int col = 0; col < n; ++col)
+      if (isValid(nQueens, row, col, n)) {
+        nQueens[row][col] = 'Q';
+        solveNQueens(n, row + 1, nQueens, result);
+        nQueens[row][col] = '.';
+      }
+  }
+  bool isValid(vector<string> &nQueens, int row, int col, const int &n) {
+    for (int i = 0; i < row; ++i) {
+      if (nQueens[i][col] == 'Q') {
         return false;
-      }	    
+      }
+    }
+    for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; --i, --j) {
+      if (nQueens[i][j] == 'Q') {
+        return false;
+      }
+    }
+    for (int i = row - 1, j = col + 1; i >= 0 && j < n; --i, ++j) {
+      if (nQueens[i][j] == 'Q') {
+        return false;
+      }
     }
     return true;
   }
-  void nQueens(vector<int> A, int row, int n, vector<vector<string> > &result) {
-    if (row == n) {
-      addResult(A, n, result);
-      return;
-    } else {
-      for (int col = 0; col < n; col++) {
-        A[row] = col;
-	if (isValid(A, row)) {
-          nQueens(A, row + 1, n, result);		
-        }
-      }	    
-    } 
-  }
-  vector<vector<string> > solveNQueens(int n) {
-    vector<vector<string> > result;
-    vector<int> A(n, 0);
-    nQueens(A, 0, n, result);
-    return result;
-  }
 };
-
-/*
-// fast version using bitmask
-class Solution 
-{
-    int n;
-    string getNq(int p)
-    {
-        string s(n, '.');
-        s[p] = 'Q';
-        return s;
-    }
-    void nQueens(int p, int l, int m, int r, vector<vector<string>> &res)
-    {
-        static vector<string> ans;
-        if (p >= n)
-        {
-            res.push_back(ans);
-            return ;
-        }
-        int mask = l | m | r;
-        for (int i = 0, b = 1; i < n; ++ i, b <<= 1)
-            if (!(mask & b))
-            {
-                ans.push_back(getNq(i));
-                nQueens(p + 1, (l | b) >> 1, m | b, (r | b) << 1, res);
-                ans.pop_back();
-            }
-    }
-public:
-    vector<vector<string> > solveNQueens(int n) 
-    {
-        this->n = n;
-        vector<vector<string>> res;
-        nQueens(0, 0, 0, 0, res);
-        return res;
-    }
-};
-*/
