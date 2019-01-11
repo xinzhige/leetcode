98a1ffe9f58c487e0b8ba89d498079db1dd05d14
Zhige Xin <xinzhige8@gmail.com>
2019-01-10 16:23:39 -0800

    Clean code

--- cpp/n_queens.cc
+++ cpp/n_queens.cc
@@ -5,31 +5,34 @@ public:
     vector<vector<string>> result;
     vector<string> nQueens(n, string(n, '.'));
     vector<int> flag_col(n, 1);
-    vector<int> flag_45(2 * n - 1, 1);
-    vector<int> flag_135(2 * n - 1, 1);
-    solveNQueens(n, 0, nQueens, flag_col, flag_45, flag_135, result);
+    vector<int> diagonal(2 * n - 1, 1);
+    vector<int> anti_diagonal(2 * n - 1, 1);
+    solveNQueens(n, 0, nQueens, flag_col, diagonal, anti_diagonal, result);
     return result;
   }
 private:
   void solveNQueens(const int &n, int row, vector<string> &nQueens,
-                    vector<int> &flag_col, vector<int> &flag_45,
-                    vector<int> &flag_135, vector<vector<string>> &result) {
+                    vector<int> &flag_col, vector<int> &diagonal,
+                    vector<int> &anti_diagonal,
+                    vector<vector<string>> &result) {
     if (row == n) {
       result.push_back(nQueens);
       return;
     }
     for (int col = 0; col < n; ++col) {
-      if (flag_col[col] == 1 && flag_45[row + col] == 1 &&
-          flag_135[n - 1 + col - row] == 1) {
+      if (flag_col[col] == 1 &&
+          diagonal[n - 1 - row + col] == 1 &&
+          anti_diagonal[row + col] == 1) {
         flag_col[col] = 0;
-        flag_45[row + col] = 0;
-        flag_135[n - 1 + col - row] = 0;
+        diagonal[n - 1 - row + col] = 0;
+        anti_diagonal[row + col] = 0;
         nQueens[row][col] = 'Q';
-        solveNQueens(n, row + 1, nQueens, flag_col, flag_45, flag_135, result);
+        solveNQueens(n, row + 1, nQueens, flag_col, diagonal, anti_diagonal,
+                     result);
         nQueens[row][col] = '.';
         flag_col[col] = 1;
-        flag_45[row + col] = 1;
-        flag_135[n - 1 + col - row] = 1;
+        diagonal[n - 1 - row + col] = 1;
+        anti_diagonal[row + col] = 1;
       }
     }
   }
