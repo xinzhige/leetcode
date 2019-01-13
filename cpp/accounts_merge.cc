7edf159485409079a717255b79a0b351fba17cb6
Zhige Xin <xinzhige8@gmail.com>
2019-01-13 01:08:00 -0800

    Add Union Find approach

--- cpp/accounts_merge.cc
+++ cpp/accounts_merge.cc
@@ -13,7 +13,7 @@ public:
         email2name[accounts[i][j]].push_back(i);
       }
     }
-    vector<bool> visited(n, false);
+    vector<int> visited(n, 0);
     for (int i = 0; i < n; ++i) {
       if (visited[i] == 0) {
         queue<int> q;
@@ -22,13 +22,13 @@ public:
         while (!q.empty()) {
           int t = q.front();
           q.pop();
-          visited[t] = true;
+          visited[t] = 1;
           for (int j = 1; j < accounts[t].size(); ++j) {
             myset.insert(accounts[t][j]);
             for (int name : email2name[accounts[t][j]]) {
-              if (!visited[name]) {
+              if (visited[name] == 0) {
                 q.push(name);
-                visited[name] = true;
+                visited[name] = 1;
               }
             }
           }
@@ -39,5 +39,47 @@ public:
       }
     }
     return result;
+  }
+};
+
+
+// Union find
+class Solution {
+public:
+  vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
+    vector<int> roots(accounts.size(), 0);
+    unordered_map<string, int> email2Id;
+    for (int i = 0; i < accounts.size(); i++) {
+      roots[i] = i;
+      for (int j = 1; j < accounts[i].size(); j++) {
+        if (email2Id.count(accounts[i][j]) > 0) {
+          int rooti = find(roots, i);
+          int roote = find(roots, email2Id[accounts[i][j]]);
+          roots[rooti] = roote;
+        } else {
+          email2Id[accounts[i][j]] = i;
+        }
+      }
+    }
+    unordered_map<int, set<string>> id2Emails;
+    for (const auto &node : email2Id) {
+      int root = find(roots, node.second);
+      id2Emails[root].insert(node.first);
+    }
+    vector<vector<string>> result;
+    for (const auto &node : id2Emails) {
+      vector<string> email(node.second.begin(), node.second.end());
+      email.insert(email.begin(), accounts[node.first][0]);
+      result.push_back(email);
+    }
+    return result;
+  }
+private:
+  int find(vector<int> &roots, int id) {
+    while (roots[id] != id) {
+      roots[id] = roots[roots[id]];
+      id = roots[id];
+    }
+    return id;
   }
 };
