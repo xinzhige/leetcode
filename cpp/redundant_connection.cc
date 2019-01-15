672929c811720ec48a618b48484a0bc567ba7b1f
Zhige Xin <xinzhige8@gmail.com>
2019-01-14 17:49:23 -0800

    Add union by rank trick

--- cpp/redundant_connection.cc
+++ cpp/redundant_connection.cc
@@ -1,7 +1,7 @@
 class Solution {
 public:
   vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-    vector<int> parent(edges.size() + 1, 0);
+    vector<int> parent(2000, 0);
     for (int i = 1; i < parent.size(); ++i) {
       parent[i] = i;
     }
@@ -15,10 +15,66 @@ public:
     }
     return {};
   }
-  int find(vector<int>& root, int x) {
-    for (; x != root[x]; x = root[x]) {
-      root[x] = root[root[x]];
+  int find(vector<int>& parent, int i) {
+    while (i != parent[i]) {
+      parent[i] = parent[parent[i]];
+      i = parent[i];
     }
-    return x;
+    return i;
   }
+};
+
+
+// Use union by rank
+class UnionFind {
+public:
+  UnionFind(int size) {
+    for (int i = 0; i < size; ++i) {
+      parent.push_back(i);
+      rank.push_back(0);
+    }
+  }
+  int find(int i) {
+    while (i != parent[i]) {
+      parent[i] = parent[parent[i]];
+      i = parent[i];
+    }
+    return i;
+  }
+  void runUnion(int x, int y) {
+    int rootx = find(x);
+    int rooty = find(y);
+    if (rootx != rooty) {
+      if (rank[rootx] > rank[rooty]) {
+        parent[rooty] = rootx;
+      } else if (rank[rootx] < rank[rooty]) {
+        parent[rootx] = rooty;
+      } else {
+        parent[rooty] = rootx;
+        rank[rootx] += 1;
+      }
+    }
+  }
+
+private:
+  vector<int> parent;
+  vector<int> rank;
+};
+
+class Solution {
+public:
+  vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+    UnionFind uf(MAX_EDGES + 1);
+    for (const auto &e: edges) {
+      int x = uf.find(e[0]);
+      int y = uf.find(e[1]);
+      if (x == y) {
+        return e;
+      }
+      uf.runUnion(x, y);
+    }
+    return {};
+  }
+private:
+  int MAX_EDGES = 2000;
 };
