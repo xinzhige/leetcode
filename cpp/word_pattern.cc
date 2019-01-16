12c16bfe822125e4b3216c1cdd9f15d2a2bc180d
Zhige Xin <xinzhige8@gmail.com>
2019-01-15 21:04:33 -0800

    Rename variables

--- cpp/word_pattern.cc
+++ cpp/word_pattern.cc
@@ -3,16 +3,16 @@
 class Solution {
 public:
   bool wordPattern(string pattern, string str) {
-    map<char, int> p2i;
-    map<string, int> w2i;
-    istringstream in(str);
+    unordered_map<char, int> c2i;
+    unordered_map<string, int> w2i;
+    istringstream is(str);
     int i = 0;
     int n = pattern.size();
-    for (string word; in >> word; ++i) {
-      if (i == n || p2i[pattern[i]] != w2i[word]) {
+    for (string word = ""; is >> word; ++i) {
+      if (i == n || c2i[pattern[i]] != w2i[word]) {
         return false;
       }
-      p2i[pattern[i]] = i + 1;
+      c2i[pattern[i]] = i + 1;
       w2i[word] = i + 1;
     }
     return i == n;
