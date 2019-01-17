3c0337f96d78606bc5cf083528ca8d45305d0415
Zhige Xin <xinzhige8@gmail.com>
2019-01-17 13:58:37 -0800

    Add comments

--- cpp/walls_and_gates.cc
+++ cpp/walls_and_gates.cc
@@ -1,23 +1,30 @@
+// Idea: from each 0, search its neighbors and update distance.
 class Solution {
 public:
   void wallsAndGates(vector<vector<int>>& rooms) {
-    for (int i = 0; i < rooms.size(); ++i) {
-      for (int j = 0; j < rooms[i].size(); ++j) {
+    if (rooms.empty() || rooms[0].empty()) {
+      return;
+    }
+    m = rooms.size();
+    n = rooms[0].size();
+    for (int i = 0; i < m; ++i) {
+      for (int j = 0; j < n; ++j) {
         if (rooms[i][j] == 0) {
           dfs(rooms, i, j, 0);
         }
       }
     }
   }
-  void dfs(vector<vector<int>> &rooms, int i, int j, int val) {
-    if (i >= 0 && i < rooms.size() && j >= 0 &&
-        j < rooms[i].size() && rooms[i][j] >= val) {
-      rooms[i][j] = val;
+  void dfs(vector<vector<int>> &rooms, int i, int j, int dist) {
+    if (i >= 0 && i < m && j >= 0 && j < n && rooms[i][j] >= dist) {
+      rooms[i][j] = dist;
       for (const auto &d : dirs) {
-        dfs(rooms, i + d[0], j + d[1], val + 1);
+        dfs(rooms, i + d[0], j + d[1], dist + 1);
       }
     }
   }
 private:
-  vector<vector<int>> dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+  const vector<vector<int>> dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+  int m = 0;
+  int n = 0;
 };
