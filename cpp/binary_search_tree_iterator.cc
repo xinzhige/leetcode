8199c4f617d95832f8ae8f9a37d4a44ac10f6212
Zhige Xin <xinzhige8@gmail.com>
2019-01-17 12:37:13 -0800

    Add comments

--- cpp/binary_search_tree_iterator.cc
+++ cpp/binary_search_tree_iterator.cc
@@ -1,4 +1,5 @@
 // Idea: use stack and do inorder traversal.
+// Note: by amortized analysis, in average, next() runs O(n)/n=O(1).
 class BSTIterator {
 public:
   BSTIterator(TreeNode *root) {
@@ -7,21 +8,24 @@ public:
 
   /** @return whether we have a next smallest number */
   bool hasNext() {
-    return !nodes.empty();
+    return !st.empty();
   }
 
   /** @return the next smallest number */
   int next() {
-    auto tmp = nodes.top();
-    nodes.pop();
+    auto tmp = st.top();
+    st.pop();
     pushAll(tmp->right);
     return tmp->val;
   }
+
   void pushAll(TreeNode *node) {
-    for (; node != nullptr; node = node->left) {
-      nodes.push(node);
+    while (node != nullptr) {
+      st.push(node);
+      node = node->left;
     }
   }
+
 private:
-  stack<TreeNode *> nodes;
+  stack<TreeNode *> st;
 };
