fc679cb09b565137e59bb02d8e337dfe4d07734c
Zhige Xin <xinzhige8@gmail.com>
2019-01-17 19:09:47 -0800

    Add comments

--- cpp/continuous_subarray_sum.cc
+++ cpp/continuous_subarray_sum.cc
@@ -1,9 +1,12 @@
+// Idea: use hashmap to store the cumulative sum (modulo) to index.
+// If there are two indices i and j such abs(i-j) > 1 and sum[i] = sum[j].
+// Note: index[0] = -1 in case nums{0, 0} and k = 0.
 class Solution {
 public:
   bool checkSubarraySum(vector<int>& nums, int k) {
-    int sum = 0;
-    map<int, int> index;
+    unordered_map<int, int> index;
     index[0] = -1;
+    int sum = 0;
     for (int i = 0; i < nums.size(); ++i) {
       sum += nums[i];
       int tmp = (k == 0) ? sum : (sum % k);
