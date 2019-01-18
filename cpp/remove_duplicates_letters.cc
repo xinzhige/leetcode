af83f2bbeb5ea827a47bfc264ad3e6e1ed410e1c
Zhige Xin <xinzhige8@gmail.com>
2019-01-17 16:24:27 -0800

    Add comments

--- cpp/remove_duplicates_letters.cc
+++ cpp/remove_duplicates_letters.cc
@@ -1,21 +1,25 @@
+// Idea: use a hashmap to memorize if the current character is visited.
+// If the tail character is bigger than the current one and its frequency
+// is larger than 0, it means there are such tail characters afterwards so
+// remove it.
 class Solution {
 public:
   string removeDuplicateLetters(string s) {
-    map<char, int> freq;
-    map<char, bool> visited;
-    for (const char &c : s) {
-      freq[c] += 1;
+    unordered_map<char, int> freq;
+    unordered_map<char, bool> visited;
+    for (const char &ch : s) {
+      freq[ch] += 1;
     }
     string result = "0";
-    for (const char &c : s) {
-      freq[c] -= 1;
-      if (!visited[c]) {
-        while (result.back() > c && freq[result.back()] > 0) {
+    for (const char &ch : s) {
+      freq[ch] -= 1;
+      if (!visited[ch]) {
+        while (result.back() > ch && freq[result.back()] > 0) {
           visited[result.back()] = false;
           result.pop_back();
         }
-        result += c;
-        visited[c] = true;
+        result += ch;
+        visited[ch] = true;
       }
     }
     return result.substr(1);
