11423279a2e4e4c76a5b66ebc2f64b42216b372d
Zhige Xin <xinzhige8@gmail.com>
2019-01-17 21:15:53 -0800

    Add comments

--- cpp/unique_paths2.cc
+++ cpp/unique_paths2.cc
@@ -1,12 +1,13 @@
-// by a vector, space: O(n)
+// Idea: if the current cell i is 0 then dp[i] = 0.
 class Solution {
 public:
   int uniquePathsWithObstacles(vector<vector<int> > &obstacleGrid) {
-    int m = obstacleGrid.size();
-    int n = obstacleGrid[0].size();
-    if(m == 0 || n == 0 || obstacleGrid[0][0] == 1) {
+    if (obstacleGrid.empty() || obstacleGrid[0].empty() ||
+        obstacleGrid[0][0] == 1) {
       return 0;
     }
+    int m = obstacleGrid.size();
+    int n = obstacleGrid[0].size();
     vector<int> dp(n, 0);
     dp[0] = 1;
     for(int i = 0; i < m; i++) {
