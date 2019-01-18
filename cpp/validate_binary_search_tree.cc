dc28ffb709f3e90cf32669a4ea4f7b9534d3cbd7
Zhige Xin <xinzhige8@gmail.com>
2019-01-17 21:51:41 -0800

    Add iterative algorithm

--- cpp/validate_binary_search_tree.cc
+++ cpp/validate_binary_search_tree.cc
@@ -16,3 +16,27 @@ public:
     return validate(root, nullptr, nullptr);
   }
 };
+
+
+// Iterative
+class Solution {
+public:
+  bool validate(TreeNode* root, TreeNode* minNode, TreeNode* maxNode) {
+    stack<TreeNode*> st;
+    TreeNode* pre = nullptr;
+    while (root != nullptr || !st.empty()) {
+      while (root != nullptr) {
+        st.push(root);
+        root = root->left;
+      }
+      root = st.top();
+      st.pop();
+      if (pre != nullptr && root->val <= pre->val) {
+        return false;
+      }
+      pre = root;
+      root = root->right;
+    }
+    return true;
+  }
+};
