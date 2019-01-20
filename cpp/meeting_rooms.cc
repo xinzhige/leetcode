859f7b1538e095b536869daaf4014c43e1151e17
Zhige Xin <xinzhige8@gmail.com>
2019-01-20 15:39:08 -0800

    Add comments

--- cpp/meeting_rooms.cc
+++ cpp/meeting_rooms.cc
@@ -1,7 +1,10 @@
+// Note: if i starts from 0 and intervals size is 0, then it can not
+// deduct 1 from intervals.size() because intervals.size() is unsigned
+// and it causes overflow.
 class Solution {
 public:
   bool canAttendMeetings(vector<Interval>& intervals) {
-    sort(intervals.begin(), intervals.end(), compare);
+    sort(intervals.begin(), intervals.end(), comp);
     for (int i = 1; i < intervals.size(); ++i) {
       if (intervals[i - 1].end > intervals[i].start) {
         return false;
@@ -11,10 +14,10 @@ public:
   }
 
 private:
-  class Compare {
+  class Comparison {
   public:
     bool operator() (const Interval & a, const Interval & b) {
       return a.start < b.start;
     }
-  }compare;
+  }comp;
 };
