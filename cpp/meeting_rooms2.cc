fe7bf0d9e9755d8d7579751be4c8fa7f98fc7ce8
Zhige Xin <xinzhige8@gmail.com>
2019-01-20 15:49:14 -0800

    Add comments

--- cpp/meeting_rooms2.cc
+++ cpp/meeting_rooms2.cc
@@ -1,24 +1,22 @@
-/**
- * Definition for an interval.
- * struct Interval {
- *     int start;
- *     int end;
- *     Interval() : start(0), end(0) {}
- *     Interval(int s, int e) : start(s), end(e) {}
- * };
- */
+/* Load all intervals to the TreeMap, where keys are intervals' start/end
+ *  boundaries, and values accumulate the changes at that point in time.
+ * Traverse the TreeMap (in other words, sweep the timeline). If a new
+ * interval starts, increase the counter (k value) by 1, and the counter
+ * decreases by 1, if an interval has finished.
+ * Calculate the number of the active ongoing intervals.
+*/
 class Solution {
 public:
   int minMeetingRooms(vector<Interval>& intervals) {
-    map<int, int> mymap;
-    for (const auto &a : intervals) {
-      mymap[a.start] += 1;
-      mymap[a.end] -= 1;
+    map<int, int> changes;
+    for (const auto &i : intervals) {
+      changes[i.start] += 1;
+      changes[i.end] -= 1;
     }
     int rooms = 0;
     int result = 0;
-    for (const auto &it : mymap) {
-      rooms += it.second; 
+    for (const auto &c : changes) {
+      rooms += c.second;
       result = max(result, rooms);
     }
     return result;
@@ -30,21 +28,21 @@ public:
 class Solution {
 public:
   int minMeetingRooms(vector<Interval>& intervals) {
-    sort(intervals.begin(), intervals.end(), mycompare);
-    priority_queue<int, vector<int>, greater<int>> q;
-    for (auto i : intervals) {
-      if (!q.empty() && q.top() <= i.start) {
-	q.pop(); 
-      } 
-      q.emplace(i.end);
+    sort(intervals.begin(), intervals.end(), comp);
+    priority_queue<int, vector<int>, greater<int>> pq;
+    for (const auto &i : intervals) {
+      if (!pq.empty() && pq.top() <= i.start) {
+        pq.pop();
+      }
+      pq.push(i.end);
     }
-    return q.size();
+    return pq.size();
   }
 
 private:
-  struct Compare {
+  struct Comparison {
     bool operator() (const Interval& a, const Interval& b) {
-        return a.start < b.start; 
+      return a.start < b.start;
     }
-  }mycompare;
+  }comp;
 };
