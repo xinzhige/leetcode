fbb1b5f93fe2f3f8d8aea607c0df899cb1b3b41e
Zhige Xin <xinzhige8@gmail.com>
2019-01-19 21:15:35 -0800

    Clean code

--- cpp/meeting_rooms.cc
+++ cpp/meeting_rooms.cc
@@ -1,23 +1,14 @@
-/**
- * Definition for an interval.
- * struct Interval {
- *     int start;
- *     int end;
- *     Interval() : start(0), end(0) {}
- *     Interval(int s, int e) : start(s), end(e) {}
- * };
- */
 class Solution {
 public:
-    bool canAttendMeetings(vector<Interval>& intervals) {
-        sort(intervals.begin(), intervals.end(), compare);
-        for (int i = 1; i < intervals.size(); ++i) {
-            if (intervals[i - 1].end > intervals[i].start) {
-                return false; 
-            } 
-        }
-        return true;
+  bool canAttendMeetings(vector<Interval>& intervals) {
+    sort(intervals.begin(), intervals.end(), compare);
+    for (int i = 1; i < intervals.size(); ++i) {
+      if (intervals[i - 1].end > intervals[i].start) {
+        return false;
+      }
     }
+    return true;
+  }
 
 private:
   class Compare {
