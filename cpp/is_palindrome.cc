b50ad75992f280c3cef1ca2d5ad9a23b6025fba6
Zhige Xin <xinzhige8@gmail.com>
2019-01-23 11:34:44 -0800

    Clean code

--- cpp/is_palindrome.cc
+++ cpp/is_palindrome.cc
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    bool isPalindrome(string s) {
-        int begin = 0;
-        int end = s.size() - 1;
-        while (begin < end) {
-            if (!isalnum(s[begin])) {
-                begin += 1;
-            } else if (!isalnum(s[end])) {
-                end -= 1;
-            } else {
-                if (tolower(s[begin]) == tolower(s[end])) {
-                    begin += 1;
-                    end -= 1;
-                } else {
-                    return false;
-                }
-            }
+  bool isPalindrome(string s) {
+    int low = 0;
+    int high = s.size() - 1;
+    while (low < high) {
+      if (!isalnum(s[low])) {
+        low += 1;
+      } else if (!isalnum(s[high])) {
+        high -= 1;
+      } else {
+        if (tolower(s[low]) == tolower(s[high])) {
+          low += 1;
+          high -= 1;
+        } else {
+          return false;
         }
-        return true;
+      }
     }
+    return true;
+  }
 };
