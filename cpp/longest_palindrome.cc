8ba4959ae51c3e4c58654859edd32a6b6a7ed892
Zhige Xin <xinzhige8@gmail.com>
2019-01-23 12:00:55 -0800

    Clean code

--- cpp/longest_palindrome.cc
+++ cpp/longest_palindrome.cc
@@ -1,31 +1,22 @@
 class Solution {
 public:
-  string expandAroundCenter(string s, int left, int right) {
-    int l = left, r = right;
-    int n = s.length();
-    while (l >= 0 && r <= n-1 && s[l] == s[r]) {
-      l--;
-      r++;
+  int longestPalindrome(string s) {
+    int result = 0;
+    unordered_map<char, int> freq;
+    for (const auto &c : s) {
+      freq[c] += 1;
     }
-    return s.substr(l + 1, r - l - 1);
-  }
-  string longestPalindrome(string s) {
-    int n = s.length();
-    if (n == 0) {
-      return "";
-    }
-    string longest = s.substr(0, 1);  // a single char itself is a palindrome
-    for (int i = 0; i < n - 1; i++) {
-      string p1 = expandAroundCenter(s, i, i);
-      if (p1.length() > longest.length()) { // center is a char
-	longest = p1;
-      }
-      
-      string p2 = expandAroundCenter(s, i, i + 1);
-      if (p2.length() > longest.length()) { // center is in between two neighbor chars
-	longest = p2;
+    bool odd = false;
+    for (const auto &f : freq) {
+      if (f.second % 2 == 0) {
+        result += f.second;
+      } else {
+        if (!odd) {
+          odd = true;
+        }
+        result += f.second - 1;
       }
     }
-    return longest;
+    return odd ? result + 1 : result;
   }
 };
