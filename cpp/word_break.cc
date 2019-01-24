ae426268e6974e8e29577268bc69578628d22838
Zhige Xin <xinzhige8@gmail.com>
2019-01-23 21:12:05 -0800

    Clean code

--- cpp/word_break.cc
+++ cpp/word_break.cc
@@ -1,21 +1,21 @@
 class Solution {
 public:
   bool wordBreak(string s, vector<string>& wordDict) {
-    int n = s.size();
-    vector<int> result(n + 1, 0);
     unordered_set<string> dict;
     for (const auto & word : wordDict) {
       dict.insert(word);
     }
-    result[0] = 1;
-    for (int i = 0; i <= n; ++i) {
+    int n = s.size();
+    vector<int> dp(n + 1, 0);
+    dp[0] = 1;
+    for (int i = 1; i <= n; ++i) {
       for (int j = 0; j < i; ++j) {
-        if (result[j] == 1 && dict.count(s.substr(j, i - j)) > 0) {
-          result[i] = 1;
+        if (dp[j] == 1 && dict.count(s.substr(j, i - j)) > 0) {
+          dp[i] = 1;
           break;
         }
       }
     }
-    return result[n] == 1;
+    return dp[n] == 1;
   }
 };
