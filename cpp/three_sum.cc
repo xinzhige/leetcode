5f6cca8e6b79f27e0d16fbe638072c6c0b7dbd94
Zhige Xin <xinzhige8@gmail.com>
2019-01-24 19:00:05 -0800

    Clean code

--- cpp/three_sum.cc
+++ cpp/three_sum.cc
@@ -6,17 +6,16 @@ public:
     int n = nums.size();
     for (int i = 0; i < n; i++) {
       if (i == 0 || nums[i] != nums[i - 1]) {
-
         int l = i + 1;
         int r = n - 1;
         while (l < r) {
           int sum = nums[i] + nums[l] + nums[r];
-          if (sum > 0) {
-            r -= 1;
-          } else if (sum < 0) {
+          if (sum < 0) {
             l += 1;
+          } else if (sum > 0) {
+            r -= 1;
           } else {
-            result.emplace_back(vector<int> {nums[i], nums[l], nums[r]});
+            result.push_back({nums[i], nums[l], nums[r]});
             for (; l < n - 1 && nums[l] == nums[l + 1]; ++l);
             for (; r > 0 && nums[r] == nums[r - 1]; --r);
             l += 1;
