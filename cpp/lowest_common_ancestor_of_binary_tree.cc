67f548e0e6393094e6a9279a1f310eaec3407876
Zhige Xin <xinzhige8@gmail.com>
2019-01-24 19:00:45 -0800

    Add iterative algorithm

--- cpp/lowest_common_ancestor_of_binary_tree.cc
+++ cpp/lowest_common_ancestor_of_binary_tree.cc
@@ -6,9 +6,51 @@ public:
     }
     TreeNode *leftNode = lowestCommonAncestor(root->left, p, q);
     TreeNode *rightNode = lowestCommonAncestor(root->right, p, q);
-    if (leftNode != nullptr && rightNode != nullptr) {
-      return root;
+    if (leftNode == nullptr) {
+      return rightNode;
     }
-    return leftNode != nullptr ? leftNode : rightNode;
+    if (rightNode == nullptr) {
+      return leftNode;
+    }
+    return root;
+  }
+};
+
+
+// Iterative
+class Solution {
+public:
+  TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    TreeNode *cur = root;
+    TreeNode *last = nullptr;
+    vector<TreeNode*> pathp;
+    vector<TreeNode*> pathq;
+    vector<TreeNode*> temp;
+    while (pathp.empty() || pathq.empty()) {
+      if (cur != nullptr) {
+        temp.push_back(cur);
+        if (temp.back() == p) {
+          pathp = temp;
+        }
+        if (temp.back() == q) {
+          pathq = temp;
+        }
+        cur = cur->left;
+      } else {
+        if (temp.back()->right != nullptr && temp.back()->right != last) {
+          cur = temp.back()->right;
+        } else {
+          last = temp.back();
+          temp.pop_back();
+        }
+      }
+    }
+    int n = min(pathp.size(), pathq.size());
+    for (int i = 1; i < n; ++i) {
+      if (pathp[i] != pathq[i]) {
+        return pathp[i - 1];
+      }
+    }
+    return pathp[n - 1];
   }
 };
