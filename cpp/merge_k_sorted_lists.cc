a8d129020b2f3ac972dec4dc8cfee1770b876dfd
Zhige Xin <xinzhige8@gmail.com>
2019-01-26 11:42:23 -0800

    Rewrite the algorithm

--- cpp/merge_k_sorted_lists.cc
+++ cpp/merge_k_sorted_lists.cc
@@ -1,38 +1,33 @@
+// Idea: like merge sort, merge two neighbors and double the gap.
 class Solution {
 public:
   ListNode *mergeKLists(vector<ListNode *> &lists) {
-    if (lists.size() == 0) {
+    if (lists.empty()) {
       return nullptr;
     }
-    int k = 0;
-    for (int n = lists.size(); n > 1; n = k) {
-      // If n is odd, k should be (n+1)/2 to avoid merging the middle.
-      k = (n + 1) / 2;
-      for (int i = 0; i < n / 2; ++i) {
-        lists[i] = mergeTwoLists(lists[i], lists[i+k]);
+    int n = lists.size();
+    for (int k = 1; k < n; k *= 2) {
+      for (int i = 0; i < n - k; i += 2*k) {
+        lists[i] = merge2Lists(lists[i], lists[i+k]);
       }
     }
     return lists[0];
   }
 
-  ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
-    auto dummy = new ListNode(0);
-    auto head = dummy;
+  ListNode *merge2Lists(ListNode *l1, ListNode *l2) {
+    ListNode *dummy = new ListNode(0);
+    ListNode *l = dummy;
     while (l1 != nullptr && l2 != nullptr) {
       if (l1->val < l2->val) {
-        head->next = l1;
+        l->next = l1;
         l1 = l1->next;
       } else {
-        head->next = l2;
+        l->next = l2;
         l2 = l2->next;
       }
-      head = head->next;
+      l = l->next;
     }
-    if (l1 == nullptr) {
-      head->next = l2;
-    } else {
-      head->next = l1;
-    }
+    l->next = (l1 == nullptr ? l2 : l1);
     return dummy->next;
   }
 };
@@ -42,10 +37,10 @@ public:
 class Solution {
 public:
   vector<int> mergekSortedArrays(vector<vector<int>>& arrays) {
-    vector<int> result;
     if (arrays.empty()) {
-      return result;
+      return {};
     }
+    vector<int> result;
     priority_queue<Node, vector<Node>, mycomparison> pq;
     for(int i = 0; i < arrays.size(); i++) {
       if(!arrays[i].empty()) {
