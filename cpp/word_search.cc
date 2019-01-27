9ced30062a4c5bc572a0f158c518dde55d6abbed
Zhige Xin <xinzhige8@gmail.com>
2019-01-27 00:13:44 -0800

    Clean code

--- cpp/word_search.cc
+++ cpp/word_search.cc
@@ -1,34 +1,41 @@
-// Suppose the board only contains the alphabet
+// Backtracking
 class Solution {
 public:
   bool exist(vector<vector<char>> &board, string word) {
-    for (int i = 0; i < board.size(); ++i) {
-      for (int j = 0; j < board[0].size(); ++j) {
-    	if (dfs(board, word, 0, i, j)) {
-    	  return true;
-    	}
+    if (board.empty() || board[0].empty()) {
+      return false;
+    }
+    m = board.size();
+    n = board[0].size();
+    for (int i = 0; i < m; ++i) {
+      for (int j = 0; j < n; ++j) {
+        if (backtrack(board, word, 0, i, j)) {
+          return true;
+        }
       }
     }
     return false;
   }
 private:
-  bool dfs(vector<vector<char>> &board, const string &word, 
-	   int index, int x, int y) {
-    if (index == word.size()) {
+  bool backtrack(vector<vector<char>> &board, const string &word,
+                 int level, int i, int j) {
+    if (level == word.size()) {
       return true;
     }
-    if (x < 0 || y < 0 || x >= board.size() || y >= board[0].size()) {
-      return false;
+    bool result = false;
+    if (i >= 0 && i < m && j >= 0 && j < n && board[i][j] == word[level]) {
+      board[i][j] = '#';
+      for (const auto &dir : dirs) {
+        result = result || backtrack(board, word, level + 1,
+                                     i + dir[0], j + dir[1]);
+      }
+      board[i][j] = word[level];
     }
-    if (board[x][y] != word[index]) {
-      return false;
-    }
-    board[x][y] = '#';
-    bool result = dfs(board, word, index + 1, x - 1, y) ||
-      dfs(board, word, index + 1, x + 1, y) ||
-      dfs(board, word, index + 1, x, y - 1) ||
-      dfs(board, word, index + 1, x, y + 1);
-    board[x][y] = word[index];
     return result;
   }
+
+private:
+  vector<vector<int>> dirs = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
+  int m = 0;
+  int n = 0;
 };
