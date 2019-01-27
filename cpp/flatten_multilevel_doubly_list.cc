301eb7a6afcef50083eec77fd839568d1adc547b
Zhige Xin <xinzhige8@gmail.com>
2019-01-26 23:38:58 -0800

    Add comments

--- cpp/flatten_multilevel_doubly_list.cc
+++ cpp/flatten_multilevel_doubly_list.cc
@@ -1,12 +1,13 @@
-// Insert the second layer list into the first one to form a new layer and
-// then insert the third layer into the new layer and so on.
+// Idea: insert the second layer list into the first one to form a new layer
+// and then insert the third layer into the new layer and so on.
+// Note: don't forget to let child pointer be null.
 class Solution {
 public:
   Node* flatten(Node* head) {
-    for (auto h = head; h != nullptr; h = h->next) {
+    for (Node* h = head; h != nullptr; h = h->next) {
       if (h->child != nullptr) {
-        auto post = h->next;
-        auto last = h->child;
+        Node* post = h->next;
+        Node* last = h->child;
         for (; last->next != nullptr; last = last->next);
         h->next = h->child;
         h->next->prev = h;
