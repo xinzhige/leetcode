d28afdcc0ab3d78ca9964af8db3bfad84c5df3a2
Zhige Xin <xinzhige8@gmail.com>
2019-01-27 15:44:43 -0800

    Clean code

--- cpp/strobogrammatic_number.cc
+++ cpp/strobogrammatic_number.cc
@@ -1,32 +1,11 @@
-// Without hash table
 class Solution {
 public:
   bool isStrobogrammatic(string num) {
-    for (int left = 0, right = num.size() - 1; left <= right; left++, right--) {
-      if ((num[left] == '0' && num[right] == '0') ||
-          (num[left] == '1' && num[right] == '1') ||
-          (num[left] == '8' && num[right] == '8')) {
-        continue;
-      } else if ((num[left] == '6' && num[right] == '9') ||
-                 (num[left] == '9' && num[right] == '6')) {
-        if (left == right) {
-          return false;
-        }
-      } else {
-        return false;
-      }
-    }
-    return true;
-  }
-};
-
-// With hash table
-class Solution {
-public:
-  bool isStrobogrammatic(string num) {
-    unordered_map<char, char> mymap{{'0', '0'}, {'1', '1'}, {'6', '9'}, {'8', '8'}, {'9', '6'}};
-    for (int left = 0, right = num.size() - 1; left <= right; left++, right--) {
-      if (mymap.find(num[left]) == mymap.end() || mymap[num[left]] != num[right]) {
+    unordered_map<char, char> mymap{{'0', '0'}, {'1', '1'}, {'6', '9'},
+                                    {'8', '8'}, {'9', '6'}};
+    for (int left = 0, right = num.size() - 1; left <= right;
+         left++, right--) {
+      if (mymap.count(num[left]) == 0 || mymap[num[left]] != num[right]) {
         return false;
       }
     }
