de975598690c54490fa32747228d7016cc9d92f0
Zhige Xin <xinzhige8@gmail.com>
2019-01-28 15:41:10 -0800

    Clean code

--- cpp/longest_consecutive_sequence.cc
+++ cpp/longest_consecutive_sequence.cc
@@ -1,26 +1,21 @@
 class Solution {
 public:
-  int longestConsecutive(vector<int> &num) {
-    unordered_map<int,bool> used;
-    for (auto x : num) {
-      used[x] = false;
+  int longestConsecutive(vector<int> &nums) {
+    unordered_set<int> used;
+    for (const int &x : nums) {
+      used.insert(x);
     }
     int result = 0;
-    for (auto curr : num) {
-      if (used[curr]) {
-        continue;
+    for (const int &x : used) {
+      if (used.count(x - 1) == 0) {
+        int curNum = x;
+        int curLen = 1;
+        while (used.count(curNum + 1) > 0) {
+          curNum += 1;
+          curLen += 1;
+        }
+        result = max(result, curLen);
       }
-      int curr_max = 1;
-      used[curr] = true;
-      for (int i = curr + 1; used.find(i) != used.end(); ++i) {
-        used[i] = true;
-        ++curr_max;
-      }
-      for (int i = curr - 1; used.find(i) != used.end(); --i) {
-        used[i] = true;
-        ++curr_max;
-      }
-      result = max(curr_max, result);
     }
     return result;
   }
