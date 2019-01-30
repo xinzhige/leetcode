c27c7ed081104ef0ef66d224f92917553e416ee2
Zhige Xin <xinzhige8@gmail.com>
2019-01-30 11:06:25 -0800

    Clean code

--- cpp/one_edit_distance.cc
+++ cpp/one_edit_distance.cc
@@ -5,13 +5,13 @@ public:
     int n = t.size();
     for (int i = 0; i < min(m, n); ++i) {
       if (s[i] != t[i]) {
-	if (m == n) {
-	  return s.substr(i+1) == t.substr(i+1);
-	} else if (m < n) {
-	  return s.substr(i) == t.substr(i+1);
-	} else {
-	  return s.substr(i+1) == t.substr(i); 
-	}
+        if (m == n) {
+          return s.substr(i+1) == t.substr(i+1);
+        } else if (m < n) {
+          return s.substr(i) == t.substr(i+1);
+        } else {
+          return s.substr(i+1) == t.substr(i);
+        }
       }
     }
     return abs(m - n) == 1;
