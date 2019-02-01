d10267f952aeaa6c28e2713ec9e1cfc897d7eb1d
Zhige Xin <xinzhige8@gmail.com>
2019-02-01 14:52:54 -0800

    Clean code

---
+++ cpp/subtree_of_another_tree.cc
@@ -0,0 +1,26 @@
+class Solution {
+public:
+  bool isSametree(TreeNode* s, TreeNode* t) {
+    if (s == nullptr && t == nullptr) {
+      return true;
+    }
+    if (s == nullptr || t == nullptr) {
+      return false;
+    }
+    if (s->val != t->val) {
+      return false;
+    }
+    return isSametree(s->left, t->left) &&
+      isSametree(s->right, t->right);
+  }
+
+  bool isSubtree(TreeNode* s, TreeNode* t) {
+    if (s == nullptr) {
+      return false;
+    }
+    if (isSametree(s, t)) {
+      return true;
+    }
+    return isSubtree(s->left, t) || isSubtree(s->right, t);
+  }
+};
