7d0895e1e749c05da2b1add658e00c6bd99470ba
Zhige Xin <xinzhige8@gmail.com>
2019-02-01 16:43:32 -0800

    Clean code

--- cpp/range_sum_query_2d_immutable.cc
+++ cpp/range_sum_query_2d_immutable.cc
@@ -1,25 +1,25 @@
 class NumMatrix {
 public:
   NumMatrix(vector<vector<int>> matrix) {
-    int m = matrix.size();
-    if (m == 0) {
+    if (matrix.empty() || matrix[0].empty()) {
       return;
     }
+    int m = matrix.size();
     int n = matrix[0].size();
-    mymatrix = vector<vector<int>>(m + 1, vector<int>(n + 1, 0));
-    for (int r = 0; r < m; ++r) {
-      for (int c = 0; c < n; ++c) {
-        mymatrix[r+1][c+1] += mymatrix[r+1][c] + mymatrix[r][c+1] -
-          mymatrix[r][c] + matrix[r][c];
+    sum = vector<vector<int>>(m + 1, vector<int>(n + 1, 0));
+    for (int i = 0; i < m; ++i) {
+      for (int j = 0; j < n; ++j) {
+        sum[i+1][j+1] += sum[i+1][j] + sum[i][j+1] -
+          sum[i][j] + matrix[i][j];
       }
     }
   }
 
   int sumRegion(int row1, int col1, int row2, int col2) {
-    return mymatrix[row2+1][col2+1] + mymatrix[row1][col1] -
-      mymatrix[row1][col2+1] - mymatrix[row2+1][col1];
+    return sum[row2+1][col2+1] + sum[row1][col1] -
+      sum[row1][col2+1] - sum[row2+1][col1];
   }
 
 private:
-  vector<vector<int>> mymatrix;
+  vector<vector<int>> sum;
 };
