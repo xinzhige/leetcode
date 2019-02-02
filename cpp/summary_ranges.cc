c1bcf2fb9c5ba7cf4ac48a0389e0a2ce70897327
Zhige Xin <xinzhige8@gmail.com>
2019-02-01 22:04:06 -0800

    Rewrite the algorithm

--- cpp/summary_ranges.cc
+++ cpp/summary_ranges.cc
@@ -1,22 +1,19 @@
+// Idea: two pointers, i and j point the begin and the end of each range.
 class Solution {
 public:
-    vector<string> summaryRanges(vector<int>& nums) {
-        const int n = nums.size();
-        vector<string> result;
-        if (n == 0) {
-            return result;
-        }
-        for (int i = 0; i < n;) {
-            int start = i;
-            int end = i;
-            for (; end + 1 < n && nums[end + 1] == nums[end] + 1; ++end);
-            if (start < end) {
-                result.push_back(to_string(nums[start]) + "->" + to_string(nums[end]));
-            } else {
-                result.push_back(to_string(nums[start]));
-            }
-            i = end + 1;
-        }
-        return result;
+  vector<string> summaryRanges(vector<int>& nums) {
+    vector<string> summary;
+    for (int i = 0, j = 0; j < nums.size(); ++j) {
+      if (j + 1 < nums.size() && nums[j + 1] == nums[j] + 1) {
+        continue;
+      }
+      if (i == j) {
+        summary.push_back(to_string(nums[i]));
+      } else {
+        summary.push_back(to_string(nums[i])+ "->" + to_string(nums[j]));
+      }
+      i = j + 1;
     }
+    return summary;
+  }
 };
