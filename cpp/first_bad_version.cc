e623bd5686d8772d8007c44df8892a5086d5f7d1
Zhige Xin <xinzhige8@gmail.com>
2019-02-01 18:36:47 -0800

    Clean code

--- cpp/first_bad_version.cc
+++ cpp/first_bad_version.cc
@@ -3,18 +3,18 @@ bool isBadVersion(int version);
 
 class Solution {
 public:
-    int firstBadVersion(int n) {
-        int begin = 1;
-        int end = n;
-        int mid = 0;
-        while (begin < end) {
-            mid = begin + (end - begin) / 2;
-            if (isBadVersion(mid)) {
-                end = mid; 
-            } else {
-                begin = mid + 1; 
-            }
-        }
-        return end;
+  int firstBadVersion(int n) {
+    int low = 1;
+    int high = n;
+    int mid = 0;
+    while (low < high) {
+      mid = low + (high - low) / 2;
+      if (isBadVersion(mid)) {
+        high = mid;
+      } else {
+        low = mid + 1;
+      }
     }
+    return high;
+  }
 };
