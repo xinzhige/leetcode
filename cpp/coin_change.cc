cd86df616a92d697000b69c1498727669185578c
Zhige Xin <xinzhige8@gmail.com>
2019-02-01 18:02:28 -0800

    Add comments

--- cpp/coin_change.cc
+++ cpp/coin_change.cc
@@ -1,12 +1,13 @@
+// DP, for each value from 1 to amount, compute minimum coins needed.
 class Solution {
 public:
   int coinChange(vector<int>& coins, int amount) {
     vector<int> dp(amount + 1, amount + 1);
     dp[0] = 0;
     for (int i = 1; i <= amount; ++i) {
-      for (int j = 0; j < coins.size(); ++j) {
-        if (coins[j] <= i) {
-          dp[i] = min(dp[i], dp[i - coins[j]] + 1);
+      for (const int &x: coins) {
+        if (x <= i) {
+          dp[i] = min(dp[i], dp[i - x] + 1);
         }
       }
     }
