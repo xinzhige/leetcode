0106130c0997bdf47d98ac3fd99de8dc951e0446
Zhige Xin <xinzhige8@gmail.com>
2019-02-05 22:00:46 -0800

    Clean code

--- cpp/target_sum.cc
+++ cpp/target_sum.cc
@@ -5,17 +5,17 @@ public:
     if ((sum < s) || ((s + sum) & 1)) {
       return 0;
     } else {
-      return subsetSum(nums, (s + sum) >> 1); 
+      return subsetSum(nums, (s + sum) >> 1);
     }
-  }   
+  }
 
 private:
   int subsetSum(vector<int>& nums, int s) {
     vector<int> dp(s+1, 0);
     dp[0] = 1;
-    for (const auto & n : nums) {
+    for (const int &n : nums) {
       for (int i = s; i >= n; i--) {
-	    dp[i] += dp[i - n];
+        dp[i] += dp[i - n];
       }
     }
     return dp[s];
