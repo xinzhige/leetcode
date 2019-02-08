b5e2515c5aed922290b8147784a3ffca3de1dfdf
Zhige Xin <xinzhige8@gmail.com>
2019-02-07 18:32:01 -0800

    Add comments

--- cpp/group_shifted_strings.cc
+++ cpp/group_shifted_strings.cc
@@ -3,18 +3,19 @@ public:
   vector<vector<string>> groupStrings(vector<string>& strings) {
     vector<vector<string>> result;
     unordered_map<string, vector<string>> mymap;
-    for (auto &s : strings) {
-      string tmp = "";
-      for (char &c : s) {
+    for (const auto &s : strings) {
+      string temp = "";
+      for (const char &c : s) {
+        // Add 26 to avoid negative value.
         int offset = (c + 26 - s[0]) % 26;
         // Add "," in case the offset is double digits
-        // like "0,1,2," and "0,12,"
-        tmp += to_string(offset) + ",";
+        // like "0,1,2," and "0,12,".
+        temp += to_string(offset) + ",";
       }
-      mymap[tmp].push_back(s);
+      mymap[temp].push_back(s);
     }
-    for (auto it = mymap.begin(); it != mymap.end(); ++it) {
-      result.push_back(it->second);
+    for (const auto &it : mymap) {
+      result.push_back(it.second);
     }
     return result;
   }
