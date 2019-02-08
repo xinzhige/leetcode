75670812beb02a5d0d316b0831af17dd4fe18ccf
Zhige Xin <xinzhige8@gmail.com>
2019-02-08 11:47:30 -0800

    Clean code

--- cpp/unique_paths.cc
+++ cpp/unique_paths.cc
@@ -1,27 +1,30 @@
-// By a matrix
+// By 2d array
 class Solution {
 public:
   int uniquePaths(int m, int n) {
-    vector<vector<int>> matrix(m, vector<int>(n, 0));
+    vector<vector<int>> grid(m, vector<int>(n, 0));
     for (int i = 0; i < m; ++i) {
-      matrix[i][0] = 1;
+      grid[i][0] = 1;
     }
     for (int j = 0; j < n; ++j) {
-      matrix[0][j] = 1;
+      grid[0][j] = 1;
     }
     for (int i = 1; i < m; ++i) {
       for (int j = 1; j < n; ++j) {
-        matrix[i][j] = matrix[i - 1][j] + matrix[i][j - 1];
+        grid[i][j] = grid[i - 1][j] + grid[i][j - 1];
       }
     }
-    return matrix[m - 1][n - 1];
+    return grid[m - 1][n - 1];
   }
 };
 
-// By a vector
+// By array
 class Solution {
 public:
   int uniquePaths(int m, int n) {
+    if (m < n) {
+      swap(m, n);
+    }
     vector<int> path(n, 1);
     for(int i = 1; i < m; ++i) {
       for(int j = 1; j < n; ++j) {
