0b268c10a1b6c3a5ee0da4a4320ee985ba39a3b6
Zhige Xin <xinzhige8@gmail.com>
2019-02-09 22:13:59 -0800

    Clean code

--- cpp/maze.cc
+++ cpp/maze.cc
@@ -1,3 +1,45 @@
+// DFS
+class Solution {
+public:
+  bool hasPath(vector<vector<int>>& maze, vector<int>& start,
+               vector<int>& destination) {
+    if (maze.empty() || maze[0].empty()) {
+      return true;
+    }
+    m = maze.size();
+    n = maze[0].size();
+    return backtrack(maze, start[0], start[1],
+                     destination[0], destination[1]);
+  }
+  bool backtrack(vector<vector<int>>& maze, int i, int j, int di, int dj) {
+    if (i == di && j == dj) {
+      return true;
+    }
+    bool result = false;
+    for (const auto &dir : dirs) {
+      int x = i;
+      int y = j;
+      while (x >= 0 && x < m && y >= 0 && y < n && maze[x][y] != 1) {
+        x += dir[0];
+        y += dir[1];
+      }
+      x -= dir[0];
+      y -= dir[1];
+      if (maze[x][y] == 0) {
+        maze[x][y] = 2;
+        result |= backtrack(maze, x, y, di, dj);
+      }
+    }
+    return result;
+  }
+
+private:
+  vector<vector<int>> dirs{{0,-1},{-1,0},{0,1},{1,0}};
+  int m = 0;
+  int n = 0;
+};
+
+
 // BFS, key: the ball can only roll in one direction.
 class Solution {
 public:
@@ -11,8 +53,8 @@ public:
     queue<pair<int, int>> ends;
     ends.emplace(start[0], start[1]);
     vector<vector<int>> dirs = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
-    vector<vector<bool>> visited(m, vector<bool>(n, false));
-    visited[start[0]][start[1]] = true;
+    vector<vector<int>> visited(m, vector<int>(n, 0));
+    visited[start[0]][start[1]] = 1;
     while (!ends.empty()) {
       auto t = ends.front();
       ends.pop();
@@ -22,14 +64,15 @@ public:
       for (const auto& dir : dirs) {
         int x = t.first;
         int y = t.second;
-        while (x + dir[0] >= 0 && x + dir[0] < m &&
-               y + dir[1] >= 0 && y + dir[1]  < n &&
-               maze[x + dir[0]][y + dir[1] ] == 0) {
+        while (x >= 0 && x < m && y >= 0 && y < n &&
+               maze[x][y] == 0) {
           x += dir[0];
           y += dir[1];
         }
-        if (!visited[x][y]) {
-          visited[x][y] = true;
+        x -= dir[0];
+        y -= dir[1];
+        if (visited[x][y] == 0) {
+          visited[x][y] = 1;
           ends.push({x, y});
         }
       }
