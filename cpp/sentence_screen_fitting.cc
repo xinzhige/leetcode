7fff2af7b199ea217388943836080e4b98a23ed1
Zhige Xin <xinzhige8@gmail.com>
2019-02-09 21:23:22 -0800

    Add comments

--- cpp/sentence_screen_fitting.cc
+++ cpp/sentence_screen_fitting.cc
@@ -1,8 +1,8 @@
 // Idea: think of the screen as one dimension and determine how many
 // sentences can be filled in.
-// Note: 1. construct sentence by adding spaces in between words.
-// 2. the last character in each row must be space to make sure a word
-// cannot be split into two lines.
+// Note: 1. construct sentence by adding space between words.
+// 2. the last character in each row must be either space to make sure
+// a word is not split into two lines or just fit.
 class Solution {
 public:
   int wordsTyping(vector<string>& sentence, int rows, int cols) {
@@ -10,6 +10,8 @@ public:
     for (auto &word : sentence) {
       s += (word + " ");
     }
+    /* start points to the character in s that can be put as the first
+       in each row. */
     int start = 0;
     int n = s.size();
     for (int i = 0; i < rows; ++i) {
