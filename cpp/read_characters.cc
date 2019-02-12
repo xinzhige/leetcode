c613e18d8424a460b2f87466d78f9d62c6d83625
Zhige Xin <xinzhige8@gmail.com>
2019-02-12 14:09:34 -0800

    Clean code

--- cpp/read_characters.cc
+++ cpp/read_characters.cc
@@ -3,21 +3,21 @@ int read4(char *buf);
 
 class Solution {
 public:
-    /**
-     * @param buf Destination buffer
-     * @param n   Maximum number of characters to read
-     * @return    The number of characters read
-     */
-    int read(char *buf, int n) {
-        int result = 0;
-        for (; n > 0; n -= 4) {
-            int tmp = min(read4(buf), n); 
-            result += tmp;
-            buf += tmp;
-            if (tmp < 4) {
-                break; 
-            }
-        }
-        return result;
+  /**
+   * @param buf Destination buffer
+   * @param n   Maximum number of characters to read
+   * @return    The number of characters read
+   */
+  int read(char *buf, int n) {
+    int result = 0;
+    for (; n > 0; n -= 4) {
+      int tmp = min(read4(buf), n);
+      result += tmp;
+      buf += tmp;
+      if (tmp < 4) {
+        break;
+      }
     }
- };
+    return result;
+  }
+};
