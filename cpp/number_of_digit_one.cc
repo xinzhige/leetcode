7cb49de2bb6bfea2daeb42a5681031e0ddf5df04
Zhige Xin <xinzhige8@gmail.com>
2019-02-14 14:29:00 -0800

    Add comments

--- cpp/number_of_digit_one.cc
+++ cpp/number_of_digit_one.cc
@@ -5,7 +5,7 @@ public:
     for (long m = 1; m <= n; m *= 10) {
       long a = n / m;
       long b = n % m;
-      long c = long((a % 10) == 1) * (b + 1);  // partial streak
+      long c = ((a % 10) == 1 ? b + 1 : 0);  // partial streak
       // If current digit is 0 or 1 then there are only n/m prefix.
       ones += (a + 8) / 10 * m + c;
     }
