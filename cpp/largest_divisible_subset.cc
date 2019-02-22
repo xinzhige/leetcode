51086b5d99f4eff035be326c17d8ce9007eea80d
xinzhige <xinzhige8@gmail.com>
2019-02-22 13:36:37 -0800

    Add comments

--- cpp/largest_divisible_subset.cc
+++ cpp/largest_divisible_subset.cc
@@ -5,19 +5,18 @@ public:
     int n = nums.size();
     vector<int> count(n, 1);
     vector<int> pre(n, -1);
+    // Note: the array should be sorted becuase the result is not ordered.
     sort(nums.begin(), nums.end());
     int cur_max = 0;
     int index = -1;
     for (int i = 0; i < n; ++i) {
       for (int j = i - 1; j >= 0; --j) {
-        if (nums[i] % nums[j] == 0) {
-          if (1 + count[j] > count[i]) {
-            count[i] = count[j] + 1;
-            pre[i] = j;
-          }
+        if (nums[i] % nums[j] == 0 && count[i] < 1 + count[j]) {
+          count[i] = count[j] + 1;
+          pre[i] = j;
         }
       }
-      if (count[i] > cur_max) {
+      if (cur_max < count[i]) {
         cur_max = count[i];
         index = i;
       }
