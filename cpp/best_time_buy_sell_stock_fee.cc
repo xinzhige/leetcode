0158bedf7796b2ddf60fe3498790c75670c58881
Zhige Xin <xinzhige8@gmail.com>
2020-07-16 23:08:34 -0700

    Add Best Time to Buy and Sell Stock with Transaction Fee

---
+++ cpp/best_time_buy_sell_stock_fee.cc
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int dp_i_0 = 0;
+        int dp_i_1 = INT_MIN;
+        for (int price : prices) {
+            int tmp = dp_i_0;
+            dp_i_0 = max(dp_i_0, dp_i_1 + price);
+            dp_i_1 = max(dp_i_1, tmp - price -fee);
+        }
+        return dp_i_0; 
+    }
+};
