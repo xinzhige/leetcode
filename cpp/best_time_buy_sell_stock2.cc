f00e28b40504f77d93ad5975f09c05af7cdebfd8
Zhige Xin <xinzhige8@gmail.com>
2020-07-16 22:45:53 -0700

    Add Best Time to Buy and Sell Stock II

--- cpp/best_time_buy_sell_stock2.cc
+++ cpp/best_time_buy_sell_stock2.cc
@@ -1,13 +1,12 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int n = prices.size();
         int dp_i_0 = 0;
         int dp_i_1 = INT_MIN;
-        for (int i = 0; i < n; ++i) {
+        for (int price : prices) {
             int tmp = dp_i_0;
-            dp_i_0 = max(dp_i_0, dp_i_1 + prices[i]);
-            dp_i_1 = max(dp_i_1, tmp -prices[i]);
+            dp_i_0 = max(dp_i_0, dp_i_1 + price);
+            dp_i_1 = max(dp_i_1, tmp - price);
         }
         return dp_i_0; 
     }
