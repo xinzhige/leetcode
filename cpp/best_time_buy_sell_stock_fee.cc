650015774198834eb7fc49f17a5e9542b7bc0d4e
Zhige Xin <xinzhige8@gmail.com>
2020-07-18 22:16:59 -0700

    Correct function parameter list

--- cpp/best_time_buy_sell_stock_fee.cc
+++ cpp/best_time_buy_sell_stock_fee.cc
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
+    int maxProfit(vector<int>& prices, int fee) {
         int dp_i_0 = 0;
         int dp_i_1 = INT_MIN;
         for (int price : prices) {
             int tmp = dp_i_0;
             dp_i_0 = max(dp_i_0, dp_i_1 + price);
-            dp_i_1 = max(dp_i_1, tmp - price -fee);
+            dp_i_1 = max(dp_i_1, tmp - price - fee);
         }
         return dp_i_0; 
     }
