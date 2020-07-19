bd8813d0cb36133c765fbc15ca3bb75e3592868f
Zhige Xin <xinzhige8@gmail.com>
2020-07-19 10:25:29 -0700

    Add 188. Best Time to Buy and Sell Stock IV

--- cpp/best_time_buy_sell_stock4.cc
+++ cpp/best_time_buy_sell_stock4.cc
@@ -1,30 +1,28 @@
 class Solution {
 public:
-  int maxProfit(int k, vector<int>& prices) {
-    if (prices.empty()) {
-      return 0;
+    int maxProfit(int k, vector<int>& prices) {
+        int len = prices.size();
+        if (k >= len / 2) {
+            return quick_solve(prices);
+        }
+        vector<vector<int>> dp(k + 1, vector<int>(len, 0));
+        for (int i = 1; i <= k; i++) {
+            int tmp_max =  -prices[0];
+            for (int j = 1; j < len; j++) {
+                dp[i][j] = max(dp[i][j - 1], prices[j] + tmp_max);
+                tmp_max =  max(tmp_max, dp[i - 1][j - 1] - prices[j]);
+            }
+        }
+        return dp[k][len - 1];
     }
-    if (k >= prices.size()) {
-      return solveMaxProfit(prices);
+
+private: int quick_solve(vector<int>& prices) {
+        int len = prices.size();
+        int profit = 0;
+        for (int i = 1; i < len; i++)
+            if (prices[i] > prices[i - 1]) {
+                profit += prices[i] - prices[i - 1];
+            }
+        return profit;
     }
-    vector<int> global(k + 1, 0);
-    vector<int> local(k + 1, 0);
-    for (int i = 0; i < prices.size() - 1; ++i) {
-      int diff = prices[i + 1] - prices[i];
-      for (int j = k; j >= 1; --j) {
-        local[j] = max(global[j - 1] + max(diff, 0), local[j] + diff); // the reason including local[j] is global[j-1] doesn't necessarily mean selling the stock at day i - 1.
-        global[j] = max(global[j], local[j]);
-      }
-    }
-    return global[k];
-  }
-  int solveMaxProfit(vector<int> &prices) {
-    int result = 0;
-    for (int i = 1; i < prices.size(); ++i) {
-      if (prices[i] - prices[i - 1] > 0) {
-        result += prices[i] - prices[i - 1];
-      }
-    }
-    return result;
-  }
 };
