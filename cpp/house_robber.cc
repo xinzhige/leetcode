c39392b700d9ef6393e87d262c7d41ef9e4b9301
Zhige Xin <xinzhige8@gmail.com>
2020-07-21 17:08:31 -0700

    Add linear space complexity solution

--- cpp/house_robber.cc
+++ cpp/house_robber.cc
@@ -1,12 +1,26 @@
+// space: O(n)
+class Solution {
+public:
+  int rob(vector<int>& nums) {
+    int n = nums.size();
+    vector<int> dp(n + 2, 0);
+    for (int i = n - 1; i >= 0; i--) {
+      dp[i] = max(dp[i+1], dp[i+2] + nums[i]);
+    }
+    return dp[0];
+  }
+};
+
+// space: O(1)
 class Solution {
 public:
   int rob(vector<int>& nums) {
     int prev_max = 0;
     int curr_max = 0;
-    for (auto &n : nums) {
-      int curr = max(curr_max, prev_max + n);
+    for (int value : nums) {
+      int curr_value = max(curr_max, prev_max + value);
       prev_max = curr_max;
-      curr_max = curr;
+      curr_max = curr_value;
     }
     return curr_max;
   }
