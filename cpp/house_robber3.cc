d3573ae55b8c6b799fc85a61ff0825b5f50e19ae
Zhige Xin <xinzhige8@gmail.com>
2020-07-23 16:41:20 -0700

    Update code

--- cpp/house_robber3.cc
+++ cpp/house_robber3.cc
@@ -3,9 +3,9 @@ public:
   int rob(TreeNode* root) {
     int l = 0;
     int r = 0;
-    return helper(root, l, r);
+    return rob_helper(root, l, r);
   }
-  int helper(TreeNode* node, int& l, int& r) {
+  int rob_helper(TreeNode* node, int& l, int& r) {
     if (node == nullptr) {
       return 0;
     }
@@ -13,8 +13,8 @@ public:
     int lr = 0;
     int rl = 0;
     int rr = 0;
-    l = helper(node->left, ll, lr);
-    r = helper(node->right, rl, rr);
+    l = rob_helper(node->left, ll, lr);
+    r = rob_helper(node->right, rl, rr);
     return max(node->val + ll + lr + rl + rr, l + r);
   }
 };
