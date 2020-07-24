b3d70ce3775528b4aba425d4ad958dd9a625e3c1
Zhige Xin <xinzhige8@gmail.com>
2020-07-23 17:48:43 -0700

    Add 875. Koko Eating Bananas

---
+++ cpp/koko_eating_bananas.cc
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int H) {
+        int left = 1;
+        int right = get_max(piles) + 1; 
+        while (left < right) {
+            int mid = (right - left) / 2 + left;
+            if (!can_finish(piles, mid, H)) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+
+    bool can_finish(vector<int>& piles, int speed, int H) {
+        int count = 0;
+        for (int n : piles) {
+            count += time_count(n, speed);
+        }
+        return count <= H;
+    }
+
+    int time_count(int n, int speed) {
+        if (n % speed == 0) {
+            return n / speed;
+        }
+        return (n / speed) + 1;
+    }
+
+    int get_max(vector<int>& piles) {
+        int res = 0;
+        for (int n : piles) {
+            res = max(res, n);
+        }
+        return res;
+    }
+};
