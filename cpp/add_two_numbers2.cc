d52fad9012f18f149fae74a13abaa71e1e7acbd8
Zhige Xin <xinzhige8@gmail.com>
2020-08-04 16:32:57 -0700

    Apply stack instead of inversing list

--- cpp/add_two_numbers2.cc
+++ cpp/add_two_numbers2.cc
@@ -1,38 +1,31 @@
 class Solution {
 public:
   ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-    ListNode* p1 = reverseList(l1);
-    ListNode* p2 = reverseList(l2);
-    int carry = 0;
-    ListNode *dummy = new ListNode(0);
-    while (p1 != nullptr || p2 != nullptr || carry != 0) {
-      int x = (p1 == nullptr) ? 0 : p1->val;
-      int y = (p2 == nullptr) ? 0 : p2->val;
-      int sum = x + y + carry;
-      carry = sum / 10;
-      ListNode* node = new ListNode(sum % 10);
-      node->next = dummy->next;
-      dummy->next = node;
-      if (p1 != nullptr) {
-        p1 = p1->next;
-      }
-      if (p2 != nullptr) {
-        p2 = p2->next;
-      }
+    stack<int> s1, s2;
+    while (l1 != nullptr) {
+      s1.push(l1->val);
+      l1 = l1->next;
     }
-    return dummy->next;
-  }
-private:
-  ListNode* reverseList(ListNode* head) {
-    ListNode* prev = nullptr;
-    ListNode* curr = head;
-    ListNode* next = nullptr;
-    while (curr != nullptr) {
-      next = curr->next;
-      curr->next = prev;
-      prev = curr;
-      curr = next;
+    while (l2 != nullptr) {
+      s2.push(l2->val);
+      l2 = l2->next;
     }
-    return prev;
+    ListNode *head = nullptr;
+    int cur_sum = 0;
+    while (!s1.empty() || !s2.empty() || cur_sum != 0) {
+      if (!s1.empty()) {
+        cur_sum += s1.top();
+        s1.pop();
+      }
+      if (!s2.empty()) {
+        cur_sum += s2.top();
+        s2.pop();
+      }
+      ListNode *node = new ListNode(cur_sum % 10);
+      cur_sum /= 10;
+      node->next = head;
+      head = node;
+    }
+    return head;
   }
 };
