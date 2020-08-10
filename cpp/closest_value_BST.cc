984c092eed8c8681214ca2b141c6747a5f90e087
Zhige Xin <xinzhige8@gmail.com>
2020-08-10 16:53:32 -0700

    Remove file

--- cpp/closest_value_BST.cc
+++ cpp/closest_value_BST.cc
@@ -11,10 +11,8 @@ class Solution {
 public:
   int closestValue(TreeNode* root, double target) {
     int closest = root->val;
-    while (root) {
-      if (abs(root->val - target) <= abs(closest - target)) {
-	closest = root->val;
-      }
+    while (root != nullptr) {
+      closest = abs(root->val - target) < abs(closest - target) ? root->val : closest;
       root = target < root->val ? root->left : root->right;
     }
     return closest;
